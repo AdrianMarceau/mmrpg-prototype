0c4e5d555b01028330720703e2d1feab0b9b7d75
Adrian Marceau <adrian.marceau@gmail.com>
2015-05-10 15:23:33 -0400

    Field object for new Prototype Subspace area, first encounter with Trill.

---
+++ data/fields/prototype-subspace.php
@@ -0,0 +1,23 @@
+<?
+// PROTOTYPE SUBSPACE
+$field = array(
+  'field_name' => 'Prototype Subspace',
+  'field_token' => 'prototype-subspace',
+  'field_multipliers' => array('experience' => 2.0),
+  'field_description' => '',
+  'field_game' => 'MMRPG',
+  'field_number' => 'RPG-002',
+  'field_class' => 'event',
+  'field_background' => 'prototype-subspace',
+  'field_foreground' => 'prototype-subspace',
+  'field_music' => 'prototype-subspace',
+  'field_music_name' => 'MM9 Suspense (Sega Genesis)',
+  'field_music_link' => array('http://youtu.be/H94zMKYWyYc'),
+  'field_mechas' => array(),
+  'field_background_frame' => array(0), //array(0,1),
+  'field_foreground_frame' => array(0),
+  'field_foreground_attachments' => array(
+    //'object_intro-field-wily' => array('class' => 'object', 'size' => 160, 'offset_x' => 12, 'offset_y' => 121, 'object_token' => 'intro-field-wily', 'object_frame' => array(0), 'object_direction' => 'right')
+    )
+  );
+?>
