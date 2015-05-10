8ee6e0d49fccdf3bca21694831eb2c03c19a4ad4
Adrian Marceau <adrian.marceau@gmail.com>
2015-05-10 14:41:15 -0400

    Adding placeholder ability objects for remaining MM3 special weapons.

---
+++ data/abilities/mm3/top-twister.php
@@ -0,0 +1,50 @@
+<?
+// TOP TWISTER
+$ability = array(
+  'ability_name' => 'Top Twister',
+  'ability_token' => 'top-twister',
+  'ability_game' => 'MM03',
+  'ability_group' => 'MM03/Weapons/021',
+  'ability_master' => 'top-man',
+  'ability_number' => 'DWN-021',
+  'ability_description' => 'The user spins rapidly to generate a large tornado that rushes toward the target to deal massive damage!',
+  'ability_type' => 'swift',
+  'ability_type2' => 'wind',
+  'ability_energy' => 8,
+  'ability_damage' => 33,
+  'ability_accuracy' => 90,
+  'ability_function' => function($objects){
+
+    // Extract all objects into the current scope
+    extract($objects);
+
+    // Target the opposing robot
+    $this_ability->target_options_update(array(
+      'frame' => 'shoot',
+      'success' => array(0, 75, 0, 10, $this_robot->print_robot_name().' uses the '.$this_ability->print_ability_name().'!')
+      ));
+    $this_robot->trigger_target($target_robot, $this_ability);
+
+    // Inflict damage on the opposing robot
+    $this_ability->damage_options_update(array(
+      'kind' => 'energy',
+      'kickback' => array(10, 0, 0),
+      'success' => array(1, -55, 0, 10, 'The '.$this_ability->print_ability_name().' hit the target!'),
+      'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_ability_name().' missed the target&hellip;')
+      ));
+    $this_ability->recovery_options_update(array(
+      'kind' => 'energy',
+      'frame' => 'taunt',
+      'kickback' => array(10, 0, 0),
+      'success' => array(1, -35, 0, 10, 'The '.$this_ability->print_ability_name().' was absorbed by the target!'),
+      'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_ability_name().' missed the target&hellip;')
+      ));
+    $energy_damage_amount = $this_ability->ability_damage;
+    $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+
+    // Return true on success
+    return true;
+
+  }
+  );
+?>
