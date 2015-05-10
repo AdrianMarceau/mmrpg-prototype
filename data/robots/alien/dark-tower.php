45115c2c82b0caac7e1754b15e511c6e9753c8b8
Adrian Marceau <adrian.marceau@gmail.com>
2015-05-10 12:08:19 -0400

    Create a placeholder robot object for the Dark Tower, though it may be changed to an item at some point like the stars.

---
+++ data/robots/alien/dark-tower.php
@@ -0,0 +1,33 @@
+<?
+// DARK TOWER
+$robot = array(
+  'robot_number' => 'DRK-TOWR', // OBJECT : DARK TOWER
+  'robot_class' => 'mecha',
+  'robot_game' => 'MMEXE',
+  'robot_name' => 'Dark Tower',
+  'robot_token' => 'dark-tower',
+  'robot_image_editor' => 412,
+  'robot_core' => 'empty',
+  'robot_field' => 'field',
+  'robot_description' => 'Dark Element Tower',
+  'robot_energy' => 200,
+  'robot_attack' => 200,
+  'robot_defense' => 200,
+  'robot_speed' => 200,
+  'robot_affinities' => array('copy', 'crystal', 'cutter', 'earth', 'electric', 'explode', 'flame', 'freeze', 'impact', 'laser', 'missile', 'nature', 'shadow', 'shield', 'space', 'swift', 'time', 'water', 'wind'),
+  'robot_abilities' => array('dark-boost', 'dark-break', 'dark-pulse'),
+  'robot_rewards' => array(
+    'abilities' => array(
+        array('level' => 0, 'token' => 'dark-boost'),
+        array('level' => 0, 'token' => 'dark-break'),
+        array('level' => 0, 'token' => 'dark-pulse')
+      )
+    ),
+  'robot_quotes' => array(
+    'battle_start' => '',
+    'battle_taunt' => '',
+    'battle_victory' => '',
+    'battle_defeat' => ''
+    )
+  );
+?>
