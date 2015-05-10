977523fa7314ca84fc3f442ac676e0eb2671f168
Adrian Marceau <adrian.marceau@gmail.com>
2015-05-10 12:02:33 -0400

    Created placeholder ability objects for the aliens Trill and Slur.

---
+++ data/abilities/alien/slur-aura.php
@@ -0,0 +1,47 @@
+<?
+// SLUR AURA
+$ability = array(
+  'ability_name' => 'Slur Aura',
+  'ability_token' => 'slur-aura',
+  'ability_game' => 'MMEXE',
+  'ability_class' => 'boss',
+  'ability_description' => 'The user surrounds itself in an aura that grants a temporarily affinity to all elemental damage for three turns!',
+  'ability_type' => 'space',
+  'ability_damage' => 0,
+  'ability_accuracy' => 100,
+  'ability_energy' => 16,
+  'ability_function' => function($objects){
+
+    // Extract all objects into the current scope
+    extract($objects);
+
+    // Target the opposing robot
+    $this_ability->target_options_update(array(
+      'frame' => 'shoot',
+      'success' => array(0, 75, 0, 10, $this_robot->print_robot_name().' uses the '.$this_ability->print_ability_name().'!')
+      ));
+    $this_robot->trigger_target($target_robot, $this_ability);
+
+    // Inflict damage on the opposing robot
+    $this_ability->damage_options_update(array(
+      'kind' => 'energy',
+      'kickback' => array(10, 0, 0),
+      'success' => array(1, -55, 0, 10, 'The '.$this_ability->print_ability_name().' hit the target!'),
+      'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_ability_name().' missed the target&hellip;')
+      ));
+    $this_ability->recovery_options_update(array(
+      'kind' => 'energy',
+      'frame' => 'taunt',
+      'kickback' => array(10, 0, 0),
+      'success' => array(1, -35, 0, 10, 'The '.$this_ability->print_ability_name().' was absorbed by the target!'),
+      'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_ability_name().' missed the target&hellip;')
+      ));
+    $energy_damage_amount = $this_ability->ability_damage;
+    $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+
+    // Return true on success
+    return true;
+
+  }
+  );
+?>
