4f6b0fac573f59f50958ebe089abdbc36f944893
Adrian Marceau <adrian.marceau@gmail.com>
2015-05-10 01:26:18 -0400

    Updated stylesheets to be compatible with new 2k15 formatting and mods.

--- styles/style.php
+++ styles/style.php
@@ -48,18 +48,27 @@ if (MMRPG_CONFIG_CACHE_INDEXES && file_exists($this_cache_filedir)){
 
   // Print out the PHP header in a comment
   $temp_css_markup = '/* -- MMRPG Prototype Stylesheet, Last Updated '.MMRPG_CONFIG_CACHE_DATE.' -- */'."\n";
-  
+
   // Require the master CSS file without any of the dynamic additions
   ob_start();
   require_once('style.css');
   $temp_css_markup .= ob_get_clean();
-  
+
   /* -- TYPE STYLES -- */
-  
+
   // Loop through every type in the database
   ob_start();
+  $u_agent = !empty($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+  $u_explorer = preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent) ? 1 : 0;
+  $u_firefox = preg_match('/Firefox/i',$u_agent) ? 1 : 0;
+  $u_chrome = preg_match('/Chrome/i',$u_agent) ? 1 : 0;
+  $u_safari = preg_match('/Safari/i',$u_agent) ? 1 : 0;
+  $u_opera = preg_match('/Opera/i',$u_agent) ? 1 : 0;
+
   foreach ($mmrpg_index['types'] AS $type_token => $type_info){
     ?>
+    #mmrpg .type.<?= $type_info['type_token'] ?>,
+    #mmrpg .type_<?= $type_info['type_token'] ?>,
     #mmrpg .ability_type_<?= $type_info['type_token'] ?>,
     #mmrpg .battle_type_<?= $type_info['type_token'] ?>,
     #mmrpg .field_type_<?= $type_info['type_token'] ?>,
@@ -68,10 +77,16 @@ if (MMRPG_CONFIG_CACHE_INDEXES && file_exists($this_cache_filedir)){
       border-color: rgb(<?= implode(',', $type_info['type_colour_dark']) ?>) !important;
       background-color: rgb(<?= implode(',', $type_info['type_colour_light']) ?>) !important;
     }
+    #mmrpg .tooltip .typelist .type_<?= $type_info['type_token'] ?>:before {
+      content: "<?= $type_info['type_name'] ?> ";
+    }
     <?
     // Loop through all the types again for the dual-type ability styles
     foreach ($mmrpg_index['types'] AS $type2_token => $type2_info){
+      if ($type_token == $type2_token){ continue; }
       ?>
+      #mmrpg .type.<?= $type_info['type_token'] ?>_<?= $type2_info['type_token'] ?>,
+      #mmrpg .type_<?= $type_info['type_token'] ?>_<?= $type2_info['type_token'] ?>,
       #mmrpg .ability_type_<?= $type_info['type_token'] ?>_<?= $type2_info['type_token'] ?>,
       #mmrpg .battle_type_<?= $type_info['type_token'] ?>_<?= $type2_info['type_token'] ?>,
       #mmrpg .field_type_<?= $type_info['type_token'] ?>_<?= $type2_info['type_token'] ?>,
@@ -79,21 +94,25 @@ if (MMRPG_CONFIG_CACHE_INDEXES && file_exists($this_cache_filedir)){
       #mmrpg .robot_type_<?= $type_info['type_token'] ?>_<?= $type2_info['type_token'] ?> {
         border-color: rgb(<?= implode(',', $type_info['type_colour_dark']) ?>) !important;
         background-color: rgb(<?= implode(',', $type_info['type_colour_light']) ?>) !important;
-        background-image: -webkit-gradient(
-          linear,
-          left top,
-          right top,
-          color-stop(0, rgb(<?= implode(',', $type_info['type_colour_light']) ?>)),
-          color-stop(1, rgb(<?= implode(',', $type2_info['type_colour_light']) ?>))
-        ) !important;
-        background-image: -o-linear-gradient(right, rgb(<?= implode(',', $type_info['type_colour_light']) ?>) 0%, rgb(<?= implode(',', $type2_info['type_colour_light']) ?>) 100%) !important;
-        background-image: -moz-linear-gradient(right, rgb(<?= implode(',', $type_info['type_colour_light']) ?>) 0%, rgb(<?= implode(',', $type2_info['type_colour_light']) ?>) 100%) !important;
-        background-image: -webkit-linear-gradient(right, rgb(<?= implode(',', $type_info['type_colour_light']) ?>) 0%, rgb(<?= implode(',', $type2_info['type_colour_light']) ?>) 100%) !important;
-        background-image: -ms-linear-gradient(right, rgb(<?= implode(',', $type_info['type_colour_light']) ?>) 0%, rgb(<?= implode(',', $type2_info['type_colour_light']) ?>) 100%) !important;
+        <? if($u_opera): ?>
+          background-image: -o-linear-gradient(right, rgb(<?= implode(',', $type_info['type_colour_light']) ?>) 0%, rgb(<?= implode(',', $type2_info['type_colour_light']) ?>) 100%) !important;
+        <? elseif($u_firefox): ?>
+          background-image: -moz-linear-gradient(right, rgb(<?= implode(',', $type_info['type_colour_light']) ?>) 0%, rgb(<?= implode(',', $type2_info['type_colour_light']) ?>) 100%) !important;
+        <? elseif($u_chrome || $u_safari): ?>
+          background-image: -webkit-gradient(
+            linear,
+            left top,
+            right top,
+            color-stop(0, rgb(<?= implode(',', $type_info['type_colour_light']) ?>)),
+            color-stop(1, rgb(<?= implode(',', $type2_info['type_colour_light']) ?>))
+          ) !important;
+          background-image: -webkit-linear-gradient(right, rgb(<?= implode(',', $type_info['type_colour_light']) ?>) 0%, rgb(<?= implode(',', $type2_info['type_colour_light']) ?>) 100%) !important;
+        <? elseif($u_explorer): ?>
+          background-image: -ms-linear-gradient(right, rgb(<?= implode(',', $type_info['type_colour_light']) ?>) 0%, rgb(<?= implode(',', $type2_info['type_colour_light']) ?>) 100%) !important;
+        <? endif; ?>
         background-image: linear-gradient(to right, rgb(<?= implode(',', $type_info['type_colour_light']) ?>) 0%, rgb(<?= implode(',', $type2_info['type_colour_light']) ?>) 100%) !important;
       }
       <?
-      
       /*
       ?>
       #mmrpg .ability_type_<?= $type_info['type_token'] ?>_<?= $type2_info['type_token'] ?>,
@@ -121,16 +140,16 @@ if (MMRPG_CONFIG_CACHE_INDEXES && file_exists($this_cache_filedir)){
     }
   }
   $temp_css_markup .= ob_get_clean();
-  
+
   // Compress the CSS markup before saving it
   $temp_css_markup = preg_replace('/\s+/', ' ', $temp_css_markup);
   $temp_css_markup = str_replace('; ', ';', $temp_css_markup);
-  
+
   // Update the style cache files
   mmrpg_save_style_markup($this_cache_filedir, $temp_css_markup);
 
 }
-  
+
 // Print out the final generated CSS markup
 echo $temp_css_markup;
 
