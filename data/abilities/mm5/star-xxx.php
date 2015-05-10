54c9368aa5701666831e870806bfe24a1e7aa952
Adrian Marceau <adrian.marceau@gmail.com>
2015-05-10 14:43:16 -0400

    Adding placeholder ability objects for remaining MM5 special weapons.

---
+++ data/abilities/mm5/star-xxx.php
@@ -0,0 +1,49 @@
+<?
+// STAR ???
+$ability = array(
+  'ability_name' => 'Star ???',
+  'ability_token' => 'star-xxx',
+  'ability_game' => 'MM05',
+  'ability_group' => 'MM05/Weapons/037',
+  'ability_master' => 'star-man',
+  'ability_number' => 'DWN-037',
+  'ability_description' => '...',
+  'ability_type' => 'space',
+  'ability_energy' => 4,
+  'ability_damage' => 10,
+  'ability_accuracy' => 90,
+  'ability_function' => function($objects){
+
+    // Extract all objects into the current scope
+    extract($objects);
+
+    // Target the opposing robot
+    $this_ability->target_options_update(array(
+      'frame' => 'shoot',
+      'success' => array(0, 75, 0, 10, $this_robot->print_robot_name().' uses the '.$this_ability->print_ability_name().'!')
+      ));
+    $this_robot->trigger_target($target_robot, $this_ability);
+
+    // Inflict damage on the opposing robot
+    $this_ability->damage_options_update(array(
+      'kind' => 'energy',
+      'kickback' => array(10, 0, 0),
+      'success' => array(1, -55, 0, 10, 'The '.$this_ability->print_ability_name().' hit the target!'),
+      'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_ability_name().' missed the target&hellip;')
+      ));
+    $this_ability->recovery_options_update(array(
+      'kind' => 'energy',
+      'frame' => 'taunt',
+      'kickback' => array(10, 0, 0),
+      'success' => array(1, -35, 0, 10, 'The '.$this_ability->print_ability_name().' was absorbed by the target!'),
+      'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_ability_name().' missed the target&hellip;')
+      ));
+    $energy_damage_amount = $this_ability->ability_damage;
+    $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+
+    // Return true on success
+    return true;
+
+  }
+  );
+?>
