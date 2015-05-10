81b9bfcaa6b734ccaf74c9c73070634574d7c1ee
Adrian Marceau <adrian.marceau@gmail.com>
2015-05-10 12:40:02 -0400

    Added individual ability objects for the elemental Star Force, complimenting the split seen in Robot Cores.

---
+++ data/abilities/item/item-star-time.php
@@ -0,0 +1,19 @@
+<?
+// ITEM : TIME STAR
+$ability = array(
+  'ability_name' => 'Time Star',
+  'ability_token' => 'item-star-time',
+  'ability_game' => 'MMRPG',
+  'ability_group' => 'MMRPG/Items/Time',
+  'ability_class' => 'item',
+  'ability_type' => 'time',
+  'ability_description' => 'A mysterious elemental star that radiates with the Time type energy of a distant planet. Collecting a single one of these items permanently grants +'.MMRPG_SETTINGS_STARS_ATTACKBOOST.' Attack toward and +'.MMRPG_SETTINGS_STARS_DEFENSEBOOST.' Defense against all Time type damage, but only if there are no Dark Elements on the field to suppress it\'s effects.  A certain character is said to be researching these items and would likely trade a respectable amount of Zenny to study one up close.',
+  'ability_energy' => 0,
+  'ability_speed' => 10,
+  'ability_accuracy' => 100,
+  'ability_target' => 'auto',
+  'ability_function' => function($objects){
+    return mmrpg_ability::item_function_core($objects);
+  }
+  );
+?>
