163877f8e6c20c78af38e5c9a74ffe3825ebf832
Adrian Marceau <adrian.marceau@gmail.com>
2015-05-10 12:14:06 -0400

    Added robot objects for the three DS (Dark Soul) fortress bosses - Mega Man DS, Bass DS, and Proto Man DS.  These will replace the dark "Copy" robots from before.

---
+++ data/robots/boss/bass-ds.php
@@ -0,0 +1,61 @@
+<?
+// BASS DS
+$robot = array(
+  'robot_number' => 'DSN-0W1',
+  'robot_game' => 'MMRPG',
+  'robot_name' => 'Bass DS',
+  'robot_token' => 'bass-ds',
+  'robot_gender' => 'male',
+  'robot_class' => 'boss',
+  'robot_image_editor' => 412,
+  'robot_core' => 'shadow',
+  'robot_description' => 'Strongest Challenger Robot',
+  'robot_field' => 'final-destination-2',
+  'robot_energy' => 100,
+  'robot_weapons' => 20,
+  'robot_attack' => 100,
+  'robot_defense' => 100,
+  'robot_speed' => 100,
+  'robot_weaknesses' => array(),
+  'robot_affinities' => array('shadow'),
+  'robot_immunities' => array('flame', 'space'),
+  'robot_abilities' => array(
+    'buster-shot',
+    'attack-boost', 'defense-boost', 'speed-boost', 'energy-boost',
+    'attack-break', 'defense-break', 'speed-break', 'energy-break',
+    'attack-swap', 'defense-swap', 'speed-swap', 'energy-swap',
+    'attack-support', 'defense-support', 'speed-support', 'energy-support',
+    'attack-assault', 'defense-assault', 'speed-assault', 'energy-assault',
+    'attack-shuffle', 'defense-shuffle', 'speed-shuffle', 'energy-shuffle',
+    'attack-mode', 'defense-mode', 'speed-mode', 'repair-mode',
+    'experience-booster', 'recovery-booster', 'damage-booster',
+    'experience-breaker', 'recovery-breaker', 'damage-breaker',
+    'field-support', 'mecha-support',
+    'mega-buster', 'proto-buster', 'roll-buster', 'disco-buster', 'rhythm-buster',
+    'light-buster', 'wily-buster', 'cossack-buster',
+    'mega-ball', 'mega-slide', 'proto-shield', 'proto-strike'
+    ),
+  'robot_rewards' => array(
+    'abilities' => array(
+      array('level' => 0, 'token' => 'buster-shot'),
+      array('level' => 2, 'token' => 'bass-buster'),
+      array('level' => 4, 'token' => 'bass-crush'),
+      array('level' => 8, 'token' => 'bass-baroque'),
+      array('level' => 0, 'token' => 'metal-blade'),
+      array('level' => 0, 'token' => 'bubble-spray'),
+      array('level' => 0, 'token' => 'atomic-fire'),
+      array('level' => 0, 'token' => 'leaf-shield'),
+      array('level' => 0, 'token' => 'air-shooter'),
+      array('level' => 0, 'token' => 'crash-bomber'),
+      array('level' => 0, 'token' => 'flash-stopper'),
+      array('level' => 0, 'token' => 'quick-boomerang')
+      )
+    ),
+  'robot_quotes' => array(
+    'battle_start' => '',
+    'battle_taunt' => '',
+    'battle_victory' => '',
+    'battle_defeat' => ''
+    )
+  );
+?>
---
+++ data/robots/boss/proto-man-ds.php
@@ -0,0 +1,61 @@
+<?
+// PROTO MAN DS
+$robot = array(
+  'robot_number' => 'DSN-0C1',
+  'robot_game' => 'MMRPG',
+  'robot_name' => 'Proto Man DS',
+  'robot_token' => 'proto-man-ds',
+  'robot_gender' => 'male',
+  'robot_class' => 'boss',
+  'robot_field' => 'final-destination-2',
+  'robot_image_editor' => 412,
+  'robot_core' => 'shadow',
+  'robot_description' => '',
+  'robot_energy' => 100,
+  'robot_weapons' => 20,
+  'robot_attack' => 100,
+  'robot_defense' => 100,
+  'robot_speed' => 100,
+  'robot_weaknesses' => array(),
+  'robot_affinities' => array('shadow'),
+  'robot_immunities' => array('impact', 'explode'),
+  'robot_abilities' => array(
+    'buster-shot',
+    'attack-boost', 'defense-boost', 'speed-boost', 'energy-boost',
+    'attack-break', 'defense-break', 'speed-break', 'energy-break',
+    'attack-swap', 'defense-swap', 'speed-swap', 'energy-swap',
+    'attack-support', 'defense-support', 'speed-support', 'energy-support',
+    'attack-assault', 'defense-assault', 'speed-assault', 'energy-assault',
+    'attack-shuffle', 'defense-shuffle', 'speed-shuffle', 'energy-shuffle',
+    'attack-mode', 'defense-mode', 'speed-mode', 'repair-mode',
+    'experience-booster', 'recovery-booster', 'damage-booster',
+    'experience-breaker', 'recovery-breaker', 'damage-breaker',
+    'field-support', 'mecha-support',
+    'mega-buster', 'bass-buster', 'roll-buster', 'disco-buster', 'rhythm-buster',
+    'light-buster', 'wily-buster', 'cossack-buster',
+    'mega-ball', 'mega-slide', 'bass-crush', 'bass-baroque'
+    ),
+  'robot_rewards' => array(
+    'abilities' => array(
+        array('level' => 0, 'token' => 'buster-shot'),
+        array('level' => 2, 'token' => 'proto-buster'),
+        array('level' => 4, 'token' => 'proto-shield'),
+        array('level' => 8, 'token' => 'proto-strike'),
+        array('level' => 0, 'token' => 'drill-blitz'),
+        array('level' => 0, 'token' => 'rain-flush'),
+        array('level' => 0, 'token' => 'bright-burst'),
+        array('level' => 0, 'token' => 'pharaoh-soul'),
+        array('level' => 0, 'token' => 'ring-boomerang'),
+        array('level' => 0, 'token' => 'dust-crusher'),
+        array('level' => 0, 'token' => 'skull-barrier'),
+        array('level' => 0, 'token' => 'dive-missile')
+      )
+    ),
+  'robot_quotes' => array(
+    'battle_start' => '',
+    'battle_taunt' => '',
+    'battle_victory' => '',
+    'battle_defeat' => ''
+    )
+  );
+?>
