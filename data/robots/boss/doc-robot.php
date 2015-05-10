6f6b2ad7a37f8ce1aa9881ee40b6dbea8059a215
Adrian Marceau <adrian.marceau@gmail.com>
2015-05-10 12:15:54 -0400

    Added the robot objects for the King and Doc Robot fortress bosses.

---
+++ data/robots/boss/doc-robot.php
@@ -0,0 +1,46 @@
+<?
+// DOC ROBOT
+$robot = array(
+  'robot_number' => 'SWK-176',
+  'robot_game' => 'MMRPG',
+  'robot_name' => 'Doc Robot',
+  'robot_token' => 'doc-robot',
+  'robot_class' => 'boss',
+  'robot_image_editor' => 412,
+  'robot_core' => 'copy',
+  'robot_description' => 'All-Purpose Battle Robot',
+  'robot_field' => 'final-destination-3',
+  'robot_energy' => 100,
+  'robot_weapons' => 32,
+  'robot_attack' => 100,
+  'robot_defense' => 100,
+  'robot_speed' => 100,
+  'robot_weaknesses' => array(),
+  'robot_resistances' => array(),
+  'robot_affinities' => array(),
+  'robot_abilities' => array(
+    'buster-shot',
+    'attack-boost', 'defense-boost', 'speed-boost', 'energy-boost',
+    'attack-break', 'defense-break', 'speed-break', 'energy-break',
+    'attack-swap', 'defense-swap', 'speed-swap', 'energy-swap',
+    'attack-support', 'defense-support', 'speed-support', 'energy-support',
+    'attack-assault', 'defense-assault', 'speed-assault', 'energy-assault',
+    'attack-shuffle', 'defense-shuffle', 'speed-shuffle', 'energy-shuffle',
+    'attack-mode', 'defense-mode', 'speed-mode', 'repair-mode',
+    'experience-booster', 'recovery-booster', 'damage-booster',
+    'experience-breaker', 'recovery-breaker', 'damage-breaker',
+    'field-support', 'mecha-support',
+    ),
+  'robot_rewards' => array(
+    'abilities' => array(
+      array('level' => 0, 'token' => 'copy-shot')
+      )
+    ),
+  'robot_quotes' => array(
+    'battle_start' => '',
+    'battle_taunt' => '',
+    'battle_victory' => '',
+    'battle_defeat' => ''
+    )
+  );
+?>
