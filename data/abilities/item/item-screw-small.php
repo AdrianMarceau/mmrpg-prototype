ee3db843cf0ae53955c99970fe6628938df1ccce
Adrian Marceau <adrian.marceau@gmail.com>
2015-06-29 22:10:34 -0400

    Added subclass of treasure for the two screws.  They will have no other purpose than to sell.

--- data/abilities/item/item-screw-small.php
+++ data/abilities/item/item-screw-small.php
@@ -6,6 +6,7 @@ $ability = array(
   'ability_game' => 'MM07',
   'ability_group' => 'MM00/Items/Screws',
   'ability_class' => 'item',
+  'ability_subclass' => 'treasure',
   'ability_description' => 'A small metal screw dropped by a defeated mecha.  This item is loved by a certain character and can be traded in for a moderate amount of Zenny. ',
   'ability_function' => function($objects){
 
