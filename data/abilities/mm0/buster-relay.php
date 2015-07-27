1bc671410d2295ec06df2e8e75f9a72ad2d29ce6
Adrian Marceau <adrian.marceau@gmail.com>
2015-07-26 21:40:00 -0400

    Updated buster relay with new mechanics.
    Revised functionality of Buster Relay ability so it passes all charges in one turn, one at a time, instead of only the oldest.  Also made it so that charges are intensified with each passing and automatically merge with existing charges of the same name (also intensifying power).

--- data/abilities/mm0/buster-relay.php
+++ data/abilities/mm0/buster-relay.php
@@ -6,7 +6,7 @@ $ability = array(
   'ability_game' => 'MM00',
   'ability_group' => 'MM00/Weapons/T0',
   'ability_image_sheets' => 0,
-  'ability_description' => 'The user relays a buster charge they are currently holding to another robot on their team, transferring any stat changes or elemental boosts to the new robot.',
+  'ability_description' => 'The user relays a buster charge they are currently holding to another robot on their side of the field, transferring any stat changes or elemental boosts to the new robot. Charges transferred this way intensify in power with each passing and will automatically merge with others of the same kind and element.',
   'ability_energy' => 0,
   'ability_accuracy' => 100,
   'ability_target' => 'select_this',
@@ -22,21 +22,10 @@ $ability = array(
       ));
     $this_robot->trigger_target($target_robot, $this_ability);
 
-    // Loop through this robot's attachments, looking for a buster
+    // Define the default ability info vars
     $relay_buster_token = '';
-    $relay_buster_object = false;
     $relay_buster_info = array();
-    if (!empty($this_robot->robot_attachments)){
-      foreach ($this_robot->robot_attachments AS $attachment_token => $attachment_info){
-        // If this is a buster charge of any kind, save its info and break
-        if (preg_match('/^([-_a-z0-9]+)-buster$/i', $attachment_token)){
-          $relay_buster_token = $attachment_token;
-          $relay_buster_info = $attachment_info;
-          $relay_buster_object = new mmrpg_ability($this_battle, $this_player, $this_robot, $relay_buster_info);
-          break;
-        }
-      }
-    }
+    $relay_buster_object = false;
 
     // Automatically fail if the user has targetted itself
     if ($target_robot->robot_id == $this_robot->robot_id){
@@ -51,8 +40,8 @@ $ability = array(
       $this_robot->trigger_target($this_robot, $this_ability);
 
     }
-    // Automatically fila if there was no buster charge to transfer
-    elseif (empty($relay_buster_token)){
+    // Automatically fail if there was no buster charge to transfer
+    elseif (empty($this_robot->robot_attachments)){
 
       // Target this robot's self and show the ability failing
       $this_ability->target_options_update(array(
@@ -64,30 +53,73 @@ $ability = array(
       $this_robot->trigger_target($this_robot, $this_ability);
 
     }
-    // Ensure there was a buster charge to pass on to the target
-    else {
+    // Otherwise if not empty, loop through this robot's attachments, looking for a buster
+    elseif (!empty($this_robot->robot_attachments)){
+      $attachment_key = 0;
+      foreach ($this_robot->robot_attachments AS $attachment_token => $attachment_info){
+        // If this is a buster charge of any kind, move it to the target robot
+        if (preg_match('/^([-_a-z0-9]+)-buster$/i', $attachment_token)){
 
-      // Remove this attachment from the source robot
-      unset($this_robot->robot_attachments[$relay_buster_token]);
-      $this_robot->update_session();
+          // Collect the information for this buster ability
+          $relay_buster_token = $attachment_token;
+          $relay_buster_info = $attachment_info;
+          $relay_buster_object = new mmrpg_ability($this_battle, $this_player, $this_robot, $relay_buster_info);
 
-      // Append this attachment to the new target robot
-      $target_robot->robot_frame = 'taunt';
-      $target_robot->robot_attachments[$relay_buster_token] = $relay_buster_info;
-      $target_robot->update_session();
+          // Remove this attachment from the source robot
+          unset($this_robot->robot_attachments[$relay_buster_token]);
+          $this_robot->update_session();
 
-      // Trigger the robot target and show the buster charge moving
+          // If this buster has any boosters or breakers, intensify their values
+          foreach ($relay_buster_info AS $field => $value){
+            if (preg_match('/^attachment_(damage|recovery)(_input|_output)?(_booster|_breaker)(_[a-z]+)?$/i', $field)){
+              $new_value = $value;
+              if (!isset($relay_buster_info[$field.'_base'])){ $relay_buster_info[$field.'_base'] = $value; }
+              $base = $relay_buster_info[$field.'_base'];
+              $new_value = $new_value * $base;
+              if (isset($target_robot->robot_attachments[$relay_buster_token][$field])){
+                $merge = $target_robot->robot_attachments[$relay_buster_token][$field];
+                $new_value = $new_value * $merge;
+              }
+              $relay_buster_info[$field] = $new_value;
+            }
+          }
+
+          // Append this attachment to the new target robot
+          $target_robot->robot_frame = $attachment_key % 2 == 0 ? 'defend' : 'taunt';
+          $target_robot->robot_attachments[$relay_buster_token] = $relay_buster_info;
+          $target_robot->update_session();
+
+          // Trigger the robot target and show the buster charge moving
+          $this_ability->target_options_update(array(
+            'frame' => $attachment_key % 2 == 0 ? 'taunt' : 'summon',
+            'success' => array(9, 0, 0, -10,
+              'The '.$relay_buster_object->print_ability_name().' charge was transferred to '.$target_robot->print_robot_name().'!<br />'
+              )
+            ));
+          $this_robot->trigger_target($target_robot, $this_ability);
+
+          // Reset the target robot's frame
+          $target_robot->robot_frame = 'base';
+          $target_robot->update_session();
+
+          // Increment the attachment key
+          $attachment_key++;
+
+        }
+      }
+    }
+
+    // Automatically fail if there was no buster charge to transfer
+    if (empty($relay_buster_token)){
+
+      // Target this robot's self and show the ability failing
       $this_ability->target_options_update(array(
-        'frame' => 'defend',
+        'frame' => 'summon',
         'success' => array(9, 0, 0, -10,
-          'The '.$relay_buster_object->print_ability_name().' charge was transferred to '.$target_robot->print_robot_name().'!<br />'
+          'But there weren\'t any buster charges to transfer&hellip;<br />'
           )
         ));
-      $this_robot->trigger_target($target_robot, $this_ability);
-
-      // Reset the target robot's frame
-      $target_robot->robot_frame = 'base';
-      $target_robot->update_session();
+      $this_robot->trigger_target($this_robot, $this_ability);
 
     }
 
