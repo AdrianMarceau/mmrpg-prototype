e6cc5b528befe806c6faedf175c10faa54f9019f
Adrian Marceau <adrian.marceau@gmail.com>
2015-12-20 11:44:00 -0500

    Updated stardroid ability file to remove garbage placeholder code.

--- data/abilities/stardroid/bubble-bomb.php
+++ data/abilities/stardroid/bubble-bomb.php
@@ -18,13 +18,10 @@ $ability = array(
   'ability_target' => 'select_target',
   'ability_function' => function($objects){
 
-    // Extract all objects into the current scope
-    extract($objects);
-
     // Target the opposing robot
     $this_ability->target_options_update(array(
       'frame' => 'throw',
-      'success' => array(0, 85, 35, 10, $this_robot->print_robot_name().' thows a '.$this_ability->print_ability_name().'!'),
+      'success' => array(0, 85, 35, 10, $this_robot->print_name().' thows a '.$this_ability->print_name().'!'),
       ));
     $this_robot->trigger_target($target_robot, $this_ability);
 
@@ -32,16 +29,16 @@ $ability = array(
     $this_ability->damage_options_update(array(
       'kind' => 'energy',
       'kickback' => array(20, 0, 0),
-      'success' => array(2, -10, -10, 10, 'The '.$this_ability->print_ability_name().' burst on contact!'),
-      'failure' => array(1, -65, -10, -10, 'The '.$this_ability->print_ability_name().' missed&hellip;')
+      'success' => array(2, -10, -10, 10, 'The '.$this_ability->print_name().' burst on contact!'),
+      'failure' => array(1, -65, -10, -10, 'The '.$this_ability->print_name().' missed&hellip;')
       ));
     $this_ability->recovery_options_update(array(
       'kind' => 'energy',
       'frame' => 'taunt',
       'frame' => 'taunt',
       'kickback' => array(10, 0, 0),
-      'success' => array(2, -10, -10, 10, 'The '.$this_ability->print_ability_name().' burst on contact!'),
-      'failure' => array(1, -65, -10, -10, 'The '.$this_ability->print_ability_name().' missed&hellip;')
+      'success' => array(2, -10, -10, 10, 'The '.$this_ability->print_name().' burst on contact!'),
+      'failure' => array(1, -65, -10, -10, 'The '.$this_ability->print_name().' missed&hellip;')
       ));
     $energy_damage_amount = $this_ability->ability_damage;
     $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
@@ -49,14 +46,14 @@ $ability = array(
     // Randomly inflict a speed break on critical chance 75%
     if ($target_robot->robot_status != 'disabled'
       && $this_ability->ability_results['this_result'] != 'failure' && $this_ability->ability_results['this_amount'] > 0
-      && $this_battle->critical_chance(50)){
+      && rpg_functions::critical_chance(50)){
       // Decrease the target robot's speed stat
       $this_ability->damage_options_update(array(
         'kind' => 'attack',
         'percent' => true,
         'frame' => 'defend',
         'kickback' => array(0, 0, 0),
-        'success' => array(1, -10, -10, -10, $target_robot->print_robot_name().'&#39;s weapons were damaged!'),
+        'success' => array(1, -10, -10, -10, $target_robot->print_name().'&#39;s weapons were damaged!'),
         'failure' => array(1, -65, -10, -10, '')
         ));
       $this_ability->recovery_options_update(array(
@@ -64,7 +61,7 @@ $ability = array(
         'percent' => true,
         'frame' => 'taunt',
         'kickback' => array(0, 0, 0),
-        'success' => array(1, -10, -10, -10, $target_robot->print_robot_name().'&#39;s weapons improved!'),
+        'success' => array(1, -10, -10, -10, $target_robot->print_name().'&#39;s weapons improved!'),
         'failure' => array(1, -65, -10, -9999, '')
         ));
       $attack_damage_amount = ceil($target_robot->robot_attack * ($this_ability->ability_damage2 / 100));
