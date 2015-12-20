64881bb2381f94329a7dbaebb4245324f31c7e9e
Adrian Marceau <adrian.marceau@gmail.com>
2015-12-20 11:31:41 -0500

    Updated defense stat ability files with recent changes to parent objects.

--- data/abilities/xdefense/defense-swap.php
+++ data/abilities/xdefense/defense-swap.php
@@ -17,19 +17,19 @@ $ability = array(
     $this_attachment_token = 'ability_'.$this_ability->ability_token;
     $this_attachment_info = array(
       'class' => 'ability',
+      'ability_id' => $this_ability->ability_id,
       'ability_token' => $this_ability->ability_token,
       'ability_frame' => 0,
       'ability_frame_offset' => array('x' => 0, 'y' => 0, 'z' => -10)
       );
 
     // Attach this ability to the target temporarily
-    $target_robot->robot_attachments[$this_attachment_token] = $this_attachment_info;
-    $target_robot->update_session();
+    $target_robot->set_attachment($this_attachment_token, $this_attachment_info);
 
     // Target this robot's self
     $this_ability->target_options_update(array(
       'frame' => 'summon',
-      'success' => array(0, 0, 10, -10, $this_robot->print_robot_name().' triggered a '.$this_ability->print_ability_name().' with '.$target_robot->print_robot_name().'!')
+      'success' => array(0, 0, 10, -10, $this_robot->print_name().' triggered a '.$this_ability->print_name().' with '.$target_robot->print_name().'!')
       ));
     $this_robot->trigger_target($this_robot, $this_ability);
 
@@ -53,16 +53,15 @@ $ability = array(
         list($temp_defense, $temp_base_defense) = explode('/', $temp_target_defense);
 
         // Update this robot's values with the random data
-        $this_robot->robot_defense = $temp_defense;
-        $this_robot->robot_base_defense = $temp_base_defense;
-        $this_robot->update_session();
+        $this_robot->set_defense($temp_defense);
+        $this_robot->set_base_defense($temp_base_defense);
 
         // Target this robot's self
         $is_her = in_array($this_robot->robot_token, array('roll', 'disco', 'rhythm', 'splash-woman')) ? true : false;
         $is_mecha = $this_robot->robot_class == 'mecha' ? true : false;
         $this_ability->target_options_update(array(
           'frame' => 'defend',
-          'success' => array(9, 0, 10, -10, $this_robot->print_robot_name().'&#39;s defense stats were modified&hellip;<br /> '.($is_her ? 'Her' : ($is_mecha ? 'Its' : 'His')).' new defense stats are '.$this_robot->print_robot_defense().' / '.$this_robot->print_robot_base_defense().'!')
+          'success' => array(9, 0, 10, -10, $this_robot->print_name().'&#39;s defense stats were modified&hellip;<br /> '.($is_her ? 'Her' : ($is_mecha ? 'Its' : 'His')).' new defense stats are '.$this_robot->print_defense().' / '.$this_robot->print_robot_base_defense().'!')
           ));
         $this_robot->trigger_target($this_robot, $this_ability, array('prevent_default_text' => true));
 
@@ -73,7 +72,7 @@ $ability = array(
         // Target this robot's self and show the ability failing
         $this_ability->target_options_update(array(
           'frame' => 'defend',
-          'success' => array(9, 0, 0, -10, $this_robot->print_robot_name().'&#39;s defense stats were not affected&hellip;')
+          'success' => array(9, 0, 0, -10, $this_robot->print_name().'&#39;s defense stats were not affected&hellip;')
           ));
         $this_robot->trigger_target($this_robot, $this_ability, array('prevent_default_text' => true));
 
@@ -116,16 +115,13 @@ $ability = array(
     // If this ability targeting is allowed
     if ($temp_allow_targeting){
       // Update this ability's targetting setting
-      $this_ability->ability_target = 'select_target';
+      $this_ability->set_target('select_target');
     }
     // Else if the ability attachment is not there, change the target back to auto
     else {
       // Update this ability's targetting setting
-      $this_ability->ability_target = 'auto';
+      $this_ability->set_target('auto');
     }
-
-    // Update the ability session
-    $this_ability->update_session();
 
     // Return true on success
     return true;
