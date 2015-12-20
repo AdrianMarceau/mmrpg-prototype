606148c90f0a9c8675fe44964b998a7fe2c0f9e9
Adrian Marceau <adrian.marceau@gmail.com>
2015-12-20 11:47:02 -0500

    Updated MM7 ability files to remove garbage placeholder code.

--- data/abilities/mm7/slash-claw.php
+++ data/abilities/mm7/slash-claw.php
@@ -1,14 +1,14 @@
 <?
-// TURBO WHEEL
+// Slash Claw
 $ability = array(
-  'ability_name' => 'Turbo Wheel',
-  'ability_token' => 'turbo-wheel',
+  'ability_name' => 'Slash Claw',
+  'ability_token' => 'slash-claw',
   'ability_game' => 'MM07',
-  'ability_group' => 'MM07/Weapons/056',
-  'ability_master' => 'turbo-man',
-  'ability_number' => 'DWN-056',
+  'ability_group' => 'MM07/Weapons/054',
+  'ability_master' => 'slash-man',
+  'ability_number' => 'DWN-054',
   'ability_description' => '...',
-  'ability_type' => 'swift',
+  'ability_type' => 'cutter',
   'ability_energy' => 4,
   'ability_damage' => 10,
   'ability_accuracy' => 90,
