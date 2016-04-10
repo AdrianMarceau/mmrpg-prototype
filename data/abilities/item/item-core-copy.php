ce274030ce4fff75828b690e5585025a2f35a3df
Adrian Marceau <adrian.marceau@gmail.com>
2016-04-10 15:07:48 -0400

    Updated all robot cores with new in-battle functionality - removed field multipliers and added throwing.
    Robot cores no longer boost field multipliers or change the colour of Copy core users.  Instead, robot cores can now be thrown at any target on the field for a set amount of percentage-based elemental damage.  Field and starforce multipliers do not apply, but weaknesses/resistances/etc. do apply.  With this in mind, the player could theoretically take down any target by throwing 10 neutrally effect robot cores, 5 super effective robot cores, or 20 not very effective robot cores at the opponent.

--- data/abilities/item/item-core-copy.php
+++ data/abilities/item/item-core-copy.php
@@ -6,13 +6,13 @@ $ability = array(
   'ability_game' => 'MMRPG',
   'ability_class' => 'item',
   'ability_type' => 'copy',
-  'ability_description' => 'A mysterious elemental core that radiates with the Copy type energy of a defeated robot master. When used in battle, this item increases the field multiplier for Copy type moves by {RECOVERY}%. This item can also be used to return a Copy Core robot to its base form.',
+  'ability_description' => 'A mysterious elemental core that radiates with the Copy type energy of a defeated robot master. When used in battle, this item can be thrown at any target to deal Copy type damage of up to {DAMAGE}%!',
   'ability_energy' => 0,
   'ability_speed' => 10,
-  'ability_recovery' => 100,
-  'ability_recovery_percent' => true,
+  'ability_damage' => 20,
+  'ability_damage_percent' => true,
   'ability_accuracy' => 100,
-  'ability_target' => 'auto',
+  'ability_target' => 'select_target',
   'ability_function' => function($objects){
     return mmrpg_ability::item_function_core($objects);
   }
