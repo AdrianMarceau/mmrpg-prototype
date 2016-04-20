03e1f4dcfaf7d432f1a8f32b4f5257b2a5192a22
Adrian Marceau <adrian.marceau@gmail.com>
2016-04-19 21:46:25 -0400

    Updated leaderboard section of website to fix (sub) page links with fixes imported from MMRPG World.
    The numbered links for the different pages of the leaderboard were overflowing over each other and other links so they had to be reformatted and styled.

--- pages/page.leaderboard_index.php
+++ pages/page.leaderboard_index.php
@@ -40,159 +40,162 @@ ob_start();
 ?>
 <h2 class="subheader thread_name field_type_<?= MMRPG_SETTINGS_CURRENT_FIELDTYPE ?>">Leaderboard Players Index</h2>
 <div class="subbody" style="margin-bottom: 6px;">
-  
-  <p class="text">The <strong>Mega Man RPG Prototype</strong> currently has <?= !empty($this_leaderboard_count) ? ($this_leaderboard_count == 1 ? '1 player' : $this_leaderboard_count.' players') : 0 ?> and that number is growing all the time.  Throughout the course of the game, players collect Battle Points on completion of missions and those points build up to unlock new abilities and other new content.  Not all players are created equal, however, and some clearly stand above the rest in terms of their commitment to the game and their skill at exploiting the battle system's mechanics.  In the spirit of competition, all players have been ranked by their total Battle Point scores and listed from highest to lowest.  Use the numbered links at the top and bottom of the page to navigate and <a href="contact/">contact me</a> if you have any questions or concerns.</p>
-  <? if(!empty($this_leaderboard_online_players)):?>
-    <p class="event text" style="min-height: 1px; text-align: right; font-size: 10px; line-height: 13px; margin-top: 30px; padding-bottom: 5px;">
-      <span><strong style="display: block; text-decoration: underline; margin-bottom: 6px;">Online Players</strong></span>
-      <? foreach ($this_leaderboard_online_players AS $key => $info){
-        if (empty($info['image'])){ $info['image'] = 'robots/mega-man/40'; }
-        list($path, $token, $size) = explode('/', $info['image']);
-        $frame = $info['placeint'] <= 3 ? 'victory' : 'base';
-        if ($key > 0 && $key % 5 == 0){ echo '<br />'; }
-        echo ' <a data-playerid="'.$info['id'].'" class="player_type player_type_'.$info['colour'].'" href="leaderboard/'.$info['token'].'/" style="text-decoration: none; line-height: 20px; padding-right: 12px; margin: 0 0 0 6px;">';
-          echo '<span style="pointer-events: none; display: inline-block; width: 34px; height: 14px; position: relative;"><span class="sprite sprite_'.$size.'x'.$size.' sprite_'.$size.'x'.$size.'_'.$frame.'" style="margin: 0; position: absolute; left: '.($size == 40 ? -4 : -26).'px; bottom: 0; background-image: url(images/'.$path.'/'.$token.'/sprite_left_'.$size.'x'.$size.'.png?'.MMRPG_CONFIG_CACHE_DATE.');">&nbsp;</span></span>';
-          echo '<span style="vertical-align: top; line-height: 18px;">'.strip_tags($info['place']).' : '.$info['name'].'</span>';
-        echo '</a>';
-      } ?>
-    </p>
-  <? endif; ?>
-  
+
+    <p class="text">The <strong>Mega Man RPG Prototype</strong> currently has <?= !empty($this_leaderboard_count) ? ($this_leaderboard_count == 1 ? '1 player' : $this_leaderboard_count.' players') : 0 ?> and that number is growing all the time.  Throughout the course of the game, players collect Battle Points on completion of missions and those points build up to unlock new abilities and other new content.  Not all players are created equal, however, and some clearly stand above the rest in terms of their commitment to the game and their skill at exploiting the battle system's mechanics.  In the spirit of competition, all players have been ranked by their total Battle Point scores and listed from highest to lowest.  Use the numbered links at the top and bottom of the page to navigate and <a href="contact/">contact me</a> if you have any questions or concerns.</p>
+    <? if(!empty($this_leaderboard_online_players)):?>
+        <p class="event text" style="min-height: 1px; text-align: right; font-size: 10px; line-height: 13px; margin-top: 30px; padding-bottom: 5px;">
+            <span><strong style="display: block; text-decoration: underline; margin-bottom: 6px;">Online Players</strong></span>
+            <? foreach ($this_leaderboard_online_players AS $key => $info){
+                if (empty($info['image'])){ $info['image'] = 'robots/mega-man/40'; }
+                list($path, $token, $size) = explode('/', $info['image']);
+                $frame = $info['placeint'] <= 3 ? 'victory' : 'base';
+                if ($key > 0 && $key % 5 == 0){ echo '<br />'; }
+                echo ' <a data-playerid="'.$info['id'].'" class="player_type player_type_'.$info['colour'].'" href="leaderboard/'.$info['token'].'/" style="text-decoration: none; line-height: 20px; padding-right: 12px; margin: 0 0 0 6px;">';
+                    echo '<span style="pointer-events: none; display: inline-block; width: 34px; height: 14px; position: relative;"><span class="sprite sprite_'.$size.'x'.$size.' sprite_'.$size.'x'.$size.'_'.$frame.'" style="margin: 0; position: absolute; left: '.($size == 40 ? -4 : -26).'px; bottom: 0; background-image: url(images/'.$path.'/'.$token.'/sprite_left_'.$size.'x'.$size.'.png?'.MMRPG_CONFIG_CACHE_DATE.');">&nbsp;</span></span>';
+                    echo '<span style="vertical-align: top; line-height: 18px;">'.strip_tags($info['place']).' : '.$info['name'].'</span>';
+                echo '</a>';
+            } ?>
+        </p>
+    <? endif; ?>
+
 </div>
 
 <div class="leaderboard">
-  <div class="wrapper">
-  <?
-  
-  // Print out the generated leaderboard markup
-  //echo $this_leaderboard_markup;
-  //die('<pre>'.print_r($this_leaderboard_markup, true).'</pre>');
-  //die('$this_start_key = '.$this_start_key.'; $this_display_limit = '.$this_display_limit.'; ');
-  if (!empty($this_leaderboard_markup)){
-    if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-    
-    // COLLECT DATA
-    
-    // Start the output buffer and start looping
-    if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-    ob_start();
-    $last_key = 0;
-    $this_start_key = $_GET['start'];
-    $this_display_limit = $_GET['limit'];
-    foreach ($this_leaderboard_markup AS $key => $leaderboard_markup){
-      // If this key is below the start limit, don't display
-      if ($key < $this_start_key){ continue; }
-      // Update the last key variable
-      $last_key = $key;
-      // Display this save file's markup
-      echo $leaderboard_markup;
-      // Only show listings up to the display limit
-      if ($key + 1 >= $this_display_limit){ break; }
-    }
-    // Define the start key for the next batch of players
-    $start_key = $last_key + 1;
-    
-    // Collect the page listing markup
-    if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-    $pagelisting_markup = trim(ob_get_clean());
-    
-    // HEADER PAGE LINKS
-    
-    // Print out the opening tag for the container dig
-    echo '<div class="container" style="overflow: hidden; padding-bottom: 0; margin-top: -5px; margin-bottom: -32px; ">';
-    
-      // Define the variable to hold the pagelink markup
-      $pagelink_markup = '';
-      ob_start();
-      
-      // If we're not on the first page, create a link to go back one
-      if ($this_display_limit > $this_display_limit_default){
-        if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-        $new_display_limit = $this_display_limit - $this_display_limit_default;
-        $new_start_key = $start_key - $this_display_limit_default - $this_display_limit_default;
-        if ($new_display_limit < $this_display_limit_default){ $new_display_limit = 0; }
-        if ($new_start_key < 0){ $new_start_key = 0; }
-        $previous_page_num = $this_current_num - 1;
-        echo '<a class="more" name="more_link" style="float: left;" href="leaderboard/'.$previous_page_num.'/" >&laquo; Previous Page</a>';
-      }
-      // If not displaying all players, create a link to show more
-      if ($this_display_limit < $this_leaderboard_count){
-        if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-        $new_display_limit = $this_display_limit + $this_display_limit_default;
-        if ($new_display_limit > $this_leaderboard_count){ $new_display_limit = $this_leaderboard_count; }
-        $next_page_num = $this_current_num + 1;
-        echo '<a class="more" name="more_link" style="float: right;" href="leaderboard/'.$next_page_num.'/" >Next Page &raquo;</a>';
-      }
-      // If we're already on the last page, display a link to go to the first
-      if ($this_display_limit >= $this_leaderboard_count){
-        if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-        echo '<a class="more" name="more_link" style="float: right;" href="leaderboard/">First Page &raquo;</a>';
-      }
-      
-      // Collect the pagelink markup
-      if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-      $pagelink_markup = trim(ob_get_clean());
-      echo $pagelink_markup;
-    
-    // Print out the closing container div
-    echo '</div>';
-    
-    
-    // MAIN LEADEBOARD AREA
-    
-    // Print out the opening tag for the container dig
-    echo '<div class="container container_numbers" style="text-align: center; margin-bottom: -32px;">';
-    
-      // Create links for all the page numbers one by one
-      if ($this_leaderboard_count > $this_display_limit_default){
-        if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-        // Define the start and end pages based on total numbers
-        $first_page_num = 1;
-        $last_page_num = ceil($this_leaderboard_count / $this_display_limit_default);
-        for ($this_page_num = $first_page_num; $this_page_num <= $last_page_num; $this_page_num++){
-          if ($this_current_num == $this_page_num){ echo '<a class="more number number_active '.(in_array($this_page_num, $this_leaderboard_online_pages) ? 'field_type field_type_nature' : '').'" name="page_link">'.$this_page_num.'</a>'; }
-          else { echo '<a class="more number '.(in_array($this_page_num, $this_leaderboard_online_pages) ? 'field_type field_type_nature' : '').'" name="page_link" href="leaderboard/'.($this_page_num > 1 ? $this_page_num.'/' : '').'" >'.$this_page_num.'</a>'; }
-          
-        }
-      }
-      
-      // Display the pregenerated pagelisting data
-      if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-      echo $pagelisting_markup;
-      
-      // Create links for all the page numbers one by one
-      if ($this_leaderboard_count > $this_display_limit_default){
-        if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-        // Define the start and end pages based on total numbers
-        $first_page_num = 1;
-        $last_page_num = ceil($this_leaderboard_count / $this_display_limit_default);
-        for ($this_page_num = $first_page_num; $this_page_num <= $last_page_num; $this_page_num++){
-          if ($this_current_num == $this_page_num){ echo '<a class="more number number_active '.(in_array($this_page_num, $this_leaderboard_online_pages) ? 'field_type field_type_nature' : '').'" name="page_link">'.$this_page_num.'</a>'; }
-          else { echo '<a class="more number '.(in_array($this_page_num, $this_leaderboard_online_pages) ? 'field_type field_type_nature' : '').'" name="page_link" href="leaderboard/'.($this_page_num > 1 ? $this_page_num.'/' : '').'" >'.$this_page_num.'</a>'; }
-        }
-      }
-    
-    // Print out the closing container div
-    echo '</div>';
-    
-    // FOOTER PAGE LINKS
-    
-    // Print out the opening tag for the container dig
-    if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-    echo '<div class="container" style="overflow: hidden; padding-bottom: 10px;">';
-    // Display the pregenerated pagelink markup
-    if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-    echo $pagelink_markup;
-    // Print out the closing container div
-    if (MMRPG_CONFIG_DEBUG_MODE){ mmrpg_debug_checkpoint(__FILE__, __LINE__);  }
-    echo '</div>';
-    
-    
-  }
+    <div class="wrapper">
+    <?
 
-  ?>
-  
-  </div>
+    // Print out the generated leaderboard markup
+    //echo $this_leaderboard_markup;
+    //die('<pre>'.print_r($this_leaderboard_markup, true).'</pre>');
+    //die('$this_start_key = '.$this_start_key.'; $this_display_limit = '.$this_display_limit.'; ');
+    if (!empty($this_leaderboard_markup)){
+        // COLLECT DATA
+
+        // Start the output buffer and start looping
+        ob_start();
+        $last_key = 0;
+        $this_start_key = $_GET['start'];
+        $this_display_limit = $_GET['limit'];
+        foreach ($this_leaderboard_markup AS $key => $leaderboard_markup){
+          // If this key is below the start limit, don't display
+          if ($key < $this_start_key){ continue; }
+          // Update the last key variable
+          $last_key = $key;
+          // Display this save file's markup
+          echo $leaderboard_markup;
+          // Only show listings up to the display limit
+          if ($key + 1 >= $this_display_limit){ break; }
+        }
+        // Define the start key for the next batch of players
+        $start_key = $last_key + 1;
+
+        // Collect the page listing markup
+        $pagelisting_markup = trim(ob_get_clean());
+
+        // HEADER PAGE LINKS
+
+        // Print out the opening tag for the container dig
+        echo '<div class="container" style="overflow: hidden; padding-bottom: 0; margin-top: -5px; margin-bottom: -32px; ">';
+
+          // Define the variable to hold the pagelink markup
+          $pagelink_markup = '';
+          ob_start();
+
+          // If we're not on the first page, create a link to go back one
+          if ($this_display_limit > $this_display_limit_default){
+            $new_display_limit = $this_display_limit - $this_display_limit_default;
+            $new_start_key = $start_key - $this_display_limit_default - $this_display_limit_default;
+            if ($new_display_limit < $this_display_limit_default){ $new_display_limit = 0; }
+            if ($new_start_key < 0){ $new_start_key = 0; }
+            $previous_page_num = $this_current_num - 1;
+            echo '<a class="more" name="more_link" style="float: left;" href="leaderboard/'.$previous_page_num.'/" >&laquo; Back</a>';
+          }
+          // If not displaying all players, create a link to show more
+          if ($this_display_limit < $this_leaderboard_count){
+            $new_display_limit = $this_display_limit + $this_display_limit_default;
+            if ($new_display_limit > $this_leaderboard_count){ $new_display_limit = $this_leaderboard_count; }
+            $next_page_num = $this_current_num + 1;
+            echo '<a class="more" name="more_link" style="float: right;" href="leaderboard/'.$next_page_num.'/" >Next &raquo;</a>';
+          }
+          // If we're already on the last page, display a link to go to the first
+          if ($this_display_limit >= $this_leaderboard_count){
+            echo '<a class="more" name="more_link" style="float: right;" href="leaderboard/">First &raquo;</a>';
+          }
+
+          // Collect the pagelink markup
+          $pagelink_markup = trim(ob_get_clean());
+          echo $pagelink_markup;
+
+        // Print out the closing container div
+        echo '</div>';
+
+
+        // MAIN LEADEBOARD AREA
+
+        // Print out the opening tag for the container dig
+        echo '<div class="container container_numbers" style="text-align: center; margin-bottom: -32px;">';
+
+          // Define the start and end pages based on total numbers
+          $display_range = 2;
+          $display_range2 = 8;
+          $first_page_num = 1;
+          $last_page_num = ceil($this_leaderboard_count / $this_display_limit_default);
+
+          // Create links for all the page numbers one by one
+          if ($this_leaderboard_count > $this_display_limit_default){
+            // Loop through and generate the page number markup
+            for ($this_page_num = $first_page_num; $this_page_num <= $last_page_num; $this_page_num++){
+              $show_page_num = false;
+              if ($this_page_num == $this_current_num){ $show_page_num = true; }
+              elseif ($this_page_num <= $this_current_num + $display_range && $this_page_num >= $this_current_num - $display_range){ $show_page_num = true; }
+              elseif ($this_page_num <= $first_page_num + $display_range2 && $this_page_num >= $first_page_num - $display_range2){ $show_page_num = true; }
+              elseif ($this_page_num <= $last_page_num + $display_range2 && $this_page_num >= $last_page_num - $display_range2){ $show_page_num = true; }
+              $show_num_text = $show_page_num ? $this_page_num : '.';
+              $show_num_type = $show_page_num ? 'number' : 'bullet';
+              $show_online = in_array($this_page_num, $this_leaderboard_online_pages) ? true : false;
+              if ($this_current_num == $this_page_num){ echo '<a class="more '.$show_num_type.' active '.($show_online ? 'field_type field_type_nature' : '').'" name="page_link">'.$show_num_text.'</a>'; }
+              else { echo '<a class="more '.$show_num_type.' '.($show_online ? 'field_type field_type_nature' : '').'" name="page_link" href="leaderboard/'.($this_page_num > 1 ? $this_page_num.'/' : '').'" >'.$show_num_text.'</a>'; }
+            }
+          }
+
+          // Display the pregenerated pagelisting data
+          echo $pagelisting_markup;
+
+          // Create links for all the page numbers one by one
+          if ($this_leaderboard_count > $this_display_limit_default){
+            // Loop through and generate the page number markup
+            for ($this_page_num = $first_page_num; $this_page_num <= $last_page_num; $this_page_num++){
+              $show_page_num = false;
+              if ($this_page_num == $this_current_num){ $show_page_num = true; }
+              elseif ($this_page_num <= $this_current_num + $display_range && $this_page_num >= $this_current_num - $display_range){ $show_page_num = true; }
+              elseif ($this_page_num <= $first_page_num + $display_range2 && $this_page_num >= $first_page_num - $display_range2){ $show_page_num = true; }
+              elseif ($this_page_num <= $last_page_num + $display_range2 && $this_page_num >= $last_page_num - $display_range2){ $show_page_num = true; }
+              $show_num_text = $show_page_num ? $this_page_num : '.';
+              $show_num_type = $show_page_num ? 'number' : 'bullet';
+              $show_online = in_array($this_page_num, $this_leaderboard_online_pages) ? true : false;
+              if ($this_current_num == $this_page_num){ echo '<a class="more '.$show_num_type.' active '.($show_online ? 'field_type field_type_nature' : '').'" name="page_link">'.$show_num_text.'</a>'; }
+              else { echo '<a class="more '.$show_num_type.' '.($show_online ? 'field_type field_type_nature' : '').'" name="page_link" href="leaderboard/'.($this_page_num > 1 ? $this_page_num.'/' : '').'" >'.$show_num_text.'</a>'; }
+            }
+          }
+
+        // Print out the closing container div
+        echo '</div>';
+
+        // FOOTER PAGE LINKS
+
+        // Print out the opening tag for the container dig
+        echo '<div class="container" style="overflow: hidden; padding-bottom: 10px;">';
+        // Display the pregenerated pagelink markup
+        echo $pagelink_markup;
+        // Print out the closing container div
+        echo '</div>';
+
+
+      }
+
+    ?>
+
+    </div>
 </div>
 
 <?
