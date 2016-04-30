34c59d1f817fd65cda5653defed43aba7d0dbaa9
Adrian Marceau <adrian.marceau@gmail.com>
2016-04-30 12:15:42 -0400

    Imported starforce menu updates from the World version of the game into the prototype.
    Stars are now displayed in a scrollable grid showing all unlocked robots masters, making it much easier to track which ones you've already collected and which ones are remaining.

--- scripts/starforce.js
+++ scripts/starforce.js
@@ -6,9 +6,13 @@ var thisContainer = false;
 var thisTypeContainer = false;
 var thisStarContainer = false;
 var thisStarSprites = false;
+var thisPageLinks = false;
+var thisPageLinksTop = false;
+var thisPageLinksSide = false;
+var thisBrowserOrientation = false;
 var thisStarSettings = {};
 thisStarSettings.containerRows = 8;
-thisStarSettings.containerColumns = 10;
+thisStarSettings.containerColumns = 8;
 thisStarSettings.containerLimit = thisStarSettings.containerRows * thisStarSettings.containerColumns;
 thisStarSettings.containerPages = 0;
 thisStarSettings.starCount = 0;
@@ -24,6 +28,10 @@ $(document).ready(function(){
   thisContainer = $('.starforce', thisPrototype);
   thisTypeContainer = $('.types_container', thisPrototype);
   thisStarContainer = $('.stars_container', thisPrototype);
+  thisPageLinks = $('.page_links', thisPrototype);
+  thisPageLinksTop = $('.page_links.top_panel', thisPrototype);
+  thisPageLinksSide = $('.page_links.side_panel', thisPrototype);
+  thisBrowserOrientation = 'portrait';
 
   thisWindow.resize(function(){ windowResizeStarforce(); });
   setTimeout(function(){ windowResizeStarforce(); }, 1000);
@@ -40,20 +48,88 @@ $(document).ready(function(){
     if ($(this).attr('href') == '#'){ return false; }
     window.location.href = $(this).attr('href');
     });
-    
+
+  // Define a click function for the arrow links
+  $('.arrow', thisPageLinks).click(function(e){
+    e.preventDefault();
+
+    var thisArrow = $(this);
+    var thisArrowScroll = thisArrow.attr('data-scroll');
+    var thisPanel = thisArrow.parents('.page_links');
+    var thisPanelType = thisPanel.hasClass('top_panel') ? 'top' : 'side';
+    var thisPanelKey = parseInt(thisPanel.attr('data-key'));
+    var thisPanelMax = parseInt(thisPanel.attr('data-max'));
+    var thisPanelTotal = thisPanel.find('.robot').length;
+
+    //console.log('arrow | type:'+thisPanelType+' | scroll:'+thisArrowScroll+' | key:'+thisPanelKey+' | max:'+thisPanelMax+' | total = '+thisPanelTotal);
+
+    if (thisPanelType == 'top' && thisArrowScroll == 'right'){
+
+      // Scroll to the RIGHT / FORWARD and increment key by one
+      if ((thisPanelMax + thisPanelKey) >= thisPanelTotal){ return false; }
+      newPanelKey = thisPanelKey + 1;
+      //console.log('scroll right / forward to '+newPanelKey);
+
+      } else if (thisPanelType == 'top' && thisArrowScroll == 'left'){
+
+      // Scroll to the LEFT / BACKWARD and decrement key by one
+      if (thisPanelKey <= 0){ return false; }
+      newPanelKey = thisPanelKey - 1;
+      //console.log('scroll left / backward to '+newPanelKey);
+
+      } else if (thisPanelType == 'side' && thisArrowScroll == 'down'){
+
+      // Scroll to the DOWN / FORWARD and increment key by one
+      if ((thisPanelMax + thisPanelKey) >= thisPanelTotal){ return false; }
+      newPanelKey = thisPanelKey + 1;
+      //console.log('scroll down / forward to '+newPanelKey);
+
+      } else if (thisPanelType == 'side' && thisArrowScroll == 'up'){
+
+      // Scroll to the UP / BACKWARD and increment key by one
+      if (thisPanelKey <= 0){ return false; }
+      newPanelKey = thisPanelKey - 1;
+      //console.log('scroll up / backward to '+newPanelKey);
+
+      }
+
+    thisPanel.attr('data-key', newPanelKey);
+    refreshStarforceContainers();
+
+    });
+
+  // Add mouse-wheel scrolling to the starforce container for easier browsing
+  thisStarContainer.bind('mousewheel', function(e){
+    e.preventDefault();
+    //console.log('event.originalEvent.wheelDeltaX = '+e.originalEvent.wheelDeltaX+' | event.originalEvent.wheelDeltaY = '+e.originalEvent.wheelDeltaY);
+    if ((e.originalEvent.wheelDeltaY / 120) > 0){
+      //console.log('scrolling up !');
+      $('.arrow[data-scroll="up"]', thisPageLinks).trigger('click');
+      } else if ((e.originalEvent.wheelDeltaY / 120) < 0) {
+      //console.log('scrolling down !');
+      $('.arrow[data-scroll="down"]', thisPageLinks).trigger('click');
+      } else if ((e.originalEvent.wheelDeltaX / 120) > 0){
+      //console.log('scrolling left !');
+      $('.arrow[data-scroll="left"]', thisPageLinks).trigger('click');
+      } else if ((e.originalEvent.wheelDeltaX / 120) < 0) {
+      //console.log('scrolling right !');
+      $('.arrow[data-scroll="right"]', thisPageLinks).trigger('click');
+      }
+    });
+
   // Count the number of stars present and creat page links if necessary
   thisStarSprites = $('.sprite_star', thisStarContainer);
   thisStarSettings.starCount = thisStarSprites.length ? thisStarSprites.length : 0;
   if (thisStarSettings.starCount > thisStarSettings.containerLimit){
-    
+
     // Calculate the number of pages based on count
     thisStarSettings.containerPages = Math.ceil(thisStarSettings.starCount / thisStarSettings.containerLimit);
-    
+
     // Append the appropriate number of links to the container
-    var thisPageLinks = $('<div class="page_links"><label class="label">Page</label></div>');
-    for (var i = 1; i <= thisStarSettings.containerPages; i++){ thisPageLinks.append('<a href="#" class="page'+(i == 1 ? ' page_active' : '')+'" data-page="'+i+'">'+i+'</a>');  }
-    $('.wrapper', thisContainer).append(thisPageLinks);
-    
+    var thisPageLinksWrapper = $('.page_links .wrapper', thisContainer); //$('<div class="page_links"><label class="label">Page</label></div>');
+    for (var i = 1; i <= thisStarSettings.containerPages; i++){ thisPageLinksWrapper.append('<a href="#" class="page'+(i == 1 ? ' page_active' : '')+'" data-page="'+i+'">'+i+'</a>');  }
+    //$('.wrapper', thisContainer).append(thisPageLinks);
+
     // Create the click event for all these new page buttons
     $('.page', thisContainer).click(function(e){
       e.preventDefault();
@@ -76,8 +152,20 @@ $(document).ready(function(){
     // Trigger a click on page one automatically
     $('.page', thisContainer).first().trigger('click');
     //console.log(thisStarSettings);
-    
+
     }
+
+  // Create the hover event for the star sprites
+  thisStarSprites.hover(function(){
+    var thisSprite = $(this);
+    var thisSideKey = thisSprite.attr('data-side-key');
+    var thisTopKey = thisSprite.attr('data-top-key');
+    thisStarSprites.removeClass('highlight');
+    thisStarSprites.filter('[data-side-key='+thisSideKey+']').addClass('highlight');
+    thisStarSprites.filter('[data-top-key='+thisTopKey+']').addClass('highlight');
+    }, function(){
+    thisStarSprites.removeClass('highlight');
+    });
 
   // Fade in the leaderboard screen slowly
   thisBody.waitForImages(function(){
@@ -98,26 +186,88 @@ function windowResizeStarforce(){
   var windowWidth = thisWindow.width();
   var windowHeight = thisWindow.height();
   var headerHeight = $('.header', thisBody).outerHeight(true);
-  var thisTypeContainerWidth = thisTypeContainer.outerWidth(true);
-  var thisStarContainerWidth = thisStarContainer.outerWidth(true);
-  
-  var thisOrientation = 'portrait';
-  if (windowWidth > 800){ thisOrientation = 'landscape'; thisBody.addClass((gameSettings.wapFlag ? 'mobileFlag' : 'windowFlag')+'_landscapeMode'); }
-  else { thisOrientation = 'portrait'; thisBody.removeClass((gameSettings.wapFlag ? 'mobileFlag' : 'windowFlag')+'_landscapeMode'); }
-  
-  // Define the type and star container widths for resizing
-  var newStarContainerWidth = windowWidth - thisTypeContainerWidth - 34;
-  thisStarContainer.css({width:newStarContainerWidth+'px'});
-  $('.page_links', thisContainer).css({width:newStarContainerWidth+'px'});
-  //var newTypeContainerWidth = windowWidth - thisStarContainerWidth - 48;
-  //thisTypeContainer.css({width:newTypeContainerWidth+'px'});
-  //console.log('var newStarContainerWidth = thisOrientation == \'portrait\' ? 516 : 772;\nnewStarContainerWidth = '+newStarContainerWidth+';');
-  //console.log('var newTypeContainerWidth = windowWidth - newStarContainerWidth - 30;\nnewTypeContainerWidth = '+newTypeContainerWidth+';');
-  
+  //var thisTypeContainerWidth = thisTypeContainer.outerWidth(true);
+  //var thisStarContainerWidth = thisStarContainer.outerWidth(true);
+  //var thisPageLinksTopWidth = thisPageLinksTop.outerWidth(true);
+  //var thisPageLinksSideWidth = thisPageLinksSide.outerWidth(true);
+
+  // Update the browser orientation
+  var menuWidth = $('.menu', thisPrototype).width();
+  if (menuWidth > 800){ thisBrowserOrientation = 'landscape'; thisBody.addClass((gameSettings.wapFlag ? 'mobileFlag' : 'windowFlag')+'_landscapeMode'); }
+  else { thisBrowserOrientation = 'portrait'; thisBody.removeClass((gameSettings.wapFlag ? 'mobileFlag' : 'windowFlag')+'_landscapeMode'); }
+  //console.log('update orientation to '+thisBrowserOrientation);
+
   // Update the body and prototype to full height
   thisBody.css({height:windowHeight+'px'});
   thisPrototype.css({height:windowHeight+'px'});
 
-  //console.log('windowWidth = '+windowWidth+'; thisTypeContainerWidth = '+thisTypeContainerWidth+'; thisStarContainerWidth = '+thisStarContainerWidth+'; ');
+  // Refresh the starforce elements
+  thisPageLinksSide.attr('data-key', 0);
+  thisPageLinksTop.attr('data-key', 0);
+  refreshStarforceContainers();
+
+}
+
+// Define a function for updating the starforce menu elements
+function refreshStarforceContainers(){
+  //console.log('refreshStarforceContainers()');
+
+  // Update the max links for the top panel based on orientation
+  var newSideDataMax = 9;
+  var newTopDataMax = 9;
+  if (thisBrowserOrientation == 'landscape'){ newTopDataMax = 19; }
+  else if (thisBrowserOrientation == 'portrait'){ newTopDataMax = 12; }
+  //console.log('update page links top data max to '+newTopDataMax);
+  thisPageLinksSide.attr('data-max', newSideDataMax);
+  thisPageLinksTop.attr('data-max', newTopDataMax);
+
+  // Define a variable to hold the allowed top and side keys
+  var allowedTopKeys = [];
+  var allowedSideKeys = [];
+
+  // Loop through the link panels and limit robot display
+  thisPageLinks.each(function(index, panel){
+
+    // Collect robot limits from the panel and define the range
+    var linkPanel = $(this);
+    var linkPanelKind = linkPanel.hasClass('top_panel') ? 'top' : 'side';
+    var displayMax = parseInt(linkPanel.attr('data-max'));
+    var currentKey = parseInt(linkPanel.attr('data-key'));
+    var firstVisibleKey = currentKey;
+    var lastVisibleKey = currentKey + displayMax - 1;
+    //console.log('parsing linkPanel '+linkPanel.attr('class'));
+    //console.log('displayMax = '+displayMax+' currentKey = '+currentKey+' firstVisibleKey = '+firstVisibleKey+' lastVisibleKey = '+lastVisibleKey);
+
+    // Hide all robots links in both menus, then show only up to max
+    $('.robot', linkPanel).each(function(index2, robot){
+      var robotLink = $(this);
+      if (index2 >= firstVisibleKey && index2 <= lastVisibleKey){
+        robotLink.removeClass('hidden');
+        if (linkPanelKind == 'top'){ allowedTopKeys.push(robotLink.attr('data-top-key')); }
+        else if (linkPanelKind == 'side'){ allowedSideKeys.push(robotLink.attr('data-side-key')); }
+        } else {
+        robotLink.addClass('hidden');
+        }
+      });
+
+    });
+
+  //console.log('allowedTopKeys = ', allowedTopKeys);
+  //console.log('allowedSideKeys = ', allowedSideKeys);
+
+  // Loop through all the star containers and hide ones that are not relevant
+  $('.sprite_star', thisStarContainer).each(function(){
+
+    var thisStar = $(this);
+    var thisTopKey = thisStar.attr('data-top-key');
+    var thisSideKey = thisStar.attr('data-side-key');
+
+    if (allowedTopKeys.indexOf(thisTopKey) != -1 && allowedSideKeys.indexOf(thisSideKey) != -1){
+      thisStar.removeClass('hidden');
+      } else {
+      thisStar.addClass('hidden');
+      }
+
+    });
 
 }
