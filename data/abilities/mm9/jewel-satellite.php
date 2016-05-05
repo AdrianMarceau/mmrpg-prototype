55d75eab643ac04a4be43b220b14b5f5989861e8
Adrian Marceau <adrian.marceau@gmail.com>
2016-05-04 20:42:03 -0400

    Updated description for Laser Trident to include secondary effect amount, Jewel Satellite with intended effect when complete.

--- data/abilities/mm9/jewel-satellite.php
+++ data/abilities/mm9/jewel-satellite.php
@@ -4,23 +4,23 @@ $ability = array(
   'ability_name' => 'Jewel Satellite',
   'ability_token' => 'jewel-satellite',
   'ability_game' => 'MM09',
-  'ability_description' => '...',
+  'ability_description' => 'The user raises a shield of orbiting diamonds that protect it from all Flame, Nature, Water, and Electric type damage.  The group of diamonds can also be thrown at an opposing robot once raised to deal moderate damage.',
   'ability_type' => 'crystal',
   'ability_type2' => 'shield',
   'ability_damage' => 10,
   'ability_accuracy' => 90,
   'ability_function' => function($objects){
-    
+
     // Extract all objects into the current scope
     extract($objects);
-    
+
     // Target the opposing robot
     $this_ability->target_options_update(array(
       'frame' => 'shoot',
       'success' => array(0, 75, 0, 10, $this_robot->print_robot_name().' uses the '.$this_ability->print_ability_name().'!')
       ));
     $this_robot->trigger_target($target_robot, $this_ability);
-    
+
     // Inflict damage on the opposing robot
     $this_ability->damage_options_update(array(
       'kind' => 'energy',
@@ -37,10 +37,10 @@ $ability = array(
       ));
     $energy_damage_amount = $this_ability->ability_damage;
     $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
-    
+
     // Return true on success
     return true;
-      
+
   }
   );
 ?>
