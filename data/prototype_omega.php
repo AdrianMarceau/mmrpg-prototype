39d8d44a341a014670763b26ea8841932a120528
Adrian Marceau <adrian.marceau@gmail.com>
2016-05-08 12:36:18 -0400

    Added Magma Man to the list of prototype omega factors so he'll appear in Flame type Starforce missions.

--- data/prototype_omega.php
+++ data/prototype_omega.php
@@ -113,7 +113,7 @@ $this_omega_factors_nine[] = array('robot' => 'splash-woman', 'field' => 'field'
 $this_omega_factors_nine[] = array('robot' => 'plug-man', 'field' => 'field', 'type' => 'electric');
 $this_omega_factors_nine[] = array('robot' => 'jewel-man', 'field' => 'gemstone-cavern', 'type' => 'crystal');
 $this_omega_factors_nine[] = array('robot' => 'hornet-man', 'field' => 'field', 'type' => 'nature');
-//$this_omega_factors_nine[] = array('robot' => 'magma-man', 'field' => 'field', 'type' => 'flame');
+$this_omega_factors_nine[] = array('robot' => 'magma-man', 'field' => 'field', 'type' => 'flame');
 $this_omega_factors_nine[] = array('robot' => 'galaxy-man', 'field' => 'field', 'type' => 'space');
 
 // Define the expansion omega factors for DR. LIGHT (MEGA MAN 10)
