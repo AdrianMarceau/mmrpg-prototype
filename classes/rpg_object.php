529d755ca070c82e18f68d28f079f9ef4f5a6954
Adrian Marceau <adrian.marceau@gmail.com>
2016-06-19 22:07:36 -0400

    Replaced reserved keyword "class" in RPG object class with "rpg_class" to prevent fatal error.
    Also added more getter and setting functions for other comment object properties.

--- classes/rpg_object.php
+++ classes/rpg_object.php
@@ -88,6 +88,24 @@ class rpg_object {
     }
 
     /**
+     * Get or set this object's string RPG class
+     * @param   string      $value      String object class (optional)
+     * @return  string
+     */
+    public function rpg_class($value = null){
+        return $this->value('class', $value);
+    }
+
+    /**
+     * Get or set this object's string RPG subclass
+     * @param   string      $value      String object subclass (optional)
+     * @return  string
+     */
+    public function rpg_subclass($value = null){
+        return $this->value('subclass', $value);
+    }
+
+    /**
      * Get or set this object's integer key
      * @param   integer     $value      Integer object key (optional)
      * @return  integer
@@ -131,14 +149,116 @@ class rpg_object {
     public function name($value = null){
         return $this->value('name', $value);
     }
+    public function base_name($value = null){
+        return $this->value('base_name', $value);
+    }
 
     /**
-     * Get or set this object's string class
-     * @param   string      $value      String object class (optional)
+     * Get or set this object's string image
+     * @param   string      $value      String object image (optional)
      * @return  string
      */
-    public function class($value = null){
-        return $this->value('class', $value);
+    public function image($value = null){
+        return $this->value('image', $value);
+    }
+    public function base_image($value = null){
+        return $this->value('base_image', $value);
+    }
+
+    /**
+     * Get or set this object's string image size
+     * @param   integer      $value      Integer object image size (optional)
+     * @return  string
+     */
+    public function image_size($value = null){
+        return $this->value('image_size', $value);
+    }
+    public function base_image_size($value = null){
+        return $this->value('base_image_size', $value);
+    }
+
+    /**
+     * Get or set this object's string image editor
+     * @param   integer      $value      Integer object image editor (optional)
+     * @return  string
+     */
+    public function image_editor($value = null){
+        return $this->value('image_editor', $value);
+    }
+    public function base_image_editor($value = null){
+        return $this->value('base_image_editor', $value);
+    }
+
+    /**
+     * Get or set this object's string image overlay
+     * @param   array       $values      Array object image overlay (optional)
+     * @return  string
+     */
+    public function image_overlay($values = null){
+        return $this->value('image_overlay', $values);
+    }
+    public function base_image_overlay($values = null){
+        return $this->value('base_image_overlay', $values);
+    }
+
+    /**
+     * Get or set this object's string type
+     * @param   string      $value      String object type (optional)
+     * @return  string
+     */
+    public function type($value = null){
+        return $this->value('type', $value);
+    }
+    public function base_type($value = null){
+        return $this->value('base_type', $value);
+    }
+
+    /**
+     * Get or set this object's string type2
+     * @param   string      $value      String object type2 (optional)
+     * @return  string
+     */
+    public function type2($value = null){
+        return $this->value('type2', $value);
+    }
+    public function base_type2($value = null){
+        return $this->value('base_type2', $value);
+    }
+
+    /**
+     * Get or set this object's string frame
+     * @param   string      $value      String object frame (optional)
+     * @return  string
+     */
+    public function frame($value = null){
+        return $this->value('frame', $value);
+    }
+
+    /**
+     * Get or set this object's string frame offset
+     * @param   array       $values      Array object frame offset (optional)
+     * @return  string
+     */
+    public function frame_offset($values = null){
+        return $this->value('frame_offset', $values);
+    }
+
+    /**
+     * Get or set this object's string frame classes
+     * @param   string      $values      String object frame classes (optional)
+     * @return  string
+     */
+    public function frame_classes($values = null){
+        return $this->value('frame_classes', $value);
+    }
+
+    /**
+     * Get or set this object's string frame styles
+     * @param   string      $values      String object frame styles (optional)
+     * @return  string
+     */
+    public function frame_styles($values = null){
+        return $this->value('frame_styles', $value);
     }
 
 
