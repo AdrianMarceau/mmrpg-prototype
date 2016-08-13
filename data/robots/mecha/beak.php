81202f583cc35dcb8e2f7c9916063b80e975cb79
Adrian Marceau <adrian.marceau@gmail.com>
2016-08-13 15:36:40 -0400

    Reduced three Beak mechas into one with alts.

--- data/robots/mecha/beak.php
+++ data/robots/mecha/beak.php
@@ -3,12 +3,16 @@
 $robot = array(
   'robot_number' => 'BEAK-001', // ROBOT : BEAK (1st Gen)
   'robot_class' => 'mecha',
-  'robot_game' => 'MM01',
+  'robot_game' => 'MM005',
   'robot_name' => 'Beak',
   'robot_token' => 'beak',
   'robot_image_editor' => 412,
+  'robot_image_alts' => array(
+    array('token' => 'alt', 'name' => 'Beak (2nd Gen)', 'summons' => 30),
+    array('token' => 'alt2', 'name' => 'Beak (3rd Gen)', 'summons' => 60)
+    ),
   'robot_core' => 'laser',
-  'robot_field' => 'orb-city',
+  'robot_field' => 'final-destination',
   'robot_description' => 'Motion Sensing Mecha',
   'robot_energy' => 100,
   'robot_attack' => 100,
