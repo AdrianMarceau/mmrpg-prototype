f501c0e2afd6fe75f6303e46511eab92275c0e9d
Adrian Marceau <adrian.marceau@gmail.com>
2016-08-13 15:51:17 -0400

    Reduced three Telly mechas into one with two extra alts.

--- data/robots/mecha/telly.php
+++ data/robots/mecha/telly.php
@@ -7,6 +7,10 @@ $robot = array(
   'robot_name' => 'Telly',
   'robot_token' => 'telly',
   'robot_image_editor' => 412,
+  'robot_image_alts' => array(
+    array('token' => 'alt', 'name' => 'Telly (2nd Gen)', 'summons' => 30),
+    array('token' => 'alt2', 'name' => 'Telly (3rd Gen)', 'summons' => 60)
+    ),
   'robot_core' => 'flame',
   'robot_field' => 'atomic-furnace',
   'robot_description' => 'Rotating Security Mecha',
