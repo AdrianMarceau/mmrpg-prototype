037a447accd037a8ff20912749696fd1a4627a76
Adrian Marceau <adrian.marceau@gmail.com>
2016-08-13 15:52:20 -0400

    Converted three generations of Nitron into three distinct elemental mecha.

--- data/robots/mecha/nitron-y.php
+++ data/robots/mecha/nitron-y.php
@@ -1,25 +1,25 @@
 <?
-// NITRON II
+// NITRON Y
 $robot = array(
-  'robot_number' => 'NTRN-002', // ROBOT : NITRON (2nd Gen)
+  'robot_number' => 'NTRN-003', // ROBOT : NITRON (YELLOW)
   'robot_class' => 'mecha',
   'robot_game' => 'MM03',
-  'robot_name' => 'Nitron',
-  'robot_token' => 'nitron-2',
+  'robot_name' => 'Nitron Y',
+  'robot_token' => 'nitron-y',
   'robot_image_editor' => 412,
-  'robot_core' => 'freeze',
+  'robot_core' => 'electric',
   'robot_field' => 'reflection-chamber',
-  'robot_description' => 'Frozen Pillars Mecha',
+  'robot_description' => 'Electric Sparks Mecha',
   'robot_energy' => 100,
   'robot_attack' => 100,
   'robot_defense' => 100,
   'robot_speed' => 100,
-  'robot_weaknesses' => array('electric', 'flame'),
-  'robot_affinities' => array('freeze'),
-  'robot_abilities' => array('nitron-ice'),
+  'robot_weaknesses' => array('flame', 'freeze'),
+  'robot_affinities' => array('electric'),
+  'robot_abilities' => array('nitron-bolt'),
   'robot_rewards' => array(
     'abilities' => array(
-        array('level' => 0, 'token' => 'nitron-ice')
+        array('level' => 0, 'token' => 'nitron-bolt')
       )
     ),
   'robot_quotes' => array(
