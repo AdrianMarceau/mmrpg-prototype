c2a60c735b382e266d38fa20f00a4dbea0497978
Adrian Marceau <adrian.marceau@gmail.com>
2016-08-13 15:46:58 -0400

    Reduced three Picket Man mechas into one with two extra alts.

--- data/robots/mecha/picket-man.php
+++ data/robots/mecha/picket-man.php
@@ -7,6 +7,10 @@ $robot = array(
   'robot_name' => 'Picket Man',
   'robot_token' => 'picket-man',
   'robot_image_editor' => 412,
+  'robot_image_alts' => array(
+    array('token' => 'alt', 'name' => 'Picket Man (2nd Gen)', 'summons' => 30),
+    array('token' => 'alt2', 'name' => 'Picket Man (3rd Gen)', 'summons' => 60)
+    ),
   'robot_core' => 'cutter',
   'robot_field' => 'mountain-mines',
   'robot_description' => 'Picket Throwing Mecha',
