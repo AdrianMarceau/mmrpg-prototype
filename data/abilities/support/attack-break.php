6ae4c7037d13a658a887a8d2f4cbe0c7d1297712
Adrian Marceau <adrian.marceau@gmail.com>
2016-08-14 14:14:15 -0400

    Updated stat boost and break abilities to treat the Met mecha like a support bot that can choose its target.

--- data/abilities/support/attack-break.php
+++ data/abilities/support/attack-break.php
@@ -10,17 +10,17 @@ $ability = array(
   'ability_damage_percent' => true,
   'ability_accuracy' => 95,
   'ability_function' => function($objects){
-    
+
     // Extract all objects into the current scope
     extract($objects);
-    
+
     // Target the opposing robot
     $this_ability->target_options_update(array(
       'frame' => 'summon',
       'success' => array(0, -2, 0, -10, $this_robot->print_robot_name().' uses '.$this_ability->print_ability_name().'!')
       ));
     $this_robot->trigger_target($target_robot, $this_ability);
-    
+
     // Decrease the target robot's attack stat
     $this_ability->damage_options_update(array(
       'kind' => 'attack',
@@ -31,20 +31,20 @@ $ability = array(
       ));
     $attack_damage_amount = ceil($target_robot->robot_attack * ($this_ability->ability_damage / 100));
     $target_robot->trigger_damage($this_robot, $this_ability, $attack_damage_amount);
-    
+
     // Return true on success
     return true;
-      
+
   },
   'ability_function_onload' => function($objects){
-    
+
     // Extract all objects into the current scope
     extract($objects);
-    
+
     // If this ability is being used by a special support robot, allow targetting
-    $temp_support_robots = array('roll', 'disco', 'rhythm');
+    $temp_support_robots = array('roll', 'disco', 'rhythm', 'met');
     if (in_array($this_robot->robot_token, $temp_support_robots) && $target_player->counters['robots_active'] > 1){
-      
+
       // Update this ability's targetting setting
       $this_ability->ability_target = 'select_target';
       $this_ability->update_session();
@@ -52,16 +52,16 @@ $ability = array(
     }
     // Else if the ability attachment is not there, change the target back to auto
     else {
-      
+
       // Update this ability's targetting setting
       $this_ability->ability_target = 'auto';
       $this_ability->update_session();
-      
+
     }
-    
+
     // Return true on success
     return true;
-      
+
     }
   );
 ?>
