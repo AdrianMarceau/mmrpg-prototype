f39878b5316faa8a54912b286deca89ae354706f
Adrian Marceau <adrian.marceau@gmail.com>
2016-08-14 14:14:15 -0400

    Updated stat boost and break abilities to treat the Met mecha like a support bot that can choose its target.

--- data/abilities/support/defense-boost.php
+++ data/abilities/support/defense-boost.php
@@ -10,20 +10,20 @@ $ability = array(
   'ability_recovery_percent' => true,
   'ability_accuracy' => 100,
   'ability_function' => function($objects){
-    
+
     // Extract all objects into the current scope
     extract($objects);
-    
+
     // Target this robot's self
     $this_ability->target_options_update(array(
       'frame' => 'summon',
       'success' => array(0, 0, 10, -10, $this_robot->print_robot_name().' uses '.$this_ability->print_ability_name().'!')
       ));
     $this_robot->trigger_target($this_robot, $this_ability);
-    
+
     // If this ability is being used by a special support robot that allows targetting
     if ($this_robot->player_id == $target_robot->player_id){
-    
+
       // Increase this robot's defense stat
       $this_ability->recovery_options_update(array(
         'kind' => 'defense',
@@ -34,11 +34,11 @@ $ability = array(
         ));
       $defense_recovery_amount = ceil($target_robot->robot_base_defense * ($this_ability->ability_recovery / 100));
       $target_robot->trigger_recovery($target_robot, $this_ability, $defense_recovery_amount);
-      
+
     }
     // Otherwise if targetting a team mate
     else {
-    
+
       // Increase the target robot's defense stat
       $this_ability->recovery_options_update(array(
         'kind' => 'defense',
@@ -49,22 +49,22 @@ $ability = array(
         ));
       $defense_recovery_amount = ceil($this_robot->robot_base_defense * ($this_ability->ability_recovery / 100));
       $this_robot->trigger_recovery($this_robot, $this_ability, $defense_recovery_amount);
-      
+
     }
-    
+
     // Return true on success
     return true;
-      
+
   },
   'ability_function_onload' => function($objects){
-    
+
     // Extract all objects into the current scope
     extract($objects);
-    
+
     // If this ability is being used by a special support robot, allow targetting
-    $temp_support_robots = array('roll', 'disco', 'rhythm');
+    $temp_support_robots = array('roll', 'disco', 'rhythm', 'met');
     if (in_array($this_robot->robot_token, $temp_support_robots) && $this_player->counters['robots_active'] > 1){
-      
+
       // Update this ability's targetting setting
       $this_ability->ability_target = 'select_this';
       $this_ability->update_session();
@@ -72,16 +72,16 @@ $ability = array(
     }
     // Else if the ability attachment is not there, change the target back to auto
     else {
-      
+
       // Update this ability's targetting setting
       $this_ability->ability_target = 'auto';
       $this_ability->update_session();
-      
+
     }
-    
+
     // Return true on success
     return true;
-      
+
     }
   );
 ?>
