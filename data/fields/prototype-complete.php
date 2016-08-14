4c91a9d71d9b78ce14c4b957de3ddaf6828a2468
Adrian Marceau <adrian.marceau@gmail.com>
2016-08-13 20:26:47 -0400

    Updated all fields to remove legacy mecha tokens.

--- data/fields/prototype-complete.php
+++ data/fields/prototype-complete.php
@@ -27,14 +27,8 @@ $field = array(
     ),
   'field_mechas' => array(
     'beak', 'beetle-borg', 'tackle-fire', 'flea', 'flutter-fly', 'picket-man', 'peng', 'spine',
-    'beak-2', 'beetle-borg-2', 'tackle-fire-2', 'flea-2', 'flutter-fly-2', 'picket-man-2', 'peng-2', 'spine-2',
-    'beak-3', 'beetle-borg-3', 'tackle-fire-3', 'flea-3', 'flutter-fly-3', 'picket-man-3', 'peng-3', 'spine-3',
     'batton', 'crazy-cannon', 'fan-fiend', 'killer-bullet', 'pierrobot', 'snapper', 'spring-head', 'telly',
-    'batton-2', 'crazy-cannon-2', 'fan-fiend-2', 'killer-bullet-2', 'pierrobot-2', 'snapper-2', 'spring-head-2', 'telly-2',
-    'batton-3', 'crazy-cannon-3', 'fan-fiend-3', 'killer-bullet-3', 'pierrobot-3', 'snapper-3', 'spring-head-3', 'telly-3',
-    'bulb-blaster', 'robo-fishtot', 'lady-blader', 'manta-missile', 'drill-mole', 'pyre-fly', 'ring-ring', 'skullmet',
-    'bulb-blaster-2', 'robo-fishtot-2', 'lady-blader-2', 'manta-missile-2', 'drill-mole-2', 'pyre-fly-2', 'ring-ring-2', 'skullmet-2',
-    'bulb-blaster-3', 'robo-fishtot-3', 'lady-blader-3', 'manta-missile-3', 'drill-mole-3', 'pyre-fly-3', 'ring-ring-3', 'skullmet-3'
+    'bulb-blaster', 'robo-fishtot', 'lady-blader', 'manta-missile', 'drill-mole', 'pyre-fly', 'ring-ring', 'skullmet'
     ),
   'field_background_frame' => array(0), //array(0,1),
   'field_foreground_frame' => array(0)
