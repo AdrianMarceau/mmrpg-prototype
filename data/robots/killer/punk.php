3d505c89ef95b4d1f5b5d3d6c207252c93caf3f9
Adrian Marceau <adrian.marceau@gmail.com>
2016-08-14 21:07:45 -0400

    Updated robot import script to include robot group for mechas and bosses too.
    Updated specific bosses with new group to push to the last database page.

--- data/robots/killer/punk.php
+++ data/robots/killer/punk.php
@@ -1,43 +1,44 @@
 <?
 // PUNK
 $robot = array(
-  'robot_number' => 'MKN-002',
-  'robot_class' => 'boss',
-  'robot_game' => 'MM20',
-  'robot_name' => 'Punk',
-  'robot_token' => 'punk',
-  'robot_image_editor' => 18,
-  'robot_image_size' => 80,
-  'robot_core' => '',
-  'robot_description' => 'Brutal Megaman Hunter',
-  'robot_description2' => 'Punk was created to be the next line of the Mega Man Killers and are arguably the most brutal of the Killers. They has great power and can morph themselves in a spiked ball to do great damage to any opponent. The Punk unit are usually quiet robots but some have displayed traits of high school bullies. They do not follow orders to anything but strangely, the Enker unit. They are dangerous opponents to fight and must be fought with great caution.',
-  'robot_energy' => 100,
-  'robot_attack' => 100,
-  'robot_defense' => 100,
-  'robot_speed' => 100,
-  'robot_weaknesses' => array('explode', 'wind'),
-  'robot_resistances' => array('shadow'),
-  'robot_abilities' => array(
-  	'screw-crusher',
-  	'buster-shot',
-  	'attack-boost', 'attack-break', 'attack-swap', 'attack-mode',
-  	'defense-boost', 'defense-break', 'defense-swap', 'defense-mode',
-    'speed-boost', 'speed-break', 'speed-swap', 'speed-mode',
-    'energy-boost', 'energy-break', 'energy-swap', 'repair-mode',
-    'field-support', 'mecha-support',
-    'light-buster', 'wily-buster', 'cossack-buster'
-    ),
-  'robot_rewards' => array(
-    'abilities' => array(
-        array('level' => 0, 'token' => 'buster-shot'),
-        array('level' => 0, 'token' => 'screw-crusher')
-      )
-    ),
-  'robot_quotes' => array(
-    'battle_start' => '',
-    'battle_taunt' => '',
-    'battle_victory' => '',
-    'battle_defeat' => ''
-    )
-  );
+    'robot_number' => 'MKN-002',
+    'robot_class' => 'boss',
+    'robot_game' => 'MM20',
+    'robot_group' => 'MMRPG2',
+    'robot_name' => 'Punk',
+    'robot_token' => 'punk',
+    'robot_image_editor' => 18,
+    'robot_image_size' => 80,
+    'robot_core' => '',
+    'robot_description' => 'Brutal Megaman Hunter',
+    'robot_description2' => 'Punk was created to be the next line of the Mega Man Killers and are arguably the most brutal of the Killers. They has great power and can morph themselves in a spiked ball to do great damage to any opponent. The Punk unit are usually quiet robots but some have displayed traits of high school bullies. They do not follow orders to anything but strangely, the Enker unit. They are dangerous opponents to fight and must be fought with great caution.',
+    'robot_energy' => 100,
+    'robot_attack' => 100,
+    'robot_defense' => 100,
+    'robot_speed' => 100,
+    'robot_weaknesses' => array('explode', 'wind'),
+    'robot_resistances' => array('shadow'),
+    'robot_abilities' => array(
+        'screw-crusher',
+        'buster-shot',
+        'attack-boost', 'attack-break', 'attack-swap', 'attack-mode',
+        'defense-boost', 'defense-break', 'defense-swap', 'defense-mode',
+        'speed-boost', 'speed-break', 'speed-swap', 'speed-mode',
+        'energy-boost', 'energy-break', 'energy-swap', 'repair-mode',
+        'field-support', 'mecha-support',
+        'light-buster', 'wily-buster', 'cossack-buster'
+        ),
+    'robot_rewards' => array(
+        'abilities' => array(
+                array('level' => 0, 'token' => 'buster-shot'),
+                array('level' => 0, 'token' => 'screw-crusher')
+            )
+        ),
+    'robot_quotes' => array(
+        'battle_start' => '',
+        'battle_taunt' => '',
+        'battle_victory' => '',
+        'battle_defeat' => ''
+        )
+    );
 ?>
