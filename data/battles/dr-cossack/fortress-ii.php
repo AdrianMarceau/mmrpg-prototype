4719cec0420a5ba9b803f652168fbfd18dea414e
Adrian Marceau <adrian.marceau@gmail.com>
2016-08-15 23:15:41 -0400

    Removed mission specific item drops from all the fortress battles.
    Items should be dropped based on who you fight, not where you fight.  A minor enemy should never drop x2 Energy Tanks.

--- data/battles/dr-cossack/fortress-ii.php
+++ data/battles/dr-cossack/fortress-ii.php
@@ -23,9 +23,6 @@ $battle = array(
     'abilities' => array(
       ),
     'items' => array(
-      array('chance' => 20, 'token' => 'energy-tank', 'min' => 1, 'max' => 2),
-      array('chance' => 20, 'token' => 'weapon-tank', 'min' => 1, 'max' => 2),
-      array('chance' => 10, 'token' => 'extra-life', 'min' => 1, 'max' => 2)
       )
     )
   );
