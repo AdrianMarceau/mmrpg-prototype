26dd254f4e9afa3fd97f636cd3b70ef6c85ab6b5
Adrian Marceau <adrian.marceau@gmail.com>
2016-08-30 22:06:33 -0400

    Updated three support bots with tier 2 signature abilities.
    Only Roll's is currently active while the others are commented out until they're created.

--- data/robots/mm0/rhythm.php
+++ data/robots/mm0/rhythm.php
@@ -1,55 +1,56 @@
 <?
 // RHYTHM
 $robot = array(
-  'robot_number' => 'SCN-001',
-  'robot_game' => 'MM00',
-  'robot_name' => 'Rhythm',
-  'robot_token' => 'rhythm',
-  'robot_gender' => 'female',
-  'robot_image_editor' => 4117,
-  'robot_image_alts' => array(
-    array('token' => 'alt', 'name' => 'Rhythm (Proto Alt)', 'summons' => 50, 'colour' => 'flame'),
-    array('token' => 'alt2', 'name' => 'Cute Dress Rhythm', 'summons' => 100, 'colour' => 'nature'),
-    array('token' => 'alt3', 'name' => 'Cute Dress Rhythm (Proto Alt)', 'summons' => 150, 'colour' => 'flame'),
-    array('token' => 'alt4', 'name' => 'Mobile Suit Rhythm', 'summons' => 200, 'colour' => 'nature'),
-    array('token' => 'alt5', 'name' => 'Mobile Suit Rhythm (Proto Alt)', 'summons' => 250, 'colour' => 'flame')
-    ),
-  'robot_description' => 'Genius Hacker Robot',
-  'robot_description2' => 'Rhythm is a digital support robot coded by Dr. Cossack and integrated directly into the data of the prototype.  Rhythm\'s initial function was to act as kind of debug tool for the two doctors during the prototype\'s development, tweaking values and swapping parameters as needed, but recent events have forced Cossack to clone her data for battle purposes instead.  As a genius hacker, the Rhythm series excels in abilities that modify the system from the inside to alter the conditions of battle in strange and unusual ways. With abilities that allow her to swap and shuffle robot stats at will, the Rhythm series is both a great ally and a formidable, unpredictable foe.',
-  'robot_field' => 'cossack-citadel',
-  'robot_energy' => 100,
-  'robot_weapons' => 15,
-  'robot_attack' => 100,
-  'robot_defense' => 100,
-  'robot_speed' => 100,
-  'robot_weaknesses' => array('laser', 'crystal'),
-  'robot_resistances' => array('space'),
-  'robot_abilities' => array(
-    'buster-shot', 'rhythm-buster',
-    'attack-boost', 'defense-boost', 'speed-boost', 'energy-boost',
-    'attack-break', 'defense-break', 'speed-break', 'energy-break',
-    'attack-swap', 'defense-swap', 'speed-swap', 'energy-swap',
-    'attack-support', 'defense-support', 'speed-support', 'energy-support',
-    'attack-assault', 'defense-assault', 'speed-assault', 'energy-assault',
-    'attack-shuffle', 'defense-shuffle', 'speed-shuffle', 'energy-shuffle',
-    'attack-mode', 'defense-mode', 'speed-mode', 'energy-mode', 'repair-mode',
-    'experience-booster', 'recovery-booster', 'damage-booster',
-    'experience-breaker', 'recovery-breaker', 'damage-breaker',
-    'field-support', 'mecha-support',
-    'proto-buster', 'roll-buster', 'disco-buster',
-    'light-buster', 'wily-buster', 'cossack-buster'
-    ),
-  'robot_rewards' => array(
-    'abilities' => array(
-      array('level' => 0, 'token' => 'buster-shot'),
-      array('level' => 22, 'token' => 'rhythm-buster')
-      )
-    ),
-  'robot_quotes' => array(
-    'battle_start' => '',
-    'battle_taunt' => '',
-    'battle_victory' => '',
-    'battle_defeat' => ''
-    )
-  );
+    'robot_number' => 'SCN-001',
+    'robot_game' => 'MM00',
+    'robot_name' => 'Rhythm',
+    'robot_token' => 'rhythm',
+    'robot_gender' => 'female',
+    'robot_image_editor' => 4117,
+    'robot_image_alts' => array(
+        array('token' => 'alt', 'name' => 'Rhythm (Proto Alt)', 'summons' => 50, 'colour' => 'flame'),
+        array('token' => 'alt2', 'name' => 'Cute Dress Rhythm', 'summons' => 100, 'colour' => 'nature'),
+        array('token' => 'alt3', 'name' => 'Cute Dress Rhythm (Proto Alt)', 'summons' => 150, 'colour' => 'flame'),
+        array('token' => 'alt4', 'name' => 'Mobile Suit Rhythm', 'summons' => 200, 'colour' => 'nature'),
+        array('token' => 'alt5', 'name' => 'Mobile Suit Rhythm (Proto Alt)', 'summons' => 250, 'colour' => 'flame')
+        ),
+    'robot_description' => 'Genius Hacker Robot',
+    'robot_description2' => 'Rhythm is a digital support robot coded by Dr. Cossack and integrated directly into the data of the prototype.  Rhythm\'s initial function was to act as kind of debug tool for the two doctors during the prototype\'s development, tweaking values and swapping parameters as needed, but recent events have forced Cossack to clone her data for battle purposes instead.  As a genius hacker, the Rhythm series excels in abilities that modify the system from the inside to alter the conditions of battle in strange and unusual ways. With abilities that allow her to swap and shuffle robot stats at will, the Rhythm series is both a great ally and a formidable, unpredictable foe.',
+    'robot_field' => 'cossack-citadel',
+    'robot_energy' => 100,
+    'robot_weapons' => 15,
+    'robot_attack' => 100,
+    'robot_defense' => 100,
+    'robot_speed' => 100,
+    'robot_weaknesses' => array('laser', 'crystal'),
+    'robot_resistances' => array('space'),
+    'robot_abilities' => array(
+        'buster-shot', 'rhythm-buster',
+        'attack-boost', 'defense-boost', 'speed-boost', 'energy-boost',
+        'attack-break', 'defense-break', 'speed-break', 'energy-break',
+        'attack-swap', 'defense-swap', 'speed-swap', 'energy-swap',
+        'attack-support', 'defense-support', 'speed-support', 'energy-support',
+        'attack-assault', 'defense-assault', 'speed-assault', 'energy-assault',
+        'attack-shuffle', 'defense-shuffle', 'speed-shuffle', 'energy-shuffle',
+        'attack-mode', 'defense-mode', 'speed-mode', 'energy-mode', 'repair-mode',
+        'experience-booster', 'recovery-booster', 'damage-booster',
+        'experience-breaker', 'recovery-breaker', 'damage-breaker',
+        'field-support', 'mecha-support',
+        'proto-buster', 'roll-buster', 'disco-buster',
+        'light-buster', 'wily-buster', 'cossack-buster'
+        ),
+    'robot_rewards' => array(
+        'abilities' => array(
+            array('level' => 0, 'token' => 'buster-shot'),
+            array('level' => 22, 'token' => 'rhythm-buster'),
+            //array('level' => 24, 'token' => 'rhythm-heaven')
+            )
+        ),
+    'robot_quotes' => array(
+        'battle_start' => '',
+        'battle_taunt' => '',
+        'battle_victory' => '',
+        'battle_defeat' => ''
+        )
+    );
 ?>
