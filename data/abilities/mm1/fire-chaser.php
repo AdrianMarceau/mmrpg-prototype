8cf35b41e4cdf5867b08a37175c89e23e41c3ab6
Adrian Marceau <adrian.marceau@gmail.com>
2016-09-04 15:08:25 -0400

    Updated Fire Chaser with improvements to damage output, display properties.
    Ability damage now increases if the target is faster than the user (not the other way around) and does NOT have any determent if the opposite is true (target is slower).  This change to power levels is also shown in real-time now via the battle button and ability name.

--- data/abilities/mm1/fire-chaser.php
+++ data/abilities/mm1/fire-chaser.php
@@ -1,49 +1,74 @@
 <?
 // FIRE CHASER
 $ability = array(
-  'ability_name' => 'Fire Chaser',
-  'ability_token' => 'fire-chaser',
-  'ability_game' => 'MM01',
-  'ability_description' => 'The user a unleashes a powerful wave of fire that chases the target, inflicting twice as much damage on slower targets but half as much on faster ones&hellip;',
-  'ability_type' => 'flame',
-  'ability_type2' => 'swift',
-  'ability_energy' => 8,
-  'ability_damage' => 24,
-  'ability_accuracy' => 94,
-  'ability_function' => function($objects){
+    'ability_name' => 'Fire Chaser',
+    'ability_token' => 'fire-chaser',
+    'ability_game' => 'MM01',
+    'ability_description' => 'The user a unleashes a powerful wave of fire that chases the target, inflicting twice as much damage if the target has higher speed than the user!',
+    'ability_type' => 'flame',
+    'ability_type2' => 'swift',
+    'ability_energy' => 8,
+    'ability_damage' => 24,
+    'ability_accuracy' => 94,
+    'ability_function' => function($objects){
 
-    // Extract all objects into the current scope
-    extract($objects);
+        // Extract all objects into the current scope
+        extract($objects);
 
-    // Target the opposing robot
-    $this_ability->target_options_update(array(
-      'frame' => 'shoot',
-      'success' => array(0, 100, 0, 10, $this_robot->print_name().' unleashes a '.$this_ability->print_name().'!'),
-      ));
-    $this_robot->trigger_target($target_robot, $this_ability);
+        // Update this ability's damage based on the user and target's speed
+        if ($target_robot->robot_speed > $this_robot->robot_speed){
+            $this_ability->set_name($this_ability->ability_base_name . ' Δ');
+            $this_ability->set_damage($this_ability->ability_base_damage * 2);
+        } else {
+            $this_ability->set_name($this_ability->ability_base_name);
+            $this_ability->set_damage($this_ability->ability_base_damage);
+        }
 
-    // Inflict damage on the opposing robot
-    $this_ability->damage_options_update(array(
-      'kind' => 'energy',
-      'kickback' => array(15, 0, 0),
-      'success' => array(1, -75, 0, 10, 'The '.$this_ability->print_name().' chased the target!'),
-      'failure' => array(1, -100, 0, -10, 'The '.$this_ability->print_name().' missed&hellip;')
-      ));
-    $this_ability->recovery_options_update(array(
-      'kind' => 'energy',
-      'frame' => 'taunt',
-      'kickback' => array(0, 0, 0),
-      'success' => array(1, -75, 0, 10, 'The '.$this_ability->print_name().' ignited the target!'),
-      'failure' => array(1, -100, 0, -10, 'The '.$this_ability->print_name().' had no effect&hellip;')
-      ));
-    if ($this_robot->robot_speed < $target_robot->robot_speed){ $speed_multiplier = 0.5; }
-    elseif ($this_robot->robot_speed > $target_robot->robot_speed){ $speed_multiplier = 2.0; }
-    else { $speed_multiplier = 1; }
-    $energy_damage_amount = ceil($this_ability->ability_damage * $speed_multiplier);
-    $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
-    // Return true on success
-    return true;
+        // Target the opposing robot
+        $this_ability->target_options_update(array(
+            'frame' => 'shoot',
+            'success' => array(0, 100, 0, 10, $this_robot->print_name().' unleashes a '.$this_ability->print_name().'!'),
+            ));
+        $this_robot->trigger_target($target_robot, $this_ability);
 
-    }
-  );
+        // Inflict damage on the opposing robot
+        $this_ability->damage_options_update(array(
+            'kind' => 'energy',
+            'kickback' => array(15, 0, 0),
+            'success' => array(1, -75, 0, 10, 'The '.$this_ability->print_name().' burned through the target!'),
+            'failure' => array(1, -100, 0, -10, 'The '.$this_ability->print_name().' missed&hellip;')
+            ));
+        $this_ability->recovery_options_update(array(
+            'kind' => 'energy',
+            'frame' => 'taunt',
+            'kickback' => array(0, 0, 0),
+            'success' => array(1, -75, 0, 10, 'The '.$this_ability->print_name().' ignited the target!'),
+            'failure' => array(1, -100, 0, -10, 'The '.$this_ability->print_name().' had no effect&hellip;')
+            ));
+        $energy_damage_amount = $this_ability->ability_damage;
+        $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+
+        // Return true on success
+        return true;
+
+        },
+    'ability_function_onload' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Update this ability's damage based on the user and target's speed
+        if ($target_robot->robot_speed > $this_robot->robot_speed){
+            $this_ability->set_name($this_ability->ability_base_name . ' Δ');
+            $this_ability->set_damage($this_ability->ability_base_damage * 2);
+        } else {
+            $this_ability->set_name($this_ability->ability_base_name);
+            $this_ability->set_damage($this_ability->ability_base_damage);
+        }
+
+        // Return true on success
+        return true;
+
+        }
+    );
 ?>
