2f5cfd7162457acedb763d8f05c004e6036da1e9
Adrian Marceau <adrian.marceau@gmail.com>
2016-09-10 15:41:56 -0400

    Added a get battle function to the rpg game class and updated legacy reference in the battle loop.

--- classes/rpg_game.php
+++ classes/rpg_game.php
@@ -16,4 +16,41 @@ class rpg_game {
 
     }
 
+    /**
+     * Create or retrive a battle object from the session
+     * @param array $this_battleinfo
+     * @return rpg_battle
+     */
+    public static function get_battle($this_battleinfo){
+
+        // If the battle index has not been created, do so
+        if (!isset(self::$index['battles'])){ self::$index['battles'] = array(); }
+
+        // Check if a battle ID has been defined
+        if (isset($this_battleinfo['battle_id'])){
+            $battle_id = $this_battleinfo['battle_id'];
+        }
+
+        // If this battle has already been created, retrieve it
+        if (!empty($battle_id) && !empty(self::$index['battles'][$battle_id])){
+
+            // Collect the battle from the index and return
+            $this_battle = self::$index['battles'][$battle_id];
+
+        }
+        // Otherwise create a new battle object in the index
+        else {
+
+            // Create and return the battle object
+            $this_battle = new rpg_battle($this_battleinfo);
+            self::$index['battles'][$this_battle->battle_id] = $this_battle;
+
+        }
+
+        // Return the collect battle object
+        $this_battle->update_session();
+        return $this_battle;
+
+    }
+
 }
