5c296b64d3c0dd53b76b7b3ad75590e794321a17
Adrian Marceau <adrian.marceau@gmail.com>
2016-09-10 15:39:23 -0400

    Created new rpg game class and included it in the top file.
    This class will act as a common container for all in-battle objects.

---
+++ classes/rpg_game.php
@@ -0,0 +1,19 @@
+<?
+/**
+ * Mega Man RPG Game
+ * <p>The top-level container class for Mega Man RPG Prototype objects and settings.</p>
+ */
+class rpg_game {
+
+    // Define global class variables
+    public static $index = array();
+
+    /**
+     * Create a new RPG object
+     * @return  rpg_game
+     */
+    public function rpg_game(){
+
+    }
+
+}
--- top.php
+++ top.php
@@ -79,6 +79,7 @@ if (!defined('MMRPG_INDEX_SESSION') && !defined('MMRPG_INDEX_STYLES')){
 }
 
 // Include mandatory class files
+require('classes/rpg_game.php');
 require('classes/rpg_user.php');
 require('classes/rpg_user_role.php');
 require('classes/rpg_type.php');
