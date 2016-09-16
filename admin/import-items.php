11dae66dbb79d6b34c8b1b0af792b9b45b9a08a4
AdrianMarceau <adrian.marceau@gmail.com>
2016-09-15 22:21:43 -0400

    Fixed bug in ability import causing all of them to be hidden

--- admin/import-items.php
+++ admin/import-items.php
@@ -215,7 +215,7 @@ if (!empty($mmrpg_index['items'])){
     $temp_insert_array['item_frame_classes'] = !empty($item_data['item_frame_classes']) ? $item_data['item_frame_classes'] : '';
 
     // Define the flags
-    $temp_insert_array['item_flag_hidden'] = $temp_insert_array['item_class'] != 'master' || in_array($temp_insert_array['item_token'], $hidden_database_items) ? 1 : 0;
+    $temp_insert_array['item_flag_hidden'] = in_array($temp_insert_array['item_token'], $hidden_database_items) ? 1 : 0;
     $temp_insert_array['item_flag_complete'] = $temp_insert_array['item_class'] == 'system' || $item_data['item_image'] != 'item' ? 1 : 0;
     $temp_insert_array['item_flag_published'] = 1;
 
