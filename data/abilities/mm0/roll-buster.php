8cc5300e34943bd482a4987fa6513ef5efc600e2
AdrianMarceau <adrian.marceau@gmail.com>
2016-09-18 22:12:11 -0400

    Updated Roll, Disco, and Rhythm Busters with support for Charge and Target Modules.

--- data/abilities/mm0/roll-buster.php
+++ data/abilities/mm0/roll-buster.php
@@ -30,8 +30,14 @@ $ability = array(
         // Loop through each existing attachment and alter the start frame by one
         foreach ($this_robot->robot_attachments AS $key => $info){ array_push($this_attachment_info['ability_frame_animate'], array_shift($this_attachment_info['ability_frame_animate'])); }
 
+        // Check if this ability is already charged
+        $is_charged = isset($this_robot->robot_attachments[$this_attachment_token]) ? true : false;
+
+        // If the user is holding a Charge Module, auto-charge the ability
+        if ($this_robot->has_item('charge-module')){ $is_charged = true; }
+
         // If the ability flag was not set, this ability begins charging
-        if (!isset($this_robot->robot_attachments[$this_attachment_token])){
+        if (!$is_charged){
 
             // Target this robot's self
             $this_ability->target_options_update(array(
@@ -97,12 +103,20 @@ $ability = array(
         // Define this ability's attachment token
         $this_attachment_token = 'ability_'.$this_ability->ability_token;
 
+        // Check if this ability is already charged
+        $is_charged = isset($this_robot->robot_attachments[$this_attachment_token]) ? true : false;
+
         // If the ability flag had already been set, reduce the weapon energy to zero
-        if (isset($this_robot->robot_attachments[$this_attachment_token])){ $this_ability->ability_energy = 0; }
+        if ($is_charged){ $this_ability->set_energy(0); }
         // Otherwise, return the weapon energy back to default
-        else { $this_ability->ability_energy = $this_ability->ability_base_energy; }
-        // Update the ability session
-        $this_ability->update_session();
+        else { $this_ability->reset_energy(); }
+
+        // If the user is holding a Charge Module, auto-charge the ability
+        if ($this_robot->has_item('charge-module')){ $is_charged = true; }
+
+        // If the user is holding a Target Module, allow bench targeting
+        if ($is_charged && $this_robot->has_item('target-module')){ $this_ability->set_target('select_target'); }
+        else { $this_ability->reset_target(); }
 
         // Return true on success
         return true;
