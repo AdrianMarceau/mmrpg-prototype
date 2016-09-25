c91974907c5b9eab15c9cfa6e1a9fb602860cc82
AdrianMarceau <adrian.marceau@gmail.com>
2016-09-24 22:12:36 -0400

    Updated the item import script to hide elemental stars and push the new event items to the end.
    The function of stars are being revamped in this update and starforce is being removed.  It now makes more sense to have a single item in the database representing all the different stars you've collected.

--- data/items/stars/star.php
+++ data/items/stars/star.php
@@ -7,7 +7,7 @@ $item = array(
     'item_group' => 'MMRPG/Items/Stars',
     'item_class' => 'item',
     'item_image_sheets' => 2,
-    'item_description' => 'A strange and mysterious star-shaped energy source that takes on the elemental properties of its surroundings and glows with a radiant light. Field Stars appear in areas with high concentrations of elemental energy and have been known to fuse with each other when in close range into newer, more powerful stars.  Field Stars and Fusion Stars alike permanantly boost the elemental damage and recovery of abilities used in battle, so collecting as many as possible is a great way to ensure victory.',
+    'item_description' => 'A mysterious elemental star that radiates with the energy of a distant planet. These stars appear to come in a variety of different types and collecting lots of them may be essential to progressing through the story.  A certain character is also said to be researching these items and would likely trade a respectable amount of Zenny to study one up close.',
     'item_function' => function($objects){
 
         // Extract all objects into the current scope
