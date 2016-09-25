a0cc8c78bb7af764d514d2b18ede421243a68402
AdrianMarceau <adrian.marceau@gmail.com>
2016-09-24 22:09:58 -0400

    Added three new event-based key items to the item database.
    These items will unlock existing functions but make more sense story-wise for them being gated off at the start.

---
+++ data/items/events/transfer-program.php
@@ -0,0 +1,23 @@
+<?
+// ITEM : TRANSFER PROGRAM
+$item = array(
+    'item_name' => 'Transfer Program',
+    'item_token' => 'transfer-program',
+    'item_game' => 'MMRPG',
+    'item_group' => 'MMRPG/Items/Events',
+    'item_class' => 'item',
+    'item_subclass' => 'event',
+    'item_type' => '',
+    'item_type2' => 'wily',
+    'item_description' => 'An essential program developed by Dr. Wily for use in the prototype, this item allows the doctors to transfer unlocked robots masters between each other.',
+    'item_energy' => 0,
+    'item_speed' => 10,
+    'item_accuracy' => 100,
+    'item_function' => function($objects){
+
+        // Return true on success
+        return true;
+
+    }
+    );
+?>
