1185b9cbc1c9a826f3c0d42ce3653bc59fa08cab
AdrianMarceau <adrian.marceau@gmail.com>
2016-09-26 17:46:45 -0400

    Updated star item with proper subclass.

--- data/items/stars/star.php
+++ data/items/stars/star.php
@@ -6,6 +6,7 @@ $item = array(
     'item_game' => 'MMRPG',
     'item_group' => 'MMRPG/Items/Stars',
     'item_class' => 'item',
+    'item_subclass' => 'collectible',
     'item_image_sheets' => 2,
     'item_description' => 'A mysterious elemental star that radiates with the energy of a distant planet. These stars appear to come in a variety of different types and collecting lots of them may be essential to progressing through the story.  A certain character is also said to be researching these items and would likely trade a respectable amount of Zenny to study one up close.',
     'item_function' => function($objects){
