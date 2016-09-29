e6193db238d0a94e417d61e27d290b52a78f3f4b
AdrianMarceau <adrian.marceau@gmail.com>
2016-09-28 22:30:37 -0400

    Added the Space Shot, Buster, and Overdrive abilities to the game.
    Adjusted default WE for Electric Overdrive and fixed a sorting bug in the ability import script.

---
+++ data/abilities/xspace/space-buster.php
@@ -0,0 +1,29 @@
+<?
+// SPACE BUSTER
+$ability = array(
+    'ability_name' => 'Space Buster',
+    'ability_token' => 'space-buster',
+    'ability_game' => 'MMRPG',
+    'ability_group' => 'MMRPG/Weapons/16/Space',
+    'ability_description' => 'The user charges itself with Space type energy on the first turn to increase its elemental abilities, then releases a powerful cosmic shot at the target on the second to inflict massive damage!',
+    'ability_type' => 'space',
+    'ability_energy' => 4,
+    'ability_damage' => 30,
+    'ability_recovery2' => 33,
+    'ability_recovery2_percent' => true,
+    'ability_accuracy' => 100,
+    'ability_target' => 'auto',
+    'ability_function' => function($objects){
+
+        // Call the common buster function from here
+        return rpg_ability::ability_function_buster($objects, 'cosmic', 'discorded', 'harmonized');
+
+        },
+    'ability_function_onload' => function($objects){
+
+        // Call the common buster onload function from here
+        return rpg_ability::ability_function_onload_buster($objects);
+
+        }
+    );
+?>
