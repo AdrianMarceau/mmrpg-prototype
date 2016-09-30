c659934a9eeedd138f56fef5d899cad00610d0b2
AdrianMarceau <adrian.marceau@gmail.com>
2016-09-29 22:39:22 -0400

    Imported the Cutter Shot/Buster/Overdrive abilities from World (almost forgot about them).

---
+++ data/abilities/xcutter/cutter-buster.php
@@ -0,0 +1,29 @@
+<?
+// CUTTER BUSTER
+$ability = array(
+	'ability_name' => 'Cutter Buster',
+	'ability_token' => 'cutter-buster',
+	'ability_game' => 'MMRPG',
+	'ability_group' => 'MMRPG/Weapons/01/Cutter',
+	'ability_description' => 'The user charges itself with Cutter type energy on the first turn to increase its elemental abilities, then releases a powerful blade shot at the target on the second to inflict massive damage!',
+	'ability_type' => 'cutter',
+	'ability_energy' => 2,
+	'ability_damage' => 30,
+	'ability_recovery2' => 33,
+	'ability_recovery2_percent' => true,
+	'ability_accuracy' => 100,
+	'ability_target' => 'auto',
+	'ability_function' => function($objects){
+
+		// Call the common buster function from here
+		return rpg_ability::ability_function_buster($objects, 'blade', 'sliced through', 'invigorated');
+
+		},
+	'ability_function_onload' => function($objects){
+
+		// Call the common buster onload function from here
+		return rpg_ability::ability_function_onload_buster($objects);
+
+		}
+	);
+?>
