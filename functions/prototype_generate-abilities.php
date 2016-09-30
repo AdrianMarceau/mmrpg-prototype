4f129c4054bc197794f0f2d09e9bcffbf48e586d
AdrianMarceau <adrian.marceau@gmail.com>
2016-09-29 23:00:27 -0400

    Fixed a bug that caused some robots to have zero abilities generated.

--- functions/prototype_generate-abilities.php
+++ functions/prototype_generate-abilities.php
@@ -16,8 +16,7 @@ function mmrpg_prototype_generate_abilities($robot_info, $robot_level = 1, $abil
                 'rolling-cutter', 'super-throw', 'ice-breath', 'hyper-bomb', 'fire-storm', 'thunder-strike', 'time-arrow', 'oil-shooter',
                 'metal-blade', 'air-shooter', 'bubble-spray', 'quick-boomerang', 'crash-bomber', 'flash-stopper', 'atomic-fire', 'leaf-shield',
                 'needle-cannon', 'magnet-missile', 'gemini-laser', 'hard-knuckle', 'top-spin', 'search-snake', 'spark-shock', 'shadow-blade',
-                'bright-burst', 'rain-flush', 'drill-blitz', 'pharaoh-shot', 'ring-boomerang', 'dust-crusher', 'dive-torpedo', 'skull-barrier'
-                /*,
+                'bright-burst', 'rain-flush', 'drill-blitz', 'pharaoh-shot', 'ring-boomerang', 'dust-crusher', 'dive-torpedo', 'skull-barrier',
                 'cutter-shot', 'cutter-buster',
                 'freeze-shot', 'freeze-buster',
                 'crystal-shot', 'crystal-buster',
@@ -25,7 +24,6 @@ function mmrpg_prototype_generate_abilities($robot_info, $robot_level = 1, $abil
                 'electric-shot', 'electric-buster',
                 'space-shot', 'space-buster',
                 'laser-shot', 'laser-buster'
-                */
                 ),
             array(
                 'rising-cutter', 'super-arm', 'ice-slasher', 'danger-bomb', 'fire-chaser', 'thunder-beam', 'time-slow', 'oil-slider',
@@ -35,8 +33,7 @@ function mmrpg_prototype_generate_abilities($robot_info, $robot_level = 1, $abil
                 'slash-claw',
                 'magic-card',
                 'bubble-bomb'
-                )
-            /*,
+                ),
             array(
                 'cutter-overdrive',
                 'freeze-overdrive',
@@ -46,13 +43,12 @@ function mmrpg_prototype_generate_abilities($robot_info, $robot_level = 1, $abil
                 'space-overdrive',
                 'laser-overdrive'
                 )
-                */
             );
     }
     if (empty($mmrpg_prototype_master_support_abilities)){
         $mmrpg_prototype_master_support_abilities = array(
             array(
-                'buster-shot'
+                'buster-shot', 'buster-charge', 'buster-relay'
                 ),
             array(
                 'attack-boost', 'defense-boost', 'speed-boost', 'energy-boost',
@@ -100,23 +96,28 @@ function mmrpg_prototype_generate_abilities($robot_info, $robot_level = 1, $abil
             );
     }
 
+    // Ensure we always have at least one ability
+    if ($ability_num < 1){ $ability_num = 1; }
+
     // Define the array for holding all of this robot's abilities
     $this_robot_abilities = array();
 
     // Loop through this robot's level-up abilities looking for one
     $this_robot_index = $robot_info;
-    if (!empty($this_robot_index['robot_rewards']['abilities'])){
-        foreach ($this_robot_index['robot_rewards']['abilities'] AS $info){
-            // If this is the buster shot or too high of a level, continue
-            if ($info['token'] == 'buster-shot' || $info['level'] > $robot_level){ continue; }
-            // If this is an incomplete master ability, continue
-            if ($this_robot_index['robot_class'] == 'master'){
-                if (!in_array($info['token'], $mmrpg_prototype_core_abilities[0]) && !in_array($info['token'], $mmrpg_prototype_core_abilities[1])){
+    if ($this_robot_index['robot_class'] == 'master'){
+        if (!empty($this_robot_index['robot_rewards']['abilities'])){
+            foreach ($this_robot_index['robot_rewards']['abilities'] AS $info){
+                // If this is the buster shot or too high of a level, continue
+                if ($info['token'] == 'buster-shot' || $info['level'] > $robot_level){ continue; }
+                // If this is an incomplete master ability, continue
+                if (!in_array($info['token'], $mmrpg_prototype_core_abilities[0])
+                    && !in_array($info['token'], $mmrpg_prototype_core_abilities[1])
+                    && !in_array($info['token'], $mmrpg_prototype_core_abilities[2])){
                     continue;
                 }
+                // Add this ability token the list
+                $this_robot_abilities[] = $info['token'];
             }
-            // Add this ability token the list
-            $this_robot_abilities[] = $info['token'];
         }
     }
 
@@ -146,7 +147,7 @@ function mmrpg_prototype_generate_abilities($robot_info, $robot_level = 1, $abil
         // Define the number of core and support abilities for the robot
         if ($this_robot_index['robot_class'] == 'master'){
             foreach ($mmrpg_prototype_core_abilities AS $group_key => $group_abilities){
-                if (floor($robot_level / 10) < ($group_key + 1)){ continue; }
+                if (!empty($this_robot_abilities) && floor($robot_level / 10) < ($group_key + 1)){ continue; }
                 foreach ($group_abilities AS $ability_key => $ability_token){
                     if (in_array($ability_token, $this_robot_abilities)){ continue; }
                     $ability_info = $this_ability_index[$ability_token];
@@ -173,7 +174,7 @@ function mmrpg_prototype_generate_abilities($robot_info, $robot_level = 1, $abil
         // Define the number of core and master support abilities for the robot
         if ($this_robot_index['robot_class'] == 'master' && $this_robot_index['robot_core'] != 'empty'){
             foreach ($mmrpg_prototype_master_support_abilities AS $group_key => $group_abilities){
-                if (floor($robot_level / 10) < ($group_key + 1)){ continue; }
+                if (!empty($this_robot_abilities) && floor($robot_level / 10) < ($group_key + 1)){ continue; }
                 foreach ($group_abilities AS $ability_key => $ability_token){
                     if (in_array($ability_token, $this_robot_abilities)){ continue; }
                     $ability_info = $this_ability_index[$ability_token];
@@ -199,7 +200,7 @@ function mmrpg_prototype_generate_abilities($robot_info, $robot_level = 1, $abil
         // Define the number of core and mecha support abilities for the robot
         elseif ($this_robot_index['robot_class'] == 'mecha' && $this_robot_index['robot_core'] != 'empty'){
             foreach ($mmrpg_prototype_mecha_support_abilities AS $group_key => $group_abilities){
-                if (floor($robot_level / 10) < ($group_key + 1)){ continue; }
+                if (!empty($this_robot_abilities) && floor($robot_level / 10) < ($group_key + 1)){ continue; }
                 foreach ($group_abilities AS $ability_key => $ability_token){
                     if (in_array($ability_token, $this_robot_abilities)){ continue; }
                     $ability_info = $this_ability_index[$ability_token];
@@ -226,7 +227,7 @@ function mmrpg_prototype_generate_abilities($robot_info, $robot_level = 1, $abil
         // Define the number of darkness abilities for the robot
         if ($this_robot_index['robot_core'] == 'empty' && $this_robot_index['robot_class'] == 'master'){
             foreach ($mmrpg_prototype_darkness_abilities AS $group_key => $group_abilities){
-                if (floor($robot_level / 10) < ($group_key + 1)){ continue; }
+                if (!empty($this_robot_abilities) && floor($robot_level / 10) < ($group_key + 1)){ continue; }
                 foreach ($group_abilities AS $ability_key => $ability_token){
                     if (in_array($ability_token, $this_robot_abilities)){ continue; }
                     $ability_info = rpg_ability::parse_index_info($this_ability_index[$ability_token]);
@@ -282,6 +283,9 @@ function mmrpg_prototype_generate_abilities($robot_info, $robot_level = 1, $abil
         $this_robot_abilities = array_slice($this_robot_abilities, 0, $ability_num);
 
     }
+
+    // If this robot truly has no abilities (which is bad), give them the buster shot
+    if (empty($this_robot_abilities)){ $this_robot_abilities[] = 'buster-shot'; }
 
     // Return the ability array, whatever it was
     return $this_robot_abilities;
