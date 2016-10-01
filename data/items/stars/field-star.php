2818b2da6db18f88275a37a30a108f79136673d7
AdrianMarceau <adrian.marceau@gmail.com>
2016-10-01 16:19:21 -0400

    Added Field and Fusion Stars as separate items to the database, updated sorting and hiding for the legacy versions.

---
+++ data/items/stars/field-star.php
@@ -0,0 +1,22 @@
+<?
+// ITEM : FIELD STAR
+$item = array(
+    'item_name' => 'Field Star',
+    'item_token' => 'field-star',
+    'item_game' => 'MMRPG',
+    'item_group' => 'MMRPG/Items/Stars',
+    'item_class' => 'item',
+    'item_subclass' => 'collectible',
+    'item_price' => 6000,
+    'item_description' => 'A mysterious elemental star that radiates with the energy of a distant planet. These stars appear to come in a variety of different types and collecting lots of them may be essential to progressing through the story.  A certain character is also said to be researching these items and would likely trade a respectable amount of Zenny to study one up close.',
+    'item_function' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Return true on success
+        return true;
+
+    }
+    );
+?>
