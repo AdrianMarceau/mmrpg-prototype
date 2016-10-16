d22dc57cfd85b65e0be4c45431c9223fe2ebf91d
AdrianMarceau <adrian.marceau@gmail.com>
2016-10-15 23:40:17 -0400

    Added hold items to robot targets in second fortress missions vs. hunters.

--- data/battles/dr-cossack/fortress-ii.php
+++ data/battles/dr-cossack/fortress-ii.php
@@ -1,29 +1,49 @@
 <?
 // PROTOTYPE BATTLE 5 : VS BALLADE
 $battle = array(
-  'battle_name' => 'Chapter Five Final Battle 1/3',
-  'battle_size' => '1x4',
-  'battle_encore' => true,
-  'battle_description' => 'Defeat the resurrected Ballade in battle!',
-  'battle_turns' => (MMRPG_SETTINGS_BATTLETURNS_PERROBOT * 1),
-  'battle_points' => (MMRPG_SETTINGS_BATTLEPOINTS_PERLEVEL * 45 * 1),
-  'battle_field_base' => array('field_id' => 100, 'field_token' => 'final-destination', 'field_name' => 'Final Destination', 'field_music' => 'final-destination-ballade', 'field_mechas' => array('bulb-blaster', 'robo-fishtot', 'lady-blader', 'manta-missile', 'drill-mole', 'pyre-fly', 'ring-ring', 'skullmet')),
-  'battle_target_player' => array(
-    'player_id' => MMRPG_SETTINGS_TARGET_PLAYERID,
-    'player_token' => 'player',
-    'player_robots' => array(
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 1), 'robot_token' => 'ballade', 'robot_level' => 45, 'robot_abilities' => array('energy-boost', 'energy-break', 'proto-strike', 'mecha-support', 'gemini-laser', 'magnet-missile', 'spark-shock', 'buster-shot')),
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 2), 'robot_token' => 'beak', 'robot_image' => 'beak_alt2', 'robot_level' => 40, 'robot_abilities' => array('beak-shot', 'attack-boost', 'defense-boost', 'speed-boost'), 'flags' => array('hide_from_mission_select' => true)),
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 3), 'robot_token' => 'beak', 'robot_image' => 'beak_alt2', 'robot_level' => 40, 'robot_abilities' => array('beak-shot', 'attack-break', 'defense-break', 'speed-break'), 'flags' => array('hide_from_mission_select' => true)),
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 4), 'robot_token' => 'beak', 'robot_image' => 'beak_alt2', 'robot_level' => 40, 'robot_abilities' => array('beak-shot', 'attack-boost', 'defense-boost', 'speed-boost'), 'flags' => array('hide_from_mission_select' => true)),
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 5), 'robot_token' => 'beak', 'robot_image' => 'beak_alt2', 'robot_level' => 40, 'robot_abilities' => array('beak-shot', 'attack-break', 'defense-break', 'speed-break'), 'flags' => array('hide_from_mission_select' => true))
-      )
-    ),
-  'battle_rewards' => array(
-    'abilities' => array(
-      ),
-    'items' => array(
-      )
-    )
-  );
+    'battle_name' => 'Chapter Five Final Battle 1/3',
+    'battle_size' => '1x4',
+    'battle_encore' => true,
+    'battle_description' => 'Defeat the resurrected Ballade in battle!',
+    'battle_turns' => (MMRPG_SETTINGS_BATTLETURNS_PERROBOT * 1),
+    'battle_points' => (MMRPG_SETTINGS_BATTLEPOINTS_PERLEVEL * 45 * 1),
+    'battle_field_base' => array('field_id' => 100, 'field_token' => 'final-destination', 'field_name' => 'Final Destination', 'field_music' => 'final-destination-ballade', 'field_mechas' => array('bulb-blaster', 'robo-fishtot', 'lady-blader', 'manta-missile', 'drill-mole', 'pyre-fly', 'ring-ring', 'skullmet')),
+    'battle_target_player' => array(
+        'player_id' => MMRPG_SETTINGS_TARGET_PLAYERID,
+        'player_token' => 'player',
+        'player_robots' => array(
+            array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 1), 'robot_token' => 'beak', 'robot_image' => 'beak_alt2', 'robot_level' => 40, 'robot_abilities' => array('beak-shot', 'attack-boost', 'defense-boost', 'speed-boost'), 'flags' => array('hide_from_mission_select' => true)),
+            array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 2), 'robot_token' => 'beak', 'robot_image' => 'beak_alt2', 'robot_level' => 40, 'robot_abilities' => array('beak-shot', 'attack-break', 'defense-break', 'speed-break'), 'flags' => array('hide_from_mission_select' => true)),
+            array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 3), 'robot_token' => 'beak', 'robot_image' => 'beak_alt2', 'robot_level' => 40, 'robot_abilities' => array('beak-shot', 'attack-mode', 'defense-mode', 'speed-mode'), 'flags' => array('hide_from_mission_select' => true)),
+            array(
+                'robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 4),
+                'robot_token' => 'ballade',
+                'robot_level' => 45,
+                'robot_item' => 'extra-life',
+                'robot_abilities' => array(
+                    'buster-shot', 'buster-charge',
+                    'energy-support', 'attack-support', 'defense-support', 'speed-support',
+                    'energy-assault', 'attack-assault', 'defense-assault', 'speed-assault',
+                    'cutter-shot', 'freeze-shot', 'flame-shot',
+                    'electric-shot',  'crystal-shot',  'space-shot',
+                    'shield-shot', 'laser-shot',
+                    'mecha-support'
+                    ),
+                'values' => array(
+                    'robot_rewards' => array(
+                        'robot_attack' => 300,
+                        'robot_defense' => 300,
+                        'robot_speed' => 300
+                        )
+                    )
+                )
+            )
+        ),
+    'battle_rewards' => array(
+        'abilities' => array(
+            ),
+        'items' => array(
+            )
+        )
+    );
 ?>
--- data/battles/dr-wily/fortress-ii.php
+++ data/battles/dr-wily/fortress-ii.php
@@ -1,29 +1,46 @@
 <?
 // PROTOTYPE BATTLE 5 : VS PUNK
 $battle = array(
-  'battle_name' => 'Chapter Five Final Battle 1/3',
-  'battle_size' => '1x4',
-  'battle_encore' => true,
-  'battle_description' => 'Defeat the resurrected Punk in battle!',
-  'battle_turns' => (MMRPG_SETTINGS_BATTLETURNS_PERROBOT * 1),
-  'battle_points' => (MMRPG_SETTINGS_BATTLEPOINTS_PERLEVEL * 40 * 1),
-  'battle_field_base' => array('field_id' => 100, 'field_token' => 'final-destination', 'field_name' => 'Final Destination', 'field_music' => 'final-destination-punk', 'field_mechas' => array('batton', 'crazy-cannon', 'fan-fiend', 'killer-bullet', 'pierrobot', 'snapper', 'spring-head', 'telly')),
-  'battle_target_player' => array(
-    'player_id' => MMRPG_SETTINGS_TARGET_PLAYERID,
-    'player_token' => 'player',
-    'player_robots' => array(
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 1), 'robot_token' => 'punk', 'robot_level' => 40, 'robot_abilities' => array('energy-boost', 'energy-break', 'bass-baroque', 'mecha-support', 'top-spin', 'shadow-blade', 'hard-knuckle', 'buster-shot')),
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 2), 'robot_token' => 'beak', 'robot_image' => 'beak_alt', 'robot_level' => 35, 'robot_abilities' => array('beak-shot', 'attack-boost', 'defense-boost', 'speed-boost'), 'flags' => array('hide_from_mission_select' => true)),
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 3), 'robot_token' => 'beak', 'robot_image' => 'beak_alt', 'robot_level' => 35, 'robot_abilities' => array('beak-shot', 'attack-break', 'defense-break', 'speed-break'), 'flags' => array('hide_from_mission_select' => true)),
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 4), 'robot_token' => 'beak', 'robot_image' => 'beak_alt', 'robot_level' => 35, 'robot_abilities' => array('beak-shot', 'attack-boost', 'defense-boost', 'speed-boost'), 'flags' => array('hide_from_mission_select' => true)),
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 5), 'robot_token' => 'beak', 'robot_image' => 'beak_alt', 'robot_level' => 35, 'robot_abilities' => array('beak-shot', 'attack-break', 'defense-break', 'speed-break'), 'flags' => array('hide_from_mission_select' => true))
-      )
-    ),
-  'battle_rewards' => array(
-    'abilities' => array(
-      ),
-    'items' => array(
-      )
-    )
-  );
+    'battle_name' => 'Chapter Five Final Battle 1/3',
+    'battle_size' => '1x4',
+    'battle_encore' => true,
+    'battle_description' => 'Defeat the resurrected Punk in battle!',
+    'battle_turns' => (MMRPG_SETTINGS_BATTLETURNS_PERROBOT * 1),
+    'battle_points' => (MMRPG_SETTINGS_BATTLEPOINTS_PERLEVEL * 40 * 1),
+    'battle_field_base' => array('field_id' => 100, 'field_token' => 'final-destination', 'field_name' => 'Final Destination', 'field_music' => 'final-destination-punk', 'field_mechas' => array('batton', 'crazy-cannon', 'fan-fiend', 'killer-bullet', 'pierrobot', 'snapper', 'spring-head', 'telly')),
+    'battle_target_player' => array(
+        'player_id' => MMRPG_SETTINGS_TARGET_PLAYERID,
+        'player_token' => 'player',
+        'player_robots' => array(
+            array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 1), 'robot_token' => 'beak', 'robot_image' => 'beak_alt', 'robot_level' => 35, 'robot_abilities' => array('beak-shot', 'attack-boost', 'defense-boost', 'speed-boost'), 'flags' => array('hide_from_mission_select' => true)),
+            array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 2), 'robot_token' => 'beak', 'robot_image' => 'beak_alt', 'robot_level' => 35, 'robot_abilities' => array('beak-shot', 'attack-break', 'defense-break', 'speed-break'), 'flags' => array('hide_from_mission_select' => true)),
+            array(
+                'robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 3),
+                'robot_token' => 'punk',
+                'robot_level' => 40,
+                'robot_item' => 'weapon-upgrade',
+                'robot_abilities' => array(
+                    'buster-shot', 'buster-charge',
+                    'energy-break', 'attack-break', 'defense-break', 'speed-break',
+                    'rising-cutter', 'shadow-blade', 'hard-knuckle',
+                    'spark-shock', 'bright-burst', 'super-throw',
+                    'mecha-support'
+                    ),
+                'values' => array(
+                    'robot_rewards' => array(
+                        'robot_attack' => 200,
+                        'robot_defense' => 200,
+                        'robot_speed' => 200
+                        )
+                    )
+                )
+            )
+        ),
+    'battle_rewards' => array(
+        'abilities' => array(
+            ),
+        'items' => array(
+            )
+        )
+    );
 ?>
