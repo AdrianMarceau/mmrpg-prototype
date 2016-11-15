d4f69e5d7befbf70b7e0f8ba504fb13aa030b6d5
AdrianMarceau <adrian.marceau@gmail.com>
2016-11-14 21:45:58 -0500

    Updated the type database and replaced the old span-based charts with jQuery driven canvas ones.
    Also adjusted some of the filter options and display styles.

--- pages/database_types.php
+++ pages/database_types.php
@@ -18,26 +18,27 @@ $this_markup_header = 'Mega Man RPG Prototype Type Database';
 //$this_markup_counter = '<span class="count">( '.(!empty($mmrpg_database_types_count) ? ($mmrpg_database_types_count == 1 ? '1 Type' : ($mmrpg_database_types_count).' Types') : '0 Types').' )';
 
 // If arguments were included in the header, apply them
-$allow_show = array('all', 'complete');
-$allow_class = array('master', 'mecha', 'boss');
+$allow_class = array('all', 'master', 'mecha', 'boss');
 $filter_incomplete = isset($_GET['incomplete']) && $_GET['incomplete'] == 'include' ? false : true;
-$filter_class = !empty($_GET['class']) && in_array($_GET['class'], $allow_class) ? $_GET['class'] : 'master';
+$filter_class = !empty($_GET['class']) && in_array($_GET['class'], $allow_class) ? $_GET['class'] : 'all';
 
 // Generate filter strings for the robot queries
 $robot_filters = '';
 if ($filter_incomplete){ $robot_filters .= "AND robot_flag_complete = 1 "; }
-if (!empty($filter_class)){ $robot_filters .= "AND robot_class = '{$filter_class}' "; }
+if ($filter_class != 'all'){ $robot_filters .= "AND robot_class = '{$filter_class}' "; }
 
 // Generate filter strings for the ability queries
 $ability_filters = '';
 if ($filter_incomplete){ $ability_filters .= "AND ability_flag_complete = 1 "; }
-if (!empty($filter_class)){ $ability_filters .= "AND ability_class = '{$filter_class}' "; }
+if ($filter_class != 'all'){ $ability_filters .= "AND ability_class = '{$filter_class}' "; }
 
 // Collect an array of type statistics for distribution of robot cores, ability types, weaknesses, etc.
 $filtered_type_stats = $db->get_array_list("SELECT
     types.type_id,
     (CASE WHEN types.type_name = 'None' THEN 'Neutral' ELSE types.type_name END) AS type_name,
     types.type_token,
+    types.type_colour_light,
+    types.type_colour_dark,
 
     -- Robot Cores
     -- IFNULL(robots.robot_count, 0) AS robot_count,
@@ -221,7 +222,6 @@ foreach ($type_stats_index AS $category_token => $category_counts){
 //echo('<pre>$type_stats_index = '.print_r($type_stats_index, true).'</pre>');
 //die();
 
-
 ?>
 <div id="types">
     <h2 class="subheader field_type_<?= MMRPG_SETTINGS_CURRENT_FIELDTYPE ?>">Types Overview</h2>
@@ -240,7 +240,7 @@ foreach ($type_stats_index AS $category_token => $category_counts){
     </div>
     <h2 class="subheader field_type_<?= MMRPG_SETTINGS_CURRENT_FIELDTYPE ?>">Type Distributions</h2>
     <div class="subbody">
-        <p class="text">Given the importance of these types classifications, knowing the distribution of them in the game might be useful in a few ways.  As an example, knowing there are many robots with a weakness to <span class="type_span ability_type ability_type_cutter">Cutter</span> attacks might prompt one to equip more of those types of abilities.  Additionally, knowing the type distribution will also make it easier to balance the game from the development side and may help prevent certain types from getting ignored or abused.  As such, a series of bar-graphs have been created to show how and where these types are being used in the RPG so far.  These bar-graphs are linked directly to the game's logic, so they will always be in sync with recent additions and updates to the project.  Please feel free to use these as reference when preparing for battle, and let me know if you have any questions.</p>
+        <p class="text">Given the importance of these elemental types, knowing their in-game distribution may be useful.  The charts and graphs below pull their data directly from the game's database and can be used as a reference when preparing for battle.</p>
         <?
         // Define a function for generating filter URLs
         function get_filter_url($args){
@@ -258,8 +258,13 @@ foreach ($type_stats_index AS $category_token => $category_counts){
             return $filter_url;
         }
         ?>
+        <script type="text/javascript">
+            var typeChartData = {};
+        </script>
         <div class="type_chart_filters">
             <div class="classes">
+                <a class="link <?= $filter_class == 'all' ? 'active' : '' ?>" href="<?= get_filter_url(array('class' => 'all')) ?>" rel="noindex,nofollow">Show All</a>
+                <span class="pipe">|</span>
                 <a class="link <?= $filter_class == 'master' ? 'active' : '' ?>" href="<?= get_filter_url(array('class' => 'master')) ?>" rel="noindex,nofollow">Robot Masters</a>
                 <span class="pipe">|</span>
                 <a class="link <?= $filter_class == 'mecha' ? 'active' : '' ?>" href="<?= get_filter_url(array('class' => 'mecha')) ?>" rel="noindex,nofollow">Support Mechas</a>
@@ -291,29 +296,33 @@ foreach ($type_stats_index AS $category_token => $category_counts){
                 }
 
                 // Define the class and title based on category
+                if ($filter_class == 'all'){ $title_prefix = 'Robot'; }
+                elseif ($filter_class == 'master'){ $title_prefix = 'Master'; }
+                elseif ($filter_class == 'mecha'){ $title_prefix = 'Mecha'; }
+                elseif ($filter_class == 'boss'){ $title_prefix = 'Boss'; }
                 if ($category_token == 'robot'){
                     $chart_class = 'cores';
-                    $chart_title = 'Robot Cores';
+                    $chart_title = $title_prefix.' Core Types';
                 }
                 elseif ($category_token == 'ability'){
                     $chart_class = 'abilities';
-                    $chart_title = 'Ability Types';
+                    $chart_title = $title_prefix.' Ability Types';
                 }
                 elseif ($category_token == 'weakness'){
                     $chart_class = 'weaknesses';
-                    $chart_title = 'Weaknesses';
+                    $chart_title = $title_prefix.' Weaknesses';
                 }
                 elseif ($category_token == 'resistance'){
                     $chart_class = 'resistances';
-                    $chart_title = 'Resistances';
+                    $chart_title = $title_prefix.' Resistances';
                 }
                 elseif ($category_token == 'affinity'){
                     $chart_class = 'affinities';
-                    $chart_title = 'Afffiities';
+                    $chart_title = $title_prefix.' Afffiities';
                 }
                 elseif ($category_token == 'immunity'){
                     $chart_class = 'immunities';
-                    $chart_title = 'Immunities';
+                    $chart_title = $title_prefix.' Immunities';
                 }
 
                 // Define other attributes based on category
@@ -339,49 +348,72 @@ foreach ($type_stats_index AS $category_token => $category_counts){
                         <span class="counter"><?= $chart_total != 1 ? $chart_total.' '.ucfirst($chart_objects) : '1 '.ucfirst($chart_object)  ?> Total</span>
                     <? endif; ?>
                     <div class="text wrapper">
-                        <? if (!empty($chart_total)): ?>
-                            <? $key_counter = 0; ?>
-                            <table width="100%">
-                                <colgroup>
-                                    <? foreach($type_stats_index[$category_token] AS $type_token => $type_count): ?>
-                                        <? $type_info = $filtered_type_stats[$type_token]; ?>
-                                        <col width="<?= $type_info[$percent_key].'%' ?>">
-                                    <? endforeach; ?>
-                                </colgroup>
-                                <tbody>
-                                    <tr>
-                                        <? foreach($type_stats_index[$category_token] AS $type_token => $type_count): ?>
-                                            <? $type_info = $filtered_type_stats[$type_token]; ?>
-                                            <? $this_title = $type_info['type_name'].' Type | '.$type_info[$count_key].' / '.$chart_total.' '.ucfirst($chart_objects).' | '.$type_info[$percent_key].'% ';  ?>
-                                            <td>
-                                                <div class="type_percent <?= $chart_object ?>_type <?= $chart_object ?>_type_<?= $type_token ?>" title="<?= $this_title ?>">
-                                                    <?= $type_info[$count_key] ?>
-                                                    <span class="details"><?= ' / '.$chart_total.' '.ucfirst($chart_objects).' ('.$type_info[$percent_key].'%'.')' ?></span>
-                                                </div>
-                                                <div class="type_label" title="<?= $this_title ?>">
-                                                    <?= $type_info['type_name'] ?>
-                                                </div>
-                                            </td>
-                                            <? $key_counter++; ?>
-                                        <? endforeach; ?>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        <? else: ?>
-                            <table width="100%">
-                                <colgroup>
-                                    <col width="100%">
-                                </colgroup>
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <div class="type_percent <?= $chart_object ?>_type <?= $chart_object ?>_type_empty">0%</div>
-                                            <div class="type_label">n/a</div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        <? endif; ?>
+
+                        <div class="chart_wrapper">
+                            <canvas class="chart_canvas" data-kind="<?= $category_token ?>" width="400" height="400"></canvas>
+                            <script type="text/javascript">
+
+                                <?
+
+                                // Define the chart type and options
+                                if ($category_token == 'robot' || $category_token == 'ability'){
+                                    $chart_type = 'bar';
+                                    $chart_options = '{
+                                        legend: {
+                                            display: false
+                                            },
+                                        scales: {
+                                            yAxes: [{
+                                                ticks: {
+                                                    beginAtZero:true
+                                                    }
+                                                }]
+                                            }
+                                        }';
+
+                                } else {
+                                    $chart_type = 'pie';
+                                    $chart_options = '{
+                                        legend: {
+                                            display: false,
+                                            position: \'bottom\'
+                                            }
+                                        }';
+                                }
+
+                                // Collect the data for this type chart
+                                $type_labels = array();
+                                $type_counts = array();
+                                $type_backgrounds = array();
+                                $type_borders = array();
+                                foreach($type_stats_index[$category_token] AS $type_token => $type_count){
+                                    $type_info = $filtered_type_stats[$type_token];
+                                    $type_labels[] = $type_info['type_name'];
+                                    $type_counts[] = $type_info[$count_key];
+                                    $type_backgrounds[] = 'rgba('.trim($type_info['type_colour_light'], '[]').', 1.0)';
+                                    $type_borders[] = 'rgba('.trim($type_info['type_colour_dark'], '[]').', 1.0)';
+                                }
+
+                                ?>
+
+                                typeChartData['<?= $category_token ?>'] = {
+                                    type: '<?= $chart_type ?>',
+                                    data: {
+                                        labels: <?= json_encode($type_labels) ?>,
+                                        datasets: [{
+                                            label: '<?= ucfirst($chart_class) ?>',
+                                            data: <?= json_encode($type_counts) ?>,
+                                            backgroundColor: <?= json_encode($type_backgrounds) ?>,
+                                            borderColor: <?= json_encode($type_borders) ?>,
+                                            borderWidth: 1
+                                            }]
+                                        },
+                                    options: <?= $chart_options ?>
+                                    };
+
+                            </script>
+                        </div>
+
                     </div>
                 </div>
 
