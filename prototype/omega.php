e1bf88630485550490b90f795fd31cb18cacac8a
AdrianMarceau <adrian.marceau@gmail.com>
2017-01-22 16:42:15 -0500

    Added Spring Man to the list of omega factors to enable him in battles.

--- prototype/omega.php
+++ prototype/omega.php
@@ -89,7 +89,7 @@ $this_omega_factors_seven[] = array('robot' => 'freeze-man', 'field' => 'field',
 //$this_omega_factors_seven[] = array('robot' => 'junk-man', 'field' => 'field', 'type' => 'earth');
 //$this_omega_factors_seven[] = array('robot' => 'burst-man', 'field' => 'field', 'type' => 'explode');
 $this_omega_factors_seven[] = array('robot' => 'cloud-man', 'field' => 'field', 'type' => 'electric');
-//$this_omega_factors_seven[] = array('robot' => 'spring-man', 'field' => 'field', 'type' => 'impact');
+$this_omega_factors_seven[] = array('robot' => 'spring-man', 'field' => 'field', 'type' => 'impact');
 $this_omega_factors_seven[] = array('robot' => 'slash-man', 'field' => 'field', 'type' => 'nature');
 //$this_omega_factors_seven[] = array('robot' => 'shade-man', 'field' => 'field', 'type' => 'shadow');
 $this_omega_factors_seven[] = array('robot' => 'turbo-man', 'field' => 'field', 'type' => 'swift');
