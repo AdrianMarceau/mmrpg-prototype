f9ad838311685f390f768036acddca8ae38ea5b4
AdrianMarceau <adrian.marceau@gmail.com>
2017-02-12 12:14:59 -0500

    Updated markup on admin home to separate options into groups.

--- admin/home.php
+++ admin/home.php
@@ -1,49 +1,66 @@
 <? ob_start(); ?>
     <div class="breadcrumb">
         <a href="admin.php">Admin Panel</a>
-        &raquo; <a href="admin.php?action=home">Main Menu</a>
+        &raquo; <a href="admin.php?action=home">Home</a>
     </div>
     <?= !empty($this_error_markup) ? '<div style="margin: 0 auto 20px">'.$this_error_markup.'</div>' : '' ?>
+
     <ul class="adminhome">
-        <li>
+        <li class="top">
+            <strong>User Controls</strong>
+        </li>
+        <li class="item">
             <a href="admin.php?action=edit_users">Moderate Users</a>
             <em>update or modify user account info and permissions</em>
         </li>
-        <li>
-            <a href="admin.php?action=purge&amp;date=<?=MMRPG_CONFIG_CACHE_DATE?>&limit=10">Purge Inactive Users</a>
+        <li class="item">
+            <a href="admin.php?action=purge&amp;date=<?=MMRPG_CONFIG_CACHE_DATE?>">Purge Inactive Users</a>
             <em>purge user accounts with zero progress</em>
         </li>
-        <li>
-            <a href="admin.php?action=import_players&amp;limit=10">Refresh Player Database</a>
-            <em>rescan the players directory and update the database</em>
-        </li>
-        <li>
-            <a href="admin.php?action=import_robots&amp;limit=10">Refresh Robot Databases</a>
-            <em>rescan the robots directory and update the database</em>
-        </li>
-        <li>
-            <a href="admin.php?action=import_abilities&amp;limit=10">Refresh Ability Database</a>
-            <em>rescan the abilities directory and update the database</em>
-        </li>
-        <li>
-            <a href="admin.php?action=import_items&amp;limit=10">Refresh Item Database</a>
-            <em>rescan the items directory and update the database</em>
-        </li>
-        <li>
-            <a href="admin.php?action=import_fields&amp;limit=10">Refresh Field Database</a>
-            <em>rescan the fields directory and update the database</em>
-        </li>
-        <li>
-            <a href="admin.php?action=delete_cache">Delete Cached Files</a>
-            <em>delete cached markup and database objects</em>
-        </li>
-        <li>
-            <a href="admin.php?action=clear_sessions">Clear All Sessions</a>
-            <em>clear sessions and log out all users</em>
-        </li>
-        <li>
+        <li class="item">
             <a href="admin.php?action=update&amp;date=<?=MMRPG_CONFIG_CACHE_DATE?>&limit=1">Patch Save Files</a>
             <em>apply specific patches to existing save files</em>
         </li>
     </ul>
+
+    <ul class="adminhome">
+        <li class="top">
+            <strong>Database Update</strong>
+        </li>
+        <li class="item">
+            <a href="admin.php?action=import_players">Refresh Player Database</a>
+            <em>rescan the players directory and update the database</em>
+        </li>
+        <li class="item">
+            <a href="admin.php?action=import_robots">Refresh Robot Databases</a>
+            <em>rescan the robots directory and update the database</em>
+        </li>
+        <li class="item">
+            <a href="admin.php?action=import_abilities">Refresh Ability Database</a>
+            <em>rescan the abilities directory and update the database</em>
+        </li>
+        <li class="item">
+            <a href="admin.php?action=import_items">Refresh Item Database</a>
+            <em>rescan the items directory and update the database</em>
+        </li>
+        <li class="item">
+            <a href="admin.php?action=import_fields">Refresh Field Database</a>
+            <em>rescan the fields directory and update the database</em>
+        </li>
+    </ul>
+
+    <ul class="adminhome">
+        <li class="top">
+            <strong>Misc Tools</strong>
+        </li>
+        <li class="item">
+            <a href="admin.php?action=delete_cache">Delete Cached Files</a>
+            <em>delete cached markup and database objects</em>
+        </li>
+        <li class="item">
+            <a href="admin.php?action=clear_sessions">Clear All Sessions</a>
+            <em>clear sessions and log out all users</em>
+        </li>
+    </ul>
+
 <? $this_page_markup .= ob_get_clean(); ?>
