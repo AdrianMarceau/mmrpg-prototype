5f6c664be42b8b2005d172f00c7dcd261ab42080
AdrianMarceau <adrian.marceau@gmail.com>
2017-02-20 21:27:17 -0500

    Updated animations and damage output for the two omega abilities.

--- data/abilities/misc/omega-pulse.php
+++ data/abilities/misc/omega-pulse.php
@@ -7,29 +7,55 @@ $ability = array(
     'ability_group' => 'MMRPG/Weapons/Xtra',
     'ability_description' => 'The user taps into its hidden power to generate a pulse of elemental energy, sending it toward the target to inflict massive damage. This ability\'s elemental type appears to be unique for each robot.',
     'ability_energy' => 4,
-    'ability_damage' => 14,
+    'ability_damage' => 15,
     'ability_accuracy' => 100,
     'ability_function' => function($objects){
 
         // Extract all objects into the current scope
         extract($objects);
 
+        // Define this ability's attachment token
+        $this_attachment_token = 'ability_'.$this_ability->ability_token;
+        $this_attachment_info = array(
+            'class' => 'ability',
+            'ability_token' => $this_ability->ability_token,
+            'ability_frame' => 0,
+            'ability_frame_span' => 1,
+            'ability_frame_animate' => array(0, 1),
+            'ability_frame_offset' => array('x' => -10, 'y' => 0, 'z' => -1),
+            'attachment_token' => $this_attachment_token
+            );
+
+        // Add the background attachment to the user
+        $this_robot->set_attachment($this_attachment_token, $this_attachment_info);
+
         // Update the ability's target options and trigger
         $this_ability->target_options_update(array(
             'frame' => 'summon',
-            'success' => array(0, 105, 0, 10, $this_robot->print_name().' uses its '.$this_ability->print_name().'!')
+            'success' => array(2, 120, -20, 10, $this_robot->print_name().' generates an '.$this_ability->print_name().'!', 2)
             ));
         $this_robot->trigger_target($target_robot, $this_ability);
 
-        // Inflict damage on the opposing robot
+        // Update ability options and trigger damage on the target
+        $this_robot->set_frame('throw');
         $this_ability->damage_options_update(array(
             'kind' => 'energy',
+            'kickback' => array(15, 0, 0),
+            'success' => array(4, -160, -20, 10, 'The '.$this_ability->print_name().' collided with its target!', 2),
+            'failure' => array(4, -160, -20, -10, 'The '.$this_ability->print_name().' missed its target...', 2)
+            ));
+        $this_ability->recovery_options_update(array(
+            'kind' => 'energy',
             'kickback' => array(10, 0, 0),
-            'success' => array(0, -60, 0, 10, 'The '.$this_ability->print_name().' hit the target!'),
-            'failure' => array(0, -60, 0, -10, 'The '.$this_ability->print_name().' missed&hellip;')
+            'success' => array(4, -140, -20, 10, 'The '.$this_ability->print_name().' invigorated its target!', 2),
+            'failure' => array(4, -140, -20, -10, 'The '.$this_ability->print_name().' missed its target...', 2)
             ));
         $energy_damage_amount = $this_ability->ability_damage;
         $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+        $this_robot->set_frame('base');
+
+        // Remove the background attachment from the user
+        $this_robot->unset_attachment($this_attachment_token);
 
         // Return true on success
         return true;
