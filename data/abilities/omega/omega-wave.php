2cf93af030817af5a7a17d6c78878de9bc7a1e1a
AdrianMarceau <adrian.marceau@gmail.com>
2017-02-25 19:16:22 -0500

    Relocated omega pulse and wave functions into their own dedicated subfolder.

--- data/abilities/omega/omega-wave.php
+++ data/abilities/omega/omega-wave.php
@@ -1,13 +1,14 @@
 <?
-// OMEGA PULSE
+// OMEGA WAVE
 $ability = array(
-    'ability_name' => 'Omega Pulse',
-    'ability_token' => 'omega-pulse',
+    'ability_name' => 'Omega Wave',
+    'ability_token' => 'omega-wave',
     'ability_game' => 'MMRPG',
-    'ability_group' => 'MMRPG/Weapons/Omega',
-    'ability_description' => 'The user taps into its hidden power to generate a pulse of elemental energy. This ability\'s type appears to differ between robots.',
-    'ability_energy' => 4,
-    'ability_damage' => 16,
+    'ability_group' => 'MM00/Weapons/Omega',
+    'ability_description' => 'The user taps into its hidden powers to generate a wave of elemental energy. This ability\'s types appear to differ between robots and players.',
+    'ability_type' => '',
+    'ability_energy' => 8,
+    'ability_damage' => 32,
     'ability_accuracy' => 100,
     'ability_function' => function($objects){
 
@@ -28,7 +29,7 @@ $ability = array(
         // Update the ability's target options and trigger
         $this_ability->target_options_update(array(
             'frame' => 'throw',
-            'success' => array(2, 120, -20, 10, $this_robot->print_name().' releases an '.$this_ability->print_name().'!', 2)
+            'success' => array(2, 140, -20, 10, $this_robot->print_name().' releases an '.$this_ability->print_name().'!', 3)
             ));
         $target_options = array('prevent_default_text' => true);
         $this_robot->trigger_target($target_robot, $this_ability, $target_options);
@@ -36,15 +37,15 @@ $ability = array(
         // Update ability options and trigger damage on the target
         $this_ability->damage_options_update(array(
             'kind' => 'energy',
-            'kickback' => array(15, 0, 0),
-            'success' => array(4, -160, -20, 10, 'The '.$this_ability->print_name().' collided with the target!', 2),
-            'failure' => array(4, -160, -20, -10, 'The '.$this_ability->print_name().' missed the target...', 2)
+            'kickback' => array(30, 0, 0),
+            'success' => array(5, -220, -20, 10, 'The '.$this_ability->print_name().' collided with the target!', 3),
+            'failure' => array(5, -220, -20, -10, 'The '.$this_ability->print_name().' missed the target...', 3)
             ));
         $this_ability->recovery_options_update(array(
             'kind' => 'energy',
-            'kickback' => array(10, 0, 0),
-            'success' => array(4, -140, -20, 10, 'The '.$this_ability->print_name().' invigorated the target!', 2),
-            'failure' => array(4, -140, -20, -10, 'The '.$this_ability->print_name().' missed the target...', 2)
+            'kickback' => array(20, 0, 0),
+            'success' => array(5, -180, -20, 10, 'The '.$this_ability->print_name().' invigorated the target!', 3),
+            'failure' => array(5, -180, -20, -10, 'The '.$this_ability->print_name().' missed the target...', 3)
             ));
         $energy_damage_amount = $this_ability->ability_damage;
         $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
@@ -67,6 +68,18 @@ $ability = array(
         $robot_ability_image = $this_ability->get_base_image().'_'.$robot_hidden_power;
         $this_ability->set_type($robot_hidden_power);
         $this_ability->set_image($robot_ability_image);
+
+        // Generate this player's omega string, collect their hidden power, and update type2
+        $player_omega_string = rpg_game::generate_omega_string($this_player->user_token, $this_player->player_token, 'robot');
+        $player_hidden_power = rpg_game::select_omega_value($player_omega_string, $hidden_power_types);
+        $player_ability_image = $this_ability->get_base_image().'_'.$player_hidden_power.'2';
+        if ($player_hidden_power != $robot_hidden_power){
+            $this_ability->set_type2($player_hidden_power);
+            $this_ability->set_image2($player_ability_image);
+        } else {
+            $this_ability->set_type2('');
+            $this_ability->set_image2('');
+        }
 
         // If the user is holding a Target Module, allow bench targeting
         if ($this_robot->has_item('target-module')){ $this_ability->set_target('select_target'); }
