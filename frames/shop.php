9b2163e99317e89df1164792d0bf9318d9b01b1d
AdrianMarceau <adrian.marceau@gmail.com>
2017-02-26 21:22:41 -0500

    Updated shop keepers to buy items/stars at a higher value if the type matches their omega value.

--- frames/shop.php
+++ frames/shop.php
@@ -184,18 +184,11 @@ if (true){
                     <?
 
                     // Only omega indicators if the abilities have been unlocked
-                    if (rpg_game::omega_abilities_unlocked()){
+                    if (!empty($shop_info['shop_hidden_power'])){
 
-                        // Collect possible hidden power types
-                        $hidden_power_types = rpg_type::get_hidden_powers('stats');
-
-                        // Generate this shop's omega string, collect it's hidden power
-                        $username_string = rpg_game::get_user_string();
-                        $shop_omega_string = rpg_game::generate_omega_string($username_string, 'shop', $shop_token);
-                        $shop_hidden_power = rpg_game::select_omega_value($shop_omega_string, $hidden_power_types);
-
-                        // Print out the omega indicators for the player
-                        echo '<span class="omega player_type type_'.$shop_hidden_power.'" title="Omega Seed || [['.ucfirst($shop_hidden_power).' Type]]">'.$shop_hidden_power.'</span>'.PHP_EOL;
+                        // Print out the omega indicators for the shop
+                        echo '<span class="omega player_type type_'.$shop_info['shop_hidden_power'].'" title="Omega Seed || [['.ucfirst($shop_info['shop_hidden_power']).' Type]]">'.$shop_info['shop_hidden_power'].'</span>'.PHP_EOL;
+                        //echo 'omega('.$shop_info['shop_omega_string'].')';
 
                     }
 
