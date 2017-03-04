76dcebdaf53883bc549ff745de2ed2bf8e3de92e
AdrianMarceau <adrian.marceau@gmail.com>
2017-03-04 16:08:43 -0500

    Updated the unlock script to reward the user with the Omega Seed after completing all three campaigns.

--- prototype/unlocks.php
+++ prototype/unlocks.php
@@ -970,6 +970,24 @@ if (!mmrpg_prototype_item_unlocked('field-codes')
 
 
 /*
+ * MULTI DR. EVENT ITEMS
+ */
+
+// Unlock the OMEGA SEED after all three Drs. have completed the prototype
+if (!mmrpg_prototype_item_unlocked('omega-seed')
+    && mmrpg_prototype_complete() >= 3
+    ){
+
+    // Unlock the Omega Seed and generate the required event details
+    mmrpg_game_unlock_item('omega-seed', array(
+        'positive_word' => 'What\'s this?',
+        'event_text' => 'A new item has been unlocked...'
+        ));
+
+}
+
+
+/*
  * COMMON OPTIONS
  */
 
