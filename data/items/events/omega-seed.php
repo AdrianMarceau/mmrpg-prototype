8aff16f72030b0cbd2eb5325823b4b8d7ea84ef3
AdrianMarceau <adrian.marceau@gmail.com>
2017-03-04 14:43:20 -0500

    Created new Omega Seed event item.  Will unlock certain functionality in the post-game.

---
+++ data/items/events/omega-seed.php
@@ -0,0 +1,23 @@
+<?
+// ITEM : OMEGA SEED
+$item = array(
+    'item_name' => 'Omega Seed',
+    'item_token' => 'omega-seed',
+    'item_game' => 'MMRPG',
+    'item_group' => 'MMRPG/Items/OmegaSeed',
+    'item_class' => 'item',
+    'item_subclass' => 'event',
+    'item_type' => '',
+    'item_type2' => '',
+    'item_description' => 'A small seed-like object of unknown origin, this item appears to reveal and grant control over the hidden Omega influences in the prototype.',
+    'item_energy' => 0,
+    'item_speed' => 10,
+    'item_accuracy' => 100,
+    'item_function' => function($objects){
+
+        // Return true on success
+        return true;
+
+    }
+    );
+?>
