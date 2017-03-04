10f2c41043d545f36d98fc53b26353478c0ba102
AdrianMarceau <adrian.marceau@gmail.com>
2017-03-03 22:16:02 -0500

    Recoloured the Swift type to be more distinct among the others and more easily recognizable at a glance.

--- data/types/swift.php
+++ data/types/swift.php
@@ -3,7 +3,7 @@
 $type = array(
   'type_name' => 'Swift',
   'type_token' => 'swift',
-  'type_colour_dark' => array(180, 71, 43),
-  'type_colour_light' => array(179, 65, 36)
+  'type_colour_dark' => array(197, 102, 73),
+  'type_colour_light' => array(187, 88, 58)
   );
 ?>
