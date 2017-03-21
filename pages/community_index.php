70dab346af5e6b00703090adba13eda0a11cfdbd
AdrianMarceau <adrian.marceau@gmail.com>
2017-03-20 21:09:31 -0400

    Updated a few responsive styles for the community headers so the buttons are easier to click.

--- pages/community_index.php
+++ pages/community_index.php
@@ -68,7 +68,7 @@ foreach ($this_categories_index AS $this_category_id => $this_category_info){
     <h2 class="subheader thread_name field_type_<?= MMRPG_SETTINGS_CURRENT_FIELDTYPE ?>" style="clear: both; <?= $this_category_key > 0 ? 'margin-top: 6px; ' : '' ?>">
         <a class="link" href="<?= 'community/'.$this_category_info['category_token'].'/' ?>" style="display: inline;"><?= $this_category_info['category_name'] ?></a>
         <span class="count">( <?= ($this_threads_count > MMRPG_SETTINGS_THREADS_RECENT  ? MMRPG_SETTINGS_THREADS_RECENT.' of ' : '').($this_threads_count == '1' ? '1 '.($this_category_info['category_id'] != 0 ? 'Discussion' : 'Message') : $this_threads_count.' '.($this_category_info['category_id'] != 0 ? 'Discussions' : 'Messages'))  ?> )</span>
-        <?= !empty($temp_header_links) ? implode("\n", $temp_header_links) : '' ?>
+        <?= !empty($temp_header_links) ? '<span class="br"></span>'.PHP_EOL.implode(PHP_EOL, $temp_header_links) : '' ?>
     </h2>
     <div class="subbody threads" style="overflow: hidden; margin-bottom: 25px;">
         <?
