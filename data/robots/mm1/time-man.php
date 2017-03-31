a3d511d6211813bdfeb201590bd931f4dfd3a87d
AdrianMarceau <adrian.marceau@gmail.com>
2017-03-30 20:10:56 -0400

    Updated all MM1 robots with unlockable flags as they're fully complete.

--- data/robots/mm1/time-man.php
+++ data/robots/mm1/time-man.php
@@ -41,6 +41,7 @@ $robot = array(
         'battle_taunt' => '',
         'battle_victory' => '',
         'battle_defeat' => ''
-        )
+        ),
+    'robot_flag_unlockable' => true
     );
 ?>
