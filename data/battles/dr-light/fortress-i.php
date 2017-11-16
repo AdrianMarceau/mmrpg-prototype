00493c69138936c87c01d33cb2ec023ecbed2298
AdrianMarceau <adrian.marceau@gmail.com>
2017-11-15 20:34:38 -0500

    Updated chapter 3 fortress missions to use darkness alts.
    Doesn't make story sense to have Dr. Light show up as an opponent for Cossack so decided to change all rival battles to use darkness alts instead and remove player character.

--- data/battles/dr-light/fortress-i.php
+++ data/battles/dr-light/fortress-i.php
@@ -1,29 +1,47 @@
 <?
-// PROTOTYPE BATTLE 5 : VS DR WILY
+// PROTOTYPE BATTLE 5 : VS DARK BASS & DISCO
 $battle = array(
-  'battle_name' => 'Chapter Three Rival Battle',
-  'battle_size' => '1x4',
-  'battle_encore' => true,
-  'battle_description' => 'Defeat Dr. Wily\'s Bass and Disco!',
-  'battle_turns' => (MMRPG_SETTINGS_BATTLETURNS_PERROBOT * 2),
-  'battle_points' => (MMRPG_SETTINGS_BATTLEPOINTS_PERLEVEL * 10 * 2),
-  'battle_field_base' => array('field_id' => 100, 'field_token' => 'wily-castle', 'field_name' => 'Wily Castle', 'field_music' => 'wily-castle'),
-  'battle_target_player' => array(
-    'player_id' => MMRPG_SETTINGS_TARGET_PLAYERID,
-    'player_token' => 'dr-wily',
-    'player_switch' => 1.5,
-    'player_robots' => array(
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 1), 'robot_token' => 'bass', 'robot_level' => 10, 'robot_item' => 'weapon-pellet', 'robot_abilities' => array('bass-buster', 'bass-crush', 'bass-baroque', 'buster-shot', 'leaf-shield', 'atomic-fire', 'bubble-lead', 'metal-blade', 'quick-boomerang', 'flash-stopper', 'crash-bomber', 'air-shooter')),
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 2), 'robot_token' => 'disco', 'robot_level' => 10, 'robot_item' => 'energy-pellet', 'robot_abilities' => array('disco-buster', 'buster-shot', 'attack-break', 'defense-break', 'speed-break', 'energy-break'))
-      )
-    ),
-  'battle_rewards' => array(
-    'robots' => array(
-      ),
-    'abilities' => array(
-      ),
-    'items' => array(
-      )
-    )
-  );
+    'battle_name' => 'Chapter Three Rival Battle',
+    'battle_size' => '1x4',
+    'battle_encore' => true,
+    'battle_description' => 'Defeat the darkness and liberate Wily Castle!',
+    'battle_turns' => (MMRPG_SETTINGS_BATTLETURNS_PERROBOT * 2),
+    'battle_points' => (MMRPG_SETTINGS_BATTLEPOINTS_PERLEVEL * 10 * 2),
+    'battle_field_base' => array('field_id' => 100, 'field_token' => 'wily-castle', 'field_name' => 'Wily Castle', 'field_music' => 'wily-castle'),
+    'battle_target_player' => array(
+        'player_id' => MMRPG_SETTINGS_TARGET_PLAYERID,
+        'player_token' => 'player',
+        'player_switch' => 1.5,
+        'player_robots' => array(
+            array(
+                'robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 1),
+                'robot_token' => 'bass',
+                'robot_name' => 'Bass Σ',
+                'robot_image' => 'bass_alt9',
+                'robot_core' => 'empty',
+                'robot_level' => 10,
+                'robot_item' => 'weapon-pellet',
+                'robot_abilities' => array('bass-buster', 'bass-crush', 'bass-baroque', 'buster-shot', 'leaf-shield', 'atomic-fire', 'bubble-lead', 'metal-blade', 'quick-boomerang', 'flash-stopper', 'crash-bomber', 'air-shooter')
+                ),
+            array(
+                'robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 2),
+                'robot_token' => 'disco',
+                'robot_name' => 'Disco Σ',
+                'robot_image' => 'disco_alt9',
+                'robot_core' => 'empty',
+                'robot_level' => 10,
+                'robot_item' => 'energy-pellet',
+                'robot_abilities' => array('disco-buster', 'buster-shot', 'attack-break', 'defense-break', 'speed-break', 'energy-break')
+                )
+            )
+        ),
+    'battle_rewards' => array(
+        'robots' => array(
+            ),
+        'abilities' => array(
+            ),
+        'items' => array(
+            )
+        )
+    );
 ?>
--- data/battles/dr-wily/fortress-i.php
+++ data/battles/dr-wily/fortress-i.php
@@ -1,29 +1,47 @@
 <?
-// PROTOTYPE BATTLE 5 : VS DR COSSACK
+// PROTOTYPE BATTLE 5 : VS DARK PROTO & RHYTHM
 $battle = array(
-  'battle_name' => 'Chapter Three Rival Battle',
-  'battle_size' => '1x4',
-  'battle_encore' => true,
-  'battle_description' => 'Defeat Dr. Cossack\'s Proto Man and Rhythm!',
-  'battle_turns' => (MMRPG_SETTINGS_BATTLETURNS_PERROBOT * 2),
-  'battle_points' => (MMRPG_SETTINGS_BATTLEPOINTS_PERLEVEL * 20 * 2),
-  'battle_field_base' => array('field_id' => 100, 'field_token' => 'cossack-citadel', 'field_name' => 'Cossack Citadel', 'field_music' => 'cossack-citadel'),
-  'battle_target_player' => array(
-    'player_id' => MMRPG_SETTINGS_TARGET_PLAYERID,
-    'player_token' => 'dr-cossack',
-    'player_switch' => 1.5,
-    'player_robots' => array(
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 1), 'robot_token' => 'proto-man', 'robot_level' => 20, 'robot_item' => 'weapon-capsule', 'robot_abilities' => array('proto-buster', 'proto-shield', 'proto-strike', 'buster-shot', 'dive-torpedo', 'skull-barrier', 'bright-burst', 'drill-blitz', 'rain-flush', 'ring-boomerang', 'pharaoh-shot', 'dust-crusher')),
-      array('robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 2), 'robot_token' => 'rhythm', 'robot_level' => 20, 'robot_item' => 'energy-capsule', 'robot_abilities' => array('rhythm-buster', 'buster-shot', 'attack-swap', 'defense-swap', 'speed-swap', 'energy-swap'))
-      )
-    ),
-  'battle_rewards' => array(
-    'robots' => array(
-      ),
-    'abilities' => array(
-      ),
-    'items' => array(
-      )
-    )
-  );
+    'battle_name' => 'Chapter Three Rival Battle',
+    'battle_size' => '1x4',
+    'battle_encore' => true,
+    'battle_description' => 'Defeat the darkness and liberate Cossack Citadel!',
+    'battle_turns' => (MMRPG_SETTINGS_BATTLETURNS_PERROBOT * 2),
+    'battle_points' => (MMRPG_SETTINGS_BATTLEPOINTS_PERLEVEL * 20 * 2),
+    'battle_field_base' => array('field_id' => 100, 'field_token' => 'cossack-citadel', 'field_name' => 'Cossack Citadel', 'field_music' => 'cossack-citadel'),
+    'battle_target_player' => array(
+        'player_id' => MMRPG_SETTINGS_TARGET_PLAYERID,
+        'player_token' => 'player',
+        'player_switch' => 1.5,
+        'player_robots' => array(
+            array(
+                'robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 1),
+                'robot_token' => 'proto-man',
+                'robot_name' => 'Proto Man Σ',
+                'robot_image' => 'proto-man_alt9',
+                'robot_core' => 'empty',
+                'robot_level' => 20,
+                'robot_item' => 'weapon-capsule',
+                'robot_abilities' => array('proto-buster', 'proto-shield', 'proto-strike', 'buster-shot', 'dive-torpedo', 'skull-barrier', 'bright-burst', 'drill-blitz', 'rain-flush', 'ring-boomerang', 'pharaoh-shot', 'dust-crusher')
+                ),
+            array(
+                'robot_id' => (MMRPG_SETTINGS_TARGET_PLAYERID + 2),
+                'robot_token' => 'rhythm',
+                'robot_name' => 'Rhythm Σ',
+                'robot_image' => 'rhythm_alt9',
+                'robot_core' => 'empty',
+                'robot_level' => 20,
+                'robot_item' => 'energy-capsule',
+                'robot_abilities' => array('rhythm-buster', 'buster-shot', 'attack-swap', 'defense-swap', 'speed-swap', 'energy-swap')
+                )
+            )
+        ),
+    'battle_rewards' => array(
+        'robots' => array(
+            ),
+        'abilities' => array(
+            ),
+        'items' => array(
+            )
+        )
+    );
 ?>
