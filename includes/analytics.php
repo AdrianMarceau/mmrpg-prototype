d45f6b8bc54e27e7bcdb9e5ea66a0ca7e2b51fb3
AdrianMarceau <adrian.marceau@gmail.com>
2018-02-04 10:24:25 -0500

    Relocated analaytics ID to config file.

--- includes/analytics.php
+++ includes/analytics.php
@@ -1,12 +1,10 @@
 <? if (MMRPG_CONFIG_IS_LIVE): ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?= LEGACY_MMRPG_GA_ACCOUNTID ?>"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-28757226-2', 'megamanpoweredup.net');
-  ga('send', 'pageview');
-
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '<?= LEGACY_MMRPG_GA_ACCOUNTID ?>');
 </script>
 <? endif; ?>
