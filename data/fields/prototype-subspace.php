168edfad9286c08a9cee1e65e00b8dccb4e55ac4
AdrianMarceau <adrian.marceau@gmail.com>
2018-02-09 21:45:23 -0500

    Added the Prototype Subspace field object to the game, may tweak settings later.

---
+++ data/fields/prototype-subspace.php
@@ -0,0 +1,17 @@
+<?
+// PROTOTYPE SUBSPACE
+$field = array(
+  'field_name' => 'Prototype Subspace',
+  'field_token' => 'prototype-subspace',
+  'field_type' => 'empty',
+  'field_class' => 'system',
+  'field_game' => 'MMRPG',
+  'field_number' => 'RPG-000',
+  'field_description' => 'A strange and unstable area of subspace in the prototype.',
+  'field_background' => 'prototype-subspace',
+  'field_foreground' => 'prototype-subspace',
+  'field_music' => 'prototype-subspace',
+  'field_background_frame' => array(0), //array(0,1),
+  'field_foreground_frame' => array(0)
+  );
+?>
