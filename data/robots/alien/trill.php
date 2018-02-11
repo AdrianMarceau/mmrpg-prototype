98ec1ded80397363b3702657f5f2d28cd805a196
AdrianMarceau <adrian.marceau@gmail.com>
2018-02-11 10:19:17 -0500

    Updated Trill's onload logic to automatically change alts based on current highest stat.

--- data/robots/alien/trill.php
+++ data/robots/alien/trill.php
@@ -43,6 +43,34 @@ $robot = array(
         'battle_taunt' => '',
         'battle_victory' => '',
         'battle_defeat' => ''
-        )
+        ),
+    'robot_function_onload' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Collect this robot's main stats for comparrison
+        $attack = $this_robot->robot_attack;
+        $defense = $this_robot->robot_defense;
+        $speed = $this_robot->robot_speed;
+
+        // Check if any of this robot's stats are higher than the others
+        if ($attack > $defense && $attack > $speed){ $dominant_type = 'attack'; }
+        elseif ($defense > $attack && $defense > $speed){ $dominant_type = 'defense'; }
+        elseif ($speed > $attack && $speed > $defense){ $dominant_type = 'speed'; }
+        else { $dominant_type = 'energy'; }
+
+        // Define this robot's image token based on dominant stat
+        if ($dominant_type == 'attack'){ $alt_image_token = $this_robot->robot_token.'_alt'; }
+        elseif ($dominant_type == 'defense'){ $alt_image_token = $this_robot->robot_token.'_alt2'; }
+        elseif ($dominant_type == 'speed'){ $alt_image_token = $this_robot->robot_token.'_alt3'; }
+        else { $alt_image_token = $this_robot->robot_token; }
+
+        // If this robot does not have the approriate image right now, change alts
+        if ($this_robot->robot_image != $alt_image_token){
+            $this_robot->set_image($alt_image_token);
+            }
+
+        }
     );
 ?>
