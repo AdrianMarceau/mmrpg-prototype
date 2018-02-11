4993930d88228335892dd8fa6ed84fc2b72a9db0
AdrianMarceau <adrian.marceau@gmail.com>
2018-02-11 10:18:50 -0500

    Adding the Dark Frag mecha to the game so it can be used in future battles.

---
+++ data/robots/mecha/dark-frag.php
@@ -0,0 +1,33 @@
+<?
+// DARK FRAG
+$robot = array(
+    'robot_number' => 'EXDN-001',
+    'robot_class' => 'mecha',
+    'robot_game' => 'MMRPG',
+    'robot_name' => 'Dark Frag',
+    'robot_token' => 'dark-frag',
+    'robot_image_editor' => 412,
+    'robot_core' => 'empty',
+    'robot_field' => 'prototype-subspace',
+    'robot_description' => 'Crystaline Energy Fragment',
+    'robot_energy' => 100,
+    'robot_attack' => 100,
+    'robot_defense' => 100,
+    'robot_speed' => 100,
+    'robot_weaknesses' => array('empty'),
+    'robot_immunities' => array('*'),
+    'robot_abilities' => array('energy-boost', 'energy-break'),
+    'robot_rewards' => array(
+        'abilities' => array(
+                array('level' => 0, 'token' => 'energy-boost'),
+                array('level' => 0, 'token' => 'energy-break')
+            )
+        ),
+    'robot_quotes' => array(
+        'battle_start' => '...',
+        'battle_taunt' => '...',
+        'battle_victory' => '...',
+        'battle_defeat' => '...'
+        )
+    );
+?>
