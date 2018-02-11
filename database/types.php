b91dda714b300d8e707b684e206281736668f40e
AdrianMarceau <adrian.marceau@gmail.com>
2018-02-11 11:23:33 -0500

    Updated the front-end database types include to also pull hidden types but separate into own array.

--- database/types.php
+++ database/types.php
@@ -2,14 +2,30 @@
 // TYPES DATABASE
 
 // Define the index of types for the game
-$mmrpg_database_types = rpg_type::get_index();
+$mmrpg_database_types = rpg_type::get_index(true);
 uasort($mmrpg_database_types, function($t1, $t2){
   if ($t1['type_order'] > $t2['type_order']){ return 1; }
   elseif ($t1['type_order'] < $t2['type_order']){ return -1; }
   else { return 0; }
 });
+
+// Extract and separate all the special types from the list
+$mmrpg_database_types_hidden = array();
+foreach ($mmrpg_database_types AS $token => $info){
+    if ($info['type_class'] != 'normal' && $info['type_token'] != 'none'){
+        $mmrpg_database_types_hidden[$token] = $info;
+        unset($mmrpg_database_types[$token]);
+    }
+}
+
+// Count the number of normal, public types to display
 $mmrpg_database_types_count = count($mmrpg_database_types);
 $mmrpg_database_types_count_added = 1;
 $mmrpg_database_types_count_actual = count($mmrpg_database_types);
+
+// Count the number of hidden special types to not-display
+$mmrpg_database_types_hidden_count = count($mmrpg_database_types);
+$mmrpg_database_types_hidden_count_added = 1;
+$mmrpg_database_types_hidden_count_actual = count($mmrpg_database_types);
 
 ?>
