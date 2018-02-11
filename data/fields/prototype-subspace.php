86ad40bfa0f417f4a422bb3fa6e91dd0d6203341
AdrianMarceau <adrian.marceau@gmail.com>
2018-02-11 11:59:56 -0500

    Updated the Prototype Subspace field to include Dark Frags in the background and be owned by Trill.

--- data/fields/prototype-subspace.php
+++ data/fields/prototype-subspace.php
@@ -1,17 +1,31 @@
 <?
 // PROTOTYPE SUBSPACE
 $field = array(
-  'field_name' => 'Prototype Subspace',
-  'field_token' => 'prototype-subspace',
-  'field_type' => 'empty',
-  'field_class' => 'system',
-  'field_game' => 'MMRPG',
-  'field_number' => 'RPG-000',
-  'field_description' => 'A strange and unstable area of subspace in the prototype.',
-  'field_background' => 'prototype-subspace',
-  'field_foreground' => 'prototype-subspace',
-  'field_music' => 'prototype-subspace',
-  'field_background_frame' => array(0), //array(0,1),
-  'field_foreground_frame' => array(0)
-  );
+    'field_name' => 'Prototype Subspace',
+    'field_token' => 'prototype-subspace',
+    'field_type' => 'empty',
+    'field_class' => 'event',
+    'field_game' => 'MMRPG',
+    'field_number' => 'RPG-000',
+    'field_description' => 'A strange and unstable area of subspace in the prototype.',
+    'field_master' => 'trill',
+    'field_mechas' => array('dark-frag'),
+    'field_background' => 'prototype-subspace',
+    'field_foreground' => 'prototype-subspace',
+    'field_music' => 'prototype-subspace',
+    'field_background_frame' => array(0), //array(0,1),
+    'field_foreground_frame' => array(0),
+    'field_background_attachments' => array(
+        'mecha-01' => array('class' => 'robot', 'size' => 40, 'offset_x' => 0, 'offset_y' => 120, 'robot_token' => 'dark-frag', 'robot_frame' => array(0), 'robot_direction' => 'right'),
+        'mecha-02' => array('class' => 'robot', 'size' => 40, 'offset_x' => 200, 'offset_y' => 120, 'robot_token' => 'dark-frag', 'robot_frame' => array(0), 'robot_direction' => 'right'),
+        'mecha-03' => array('class' => 'robot', 'size' => 40, 'offset_x' => 200, 'offset_y' => 120, 'robot_token' => 'dark-frag', 'robot_frame' => array(0), 'robot_direction' => 'left'),
+        'mecha-04' => array('class' => 'robot', 'size' => 40, 'offset_x' => 0, 'offset_y' => 120, 'robot_token' => 'dark-frag', 'robot_frame' => array(0), 'robot_direction' => 'left')
+        ),
+    'field_foreground_attachments' => array(
+        'mecha-05' => array('class' => 'robot', 'size' => 80, 'offset_x' => -100, 'offset_y' => 0, 'robot_token' => 'dark-frag', 'robot_frame' => array(0), 'robot_direction' => 'right'),
+        'mecha-06' => array('class' => 'robot', 'size' => 80, 'offset_x' => 100, 'offset_y' => 0, 'robot_token' => 'dark-frag', 'robot_frame' => array(0), 'robot_direction' => 'right'),
+        'mecha-07' => array('class' => 'robot', 'size' => 80, 'offset_x' => 100, 'offset_y' => 0, 'robot_token' => 'dark-frag', 'robot_frame' => array(0), 'robot_direction' => 'left'),
+        'mecha-08' => array('class' => 'robot', 'size' => 80, 'offset_x' => -100, 'offset_y' => 0, 'robot_token' => 'dark-frag', 'robot_frame' => array(0), 'robot_direction' => 'left')
+        )
+    );
 ?>
