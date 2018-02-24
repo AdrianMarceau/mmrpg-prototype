b6d8f08a43cd7b9eac22fdb82421b3b9694bde63
AdrianMarceau <adrian.marceau@gmail.com>
2018-02-24 18:42:45 -0500

    Tweaked colour values for Empty type.

--- data/types/empty.php
+++ data/types/empty.php
@@ -4,7 +4,9 @@ $type = array(
   'type_name' => 'Empty',
   'type_token' => 'empty',
   'type_class' => 'special',
-  'type_colour_dark' => array(22, 22, 22),
-  'type_colour_light' => array(32, 32, 32)
+  'type_colour_dark' => array(48, 48, 54),
+  'type_colour_light' => array(36, 36, 41),
+  //'type_colour_dark' => array(22, 22, 22),
+  //'type_colour_light' => array(32, 32, 32)
   );
 ?>
