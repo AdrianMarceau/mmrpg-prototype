977eaa4d1de48d821f45b56929a6878fad5fd32c
AdrianMarceau <adrian.marceau@gmail.com>
2018-12-09 16:26:29 -0500

    Shop fields no longer require pre-scanning the master

--- frames/shop_selling_fields.php
+++ frames/shop_selling_fields.php
@@ -57,22 +57,10 @@
                     if (!empty($field_info['field_type2'])){ $field_info_type .= '_'.$field_info['field_type2']; }
                     if (!empty($field_info['field_master']) && !empty($mmrpg_database_robots[$field_info['field_master']])){ $field_info_master = $mmrpg_database_robots[$field_info['field_master']]; }
                     $field_info_unlocked = in_array($field_info_token, $field_list_unlocked) ? true : false;
-                    $field_info_hidden = empty($_SESSION['GAME']['values']['robot_database'][$field_info_master['robot_token']]['robot_scanned']) ? true : false;
-                    if ($field_info_hidden){
-                        $field_info_name = preg_replace('/[a-z]{1}/i', '?', $field_info_name);
-                        $global_item_quantities['field-'.$field_info_token] = 1;
-                        $global_item_prices['buy']['field-'.$field_info_token] = 0;
-                        $temp_master_name = 'an undisclosed robot';
-                        //if ($field_info_master){ $temp_master_name = preg_match('/^(a|e|i|o|u)/i', $field_info_master['robot_name']) ? 'an '.$field_info_master['robot_name'] : 'a '.$field_info_master['robot_name']; }
-                        if ($field_info_master){ $temp_master_name = preg_match('/^(a|e|i|o|u)/i', $field_info_master['robot_name']) ? 'an '.$field_info_master['robot_name'] : 'a '.$field_info_master['robot_name']; }
-                        $temp_info_tooltip = 'My apologies, but I haven\'t finished this one yet. If you encounter '.$temp_master_name.' in battle, would you mind scanning its data for me?';
-                    } else {
-                        $global_item_quantities['field-'.$field_info_token] = $field_info_unlocked ? 1 : 0;
-                        $global_item_prices['buy']['field-'.$field_info_token] = $field_info_unlocked ? 0 : $field_info_price;
-                        $temp_info_tooltip = rpg_field::print_editor_title_markup($player_info, $field_info);
-                        $temp_info_tooltip = htmlentities($temp_info_tooltip, ENT_QUOTES, 'UTF-8', true);
-                    }
-
+                    $global_item_quantities['field-'.$field_info_token] = $field_info_unlocked ? 1 : 0;
+                    $global_item_prices['buy']['field-'.$field_info_token] = $field_info_unlocked ? 0 : $field_info_price;
+                    $temp_info_tooltip = rpg_field::print_editor_title_markup($player_info, $field_info);
+                    $temp_info_tooltip = htmlentities($temp_info_tooltip, ENT_QUOTES, 'UTF-8', true);
                     if ($field_info_unlocked){ $field_info_price = 0; }
 
                     $field_counter++;
