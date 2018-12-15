651f33d0d18afac5b821f48d66cac2076980b615
AdrianMarceau <adrian.marceau@gmail.com>
2018-12-15 14:31:53 -0500

    Rebalanced WE costs for Stat Support + Assault abilities

--- data/abilities/support/attack-support.php
+++ data/abilities/support/attack-support.php
@@ -6,7 +6,7 @@ $ability = array(
     'ability_game' => 'MMRPG',
     'ability_group' => 'MMRPG/Support/Attack2',
     'ability_description' => 'The user triggers weapon optimizations for all robots on their side of the field to raise their attack stats!',
-    'ability_energy' => 6,
+    'ability_energy' => 10,
     'ability_accuracy' => 100,
     'ability_function' => function($objects){
 
