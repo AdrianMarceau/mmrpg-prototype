8d4623f6c42e29caa6d402b7a6adabbf894109c2
AdrianMarceau <adrian.marceau@gmail.com>
2018-12-15 01:25:01 -0500

    Updated Attack Boost & Break to use new mechanics

--- data/abilities/support/attack-break.php
+++ data/abilities/support/attack-break.php
@@ -5,33 +5,20 @@ $ability = array(
     'ability_token' => 'attack-break',
     'ability_game' => 'MMRPG',
     'ability_group' => 'MMRPG/Support/Attack',
-    'ability_description' => 'The user breaks down the target\'s weapon systems, lowering its attack by up to {DAMAGE}%!',
+    'ability_description' => 'The user breaks down the target\'s weapon systems, sharply lowering its attack stat!',
     'ability_energy' => 4,
-    'ability_damage' => 33,
-    'ability_damage_percent' => true,
-    'ability_accuracy' => 98,
+    'ability_accuracy' => 100,
     'ability_function' => function($objects){
 
         // Extract all objects into the current scope
         extract($objects);
 
         // Target the opposing robot
-        $this_ability->target_options_update(array(
-            'frame' => 'summon',
-            'success' => array(0, -2, 0, -10, $this_robot->print_name().' uses '.$this_ability->print_name().'!')
-            ));
+        $this_ability->target_options_update(array('frame' => 'summon', 'success' => array(0, -2, 0, -10, $this_robot->print_name().' uses '.$this_ability->print_name().'!')));
         $this_robot->trigger_target($target_robot, $this_ability);
 
-        // Decrease the target robot's attack stat
-        $this_ability->damage_options_update(array(
-            'kind' => 'attack',
-            'percent' => true,
-            'kickback' => array(10, 0, 0),
-            'success' => array(0, -2, 0, -10, $target_robot->print_name().'&#39;s weapons were damaged!'),
-            'failure' => array(9, -2, 0, -10, 'It had no effect on '.$target_robot->print_name().'&hellip;')
-            ));
-        $attack_damage_amount = ceil($target_robot->robot_attack * ($this_ability->ability_damage / 100));
-        $target_robot->trigger_damage($this_robot, $this_ability, $attack_damage_amount);
+        // Call the global stat boost function with customized options
+        return rpg_ability::ability_function_stat_break($target_robot, 'attack', 2, $this_ability);
 
         // Return true on success
         return true;
