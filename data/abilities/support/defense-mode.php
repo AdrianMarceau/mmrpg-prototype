7292e0616100e1e8d436d0c75e10603325fb88c7
AdrianMarceau <adrian.marceau@gmail.com>
2018-12-15 18:09:24 -0500

    Updated Stat Mode abilities to work with new mechanics

--- data/abilities/support/defense-mode.php
+++ data/abilities/support/defense-mode.php
@@ -5,57 +5,47 @@ $ability = array(
     'ability_token' => 'defense-mode',
     'ability_game' => 'MMRPG',
     'ability_group' => 'MMRPG/Support/Defense2',
-    'ability_description' => 'The user lowers its attack and speed by {DAMAGE}% to greatly raise defense and improve shields by {RECOVERY}%!',
-    'ability_energy' => 4,
-    'ability_recovery' => 60,
-    'ability_recovery_percent' => true,
-    'ability_damage' => 30,
-    'ability_damage_percent' => true,
+    'ability_description' => 'The user harshly lowers its own attack and speed stats in exchange for a maxed-out defense stat to improve shields! However the weapon energy cost for this ability increases after each use.',
+    'ability_energy' => 8,
     'ability_accuracy' => 100,
     'ability_function' => function($objects){
 
         // Extract all objects into the current scope
         extract($objects);
 
-        // Target this robot's self
-        $this_ability->target_options_update(array(
-            'frame' => 'summon',
-            'success' => array(9, 0, 0, -10, $this_robot->print_name().' enters '.$this_ability->print_name().'!')
-            ));
+        // Target this robot's self and init ability
+        $this_ability->target_options_update(array('frame' => 'summon','success' => array(9, 0, 0, -10, $this_robot->print_name().' enters '.$this_ability->print_name().'!')));
         $this_robot->trigger_target($this_robot, $this_ability);
 
-        // Decrease this robot's attack stat
-        $this_ability->damage_options_update(array(
-            'kind' => 'attack',
-            'frame' => 'defend',
-            'percent' => true,
-            'success' => array(1, -2, 0, -10,  $this_robot->print_name().'&#39;s weapons powered down&hellip;'),
-            'failure' => array(9, -2, 0, -10, $this_robot->print_name().'&#39;s weapons were not affected&hellip;')
-            ));
-        $attack_damage_amount = ceil($this_robot->robot_attack * ($this_ability->ability_damage / 100));
-        $this_robot->trigger_damage($this_robot, $this_ability, $attack_damage_amount);
+        // Define the the stats to lower and raise for this mode
+        $raise_stat = 'defense';
+        $lower_stats = array('attack', 'speed');
 
-        // Decrease this robot's speed stat
-        $this_ability->damage_options_update(array(
-            'kind' => 'speed',
-            'frame' => 'defend',
-            'percent' => true,
-            'success' => array(2, -4, 0, -10,  $this_robot->print_name().'&#39;s mobility slowed&hellip;'),
-            'failure' => array(9, -4, 0, -10, $this_robot->print_name().'&#39;s mobility was not affected&hellip;')
-            ));
-        $speed_damage_amount = ceil($this_robot->robot_speed * ($this_ability->ability_damage / 100));
-        $this_robot->trigger_damage($this_robot, $this_ability, $speed_damage_amount);
+        // Check to see if this robot is allowed to use the move or not
+        $allow_move = true;
+        $stats_wont_go = '';
+        if ($this_robot->counters[$raise_stat.'_mods'] >= MMRPG_SETTINGS_STATS_MOD_MAX){ $allow_move = false; $stats_wont_go = $raise_stat.' stat wont go any higher'; }
+        elseif ($this_robot->counters[$lower_stats[0].'_mods'] <= MMRPG_SETTINGS_STATS_MOD_MIN){ $allow_move = false; $stats_wont_go = $lower_stats[0].' stat wont go any lower'; }
+        elseif ($this_robot->counters[$lower_stats[1].'_mods'] <= MMRPG_SETTINGS_STATS_MOD_MIN){ $allow_move = false; $stats_wont_go = $lower_stats[1].' stat wont go any lower'; }
 
-        // Increase this robot's defense stat
-        $this_ability->recovery_options_update(array(
-            'kind' => 'defense',
-            'frame' => 'taunt',
-            'percent' => true,
-            'success' => array(0, -6, 0, -10,  $this_robot->print_name().'&#39;s shields powered up&hellip;'),
-            'failure' => array(9, -6, 0, -10, $this_robot->print_name().'&#39;s shields were not affected&hellip;')
-            ));
-        $defense_recovery_amount = ceil($this_robot->robot_defense * ($this_ability->ability_recovery / 100));
-        $this_robot->trigger_recovery($this_robot, $this_ability, $defense_recovery_amount);
+        // If move is not allowed, do nothing right now
+        if (!$allow_move){
+
+            // Target this robot's self to show the failure message
+            $this_ability->target_options_update(array('frame' => 'defend', 'success' => array(9, -2, 0, -10, 'But '.$this_robot->print_name().'&#39;s '.$stats_wont_go.'!')));
+            $this_robot->trigger_target($this_robot, $this_ability);
+            return;
+
+        }
+
+        // Call the global stat break function with customized options
+        rpg_ability::ability_function_stat_break($this_robot, $lower_stats[0], 2, $this_ability, 1);
+
+        // Call the global stat break function with customized options
+        rpg_ability::ability_function_stat_break($this_robot, $lower_stats[1], 2, $this_ability, 2);
+
+        // Call the global stat boost function with customized options
+        rpg_ability::ability_function_stat_boost($this_robot, $raise_stat, 5, $this_ability, 0);
 
         // Return true on success
         return true;
--- data/abilities/support/energy-mode.php
+++ data/abilities/support/energy-mode.php
@@ -5,72 +5,82 @@ $ability = array(
     'ability_token' => 'energy-mode',
     'ability_game' => 'MMRPG',
     'ability_group' => 'MMRPG/Support/Energy2',
-    'ability_description' => 'The user lowers its attack, defense, and speed by {DAMAGE}% to repair damaged systems and recover up to {RECOVERY}% energy!',
-    'ability_energy' => 4,
-    'ability_recovery' => 60,
-    'ability_recovery_percent' => true,
-    'ability_damage' => 20,
-    'ability_damage_percent' => true,
+    'ability_description' => 'The user harshly lowers its own attack, defense, and speed stats in exchange for fully recovering life energy and returning to perfect health!',
+    'ability_energy' => 8,
     'ability_accuracy' => 100,
     'ability_function' => function($objects){
 
         // Extract all objects into the current scope
         extract($objects);
 
-        // Target this robot's self
-        $this_ability->target_options_update(array(
-            'frame' => 'summon',
-            'success' => array(9, 0, 0, -10, $this_robot->print_name().' enters '.$this_ability->print_name().'!')
-            ));
+        // Target this robot's self and init ability
+        $this_ability->target_options_update(array('frame' => 'summon','success' => array(9, 0, 0, -10, $this_robot->print_name().' enters '.$this_ability->print_name().'!')));
         $this_robot->trigger_target($this_robot, $this_ability);
 
-        // Decrease this robot's attack stat
-        $this_ability->damage_options_update(array(
-            'kind' => 'attack',
-            'frame' => 'defend',
-            'percent' => true,
-            'success' => array(1, -2, 0, -10,  $this_robot->print_name().'&#39;s weapons powered down&hellip;'),
-            'failure' => array(9, -2, 0, -10, $this_robot->print_name().'&#39;s weapons were not affected&hellip;')
-            ));
-        $attack_damage_amount = ceil($this_robot->robot_attack * ($this_ability->ability_damage / 100));
-        $this_robot->trigger_damage($this_robot, $this_ability, $attack_damage_amount);
+        // Define the the stats to lower for this mode
+        $lower_stats = array('attack', 'defense', 'speed');
 
-        // Decrease this robot's defense stat
-        $this_ability->damage_options_update(array(
-            'kind' => 'defense',
-            'frame' => 'defend',
-            'percent' => true,
-            'success' => array(2, -4, 0, -10,  $this_robot->print_name().'&#39;s shields powered down&hellip;'),
-            'failure' => array(9, -4, 0, -10, $this_robot->print_name().'&#39;s shields were not affected&hellip;')
-            ));
-        $defense_damage_amount = ceil($this_robot->robot_defense * ($this_ability->ability_damage / 100));
-        $this_robot->trigger_damage($this_robot, $this_ability, $defense_damage_amount);
+        // Check to see if this robot is allowed to use the move or not
+        $allow_move = true;
+        $stats_wont_go = '';
+        if ($this_robot->robot_energy >= $this_robot->robot_base_energy){ $allow_move = false; $stats_wont_go = ' is already a full health'; }
+        elseif ($this_robot->counters[$lower_stats[0].'_mods'] <= MMRPG_SETTINGS_STATS_MOD_MIN){ $allow_move = false; $stats_wont_go = '&#39;s '.$lower_stats[0].' stat wont go any lower'; }
+        elseif ($this_robot->counters[$lower_stats[1].'_mods'] <= MMRPG_SETTINGS_STATS_MOD_MIN){ $allow_move = false; $stats_wont_go = '&#39;s '.$lower_stats[1].' stat wont go any lower'; }
+        elseif ($this_robot->counters[$lower_stats[2].'_mods'] <= MMRPG_SETTINGS_STATS_MOD_MIN){ $allow_move = false; $stats_wont_go = '&#39;s '.$lower_stats[2].' stat wont go any lower'; }
 
-        // Decrease this robot's speed stat
-        $this_ability->damage_options_update(array(
-            'kind' => 'speed',
-            'frame' => 'defend',
-            'percent' => true,
-            'success' => array(3, -6, 0, -10,  $this_robot->print_name().'&#39;s mobility slowed&hellip;'),
-            'failure' => array(9, -6, 0, -10, $this_robot->print_name().'&#39;s mobility was not affected&hellip;')
-            ));
-        $speed_damage_amount = ceil($this_robot->robot_speed * ($this_ability->ability_damage / 100));
-        $this_robot->trigger_damage($this_robot, $this_ability, $speed_damage_amount);
+        // If move is not allowed, do nothing right now
+        if (!$allow_move){
 
-        // Increase this robot's energy stat
+            // Target this robot's self to show the failure message
+            $this_ability->target_options_update(array('frame' => 'defend', 'success' => array(9, -2, 0, -10, 'But '.$this_robot->print_name().$stats_wont_go.'!')));
+            $this_robot->trigger_target($this_robot, $this_ability);
+            return;
+
+        }
+
+        // Call the global stat break function with customized options
+        rpg_ability::ability_function_stat_break($this_robot, $lower_stats[0], 2, $this_ability, 1);
+
+        // Call the global stat break function with customized options
+        rpg_ability::ability_function_stat_break($this_robot, $lower_stats[1], 2, $this_ability, 2);
+
+        // Call the global stat break function with customized options
+        rpg_ability::ability_function_stat_break($this_robot, $lower_stats[2], 2, $this_ability, 3);
+
+        // Increase this robot's life energy stat to max
         $this_ability->recovery_options_update(array(
             'kind' => 'energy',
-            'frame' => 'taunt',
             'percent' => true,
-            'success' => array(0, -8, 0, -10,  $this_robot->print_name().' recovered energy!'),
-            'failure' => array(9, -8, 0, -10, $this_robot->print_name().'&#39;s energy was not affected&hellip;')
+            'modifiers' => false,
+            'frame' => 'taunt',
+            'success' => array(0, 0, 0, -10, $this_robot->print_name().'&#39;s life energy was fully restored!'),
+            'failure' => array(9, 0, 0, -9999, $this_robot->print_name().'&#39;s life energy was not affected&hellip;')
             ));
-        $energy_recovery_amount = ceil($this_robot->robot_base_energy * ($this_ability->ability_recovery / 100));
+        $energy_recovery_amount = $this_robot->robot_base_energy - $this_robot->robot_energy;
         $this_robot->trigger_recovery($this_robot, $this_ability, $energy_recovery_amount);
 
         // Return true on success
         return true;
 
-    }
+        },
+    'ability_function_onload' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Check to see if this ability has been used already, and if so increase the cost
+        if (!empty($this_robot->history['triggered_abilities'])){
+            $trigger_counts = array_count_values($this_robot->history['triggered_abilities']);
+            if (!empty($trigger_counts[$this_ability->ability_token])){
+                $trigger_count = $trigger_counts[$this_ability->ability_token];
+                $new_energy_cost = $this_ability->ability_base_energy * ($trigger_count + 1);
+                $this_ability->set_energy($new_energy_cost);
+            }
+        }
+
+        // Return true on success
+        return true;
+
+        }
     );
 ?>
