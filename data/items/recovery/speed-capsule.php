64a1fcb3e2c53633363df6a21f61d8f5b279bbe9
AdrianMarceau <adrian.marceau@gmail.com>
2018-12-18 23:06:26 -0500

    Updated all consumable stat items to work with new stat mechanics

--- data/items/recovery/speed-capsule.php
+++ data/items/recovery/speed-capsule.php
@@ -8,18 +8,17 @@ $item = array(
     'item_class' => 'item',
     'item_subclass' => 'consumable',
     'item_type' => 'speed',
-    'item_description' => 'A large mobility capsule that that boosts the speed stat of one robot on the user\'s side of the field by to to {RECOVERY}% its base value.',
+    'item_description' => 'A large mobility capsule that drastically raises the speed stat of one robot on the user\'s side of the field.',
     'item_energy' => 0,
     'item_speed' => 10,
-    'item_recovery' => 50,
-    'item_recovery_percent' => true,
+    'item_recovery' => 3,
     'item_accuracy' => 100,
     'item_price' => 2700,
     'item_target' => 'select_this',
     'item_function' => function($objects){
 
-            // Call the global stat booster item function
-            return rpg_item::item_function_stat_booster($objects);
+        // Call the global stat booster item function
+        return rpg_item::item_function_stat_booster($objects);
 
         }
     );
