8517de416484a8fe2c42772d5760a660fd64ef08
AdrianMarceau <adrian.marceau@gmail.com>
2018-12-24 01:48:56 -0500

    Added images + data for Frost Wave ability

--- data/abilities/mm8/frost-wave.php
+++ data/abilities/mm8/frost-wave.php
@@ -9,33 +9,38 @@ $ability = array(
     'ability_type' => 'freeze',
     'ability_energy' => 4,
     'ability_speed' => 3,
-    'ability_damage' => 12,
+    'ability_damage' => 10,
     'ability_accuracy' => 100,
     'ability_function' => function($objects){
 
         // Extract all objects into the current scope
         extract($objects);
 
+        // If the user is Frost Man, wind-up first
+        if ($this_robot->robot_token === 'frost-man'){ $this_robot->set_frame('summon'); $this_battle->events_create(false, false, '', ''); }
+
         // Target the opposing robot
+        $x_position = 80 + ($this_robot->robot_image_size / 2);
         $this_ability->target_options_update(array(
-            'frame' => 'shoot',
-            'success' => array(0, 75, 0, 10, $this_robot->print_name().' fires the '.$this_ability->print_name().'!')
+            'frame' => ($this_robot->robot_token === 'frost-man' ? 'throw' : 'shoot'),
+            'success' => array(1, $x_position, 0, 10, $this_robot->print_name().' '.($this_robot->robot_token === 'frost-man' ? 'summons' : 'fires').' the '.$this_ability->print_name().'!')
             ));
         $this_robot->trigger_target($target_robot, $this_ability);
+        $this_robot->set_frame('base');
 
         // Inflict damage on the opposing robot
         $this_ability->damage_options_update(array(
             'kind' => 'energy',
             'kickback' => array(10, 0, 0),
-            'success' => array(1, -55, 0, 10, 'The '.$this_ability->print_name().' hit the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+            'success' => array(2, -45, 0, 10, 'The '.$this_ability->print_name().' hit the target!'),
+            'failure' => array(2, -65, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
             ));
         $this_ability->recovery_options_update(array(
             'kind' => 'energy',
             'frame' => 'taunt',
             'kickback' => array(10, 0, 0),
-            'success' => array(1, -35, 0, 10, 'The '.$this_ability->print_name().' was absorbed by the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+            'success' => array(2, -40, 0, 10, 'The '.$this_ability->print_name().' was absorbed by the target!'),
+            'failure' => array(2, -60, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
             ));
         $energy_damage_amount = $this_ability->ability_damage;
         $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
