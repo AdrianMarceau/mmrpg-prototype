953bb2ee94636dd26199641b8975d982c7926ef6
AdrianMarceau <adrian.marceau@gmail.com>
2019-01-02 14:48:37 -0500

    Updated DevPower to be more flexible

--- data/abilities/_system/action-devpower-clearmission.php
+++ data/abilities/_system/action-devpower-clearmission.php
@@ -26,20 +26,25 @@ $ability = array(
         $target_robots_active = $target_player->counters['robots_active'];
 
         // Inflict damage on the opposing robot
+        $damage_type = '';
+        if ($this_robot->robot_core === 'copy'){ $damage_type = !empty($target_robot->robot_weaknesses[0]) ? $target_robot->robot_weaknesses[0] : ''; }
+        elseif (!empty($this_robot->robot_core)){ $damage_type = $this_robot->robot_core; }
         $this_ability->damage_options_update(array(
             'kind' => 'energy',
+            'type' => $damage_type,
             'frame' => 'damage',
             'modifiers' => false,
             'success' => array(0, 0, 0, 0, 'The <strong class="ability_name ability_type type_nature_shield">DevPower</strong> cleared out '.$target_robot->print_name().'!')
             ));
         $this_ability->recovery_options_update(array(
             'kind' => 'energy',
+            'type' => $damage_type,
             'frame' => 'damage',
             'modifiers' => false,
-            'success' => array(0, 0, 0, 0, 'The <strong class="ability_name ability_type type_nature_shield">DevPower</strong> cleared out '.$target_robot->print_name().'!')
+            'success' => array(0, 0, 0, 0, 'The <strong class="ability_name ability_type type_'.(!empty($damage_type) ? $damage_type : 'none').'">DevPower</strong> cleared out '.$target_robot->print_name().'!')
             ));
         $energy_damage_amount = $target_robot->robot_base_energy;
-        $trigger_options = array('apply_modifiers' => false, 'apply_position_modifiers' => false, 'apply_stat_modifiers' => false);
+        $trigger_options = array('apply_modifiers' => true, 'apply_position_modifiers' => false, 'apply_stat_modifiers' => false);
         $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, false, $trigger_options);
 
         // Loop through the target's benched robots, inflicting damage to each
@@ -48,17 +53,22 @@ $ability = array(
             if ($info['robot_id'] == $target_robot->robot_id){ continue; }
             $temp_target_robot = rpg_game::get_robot($this_battle, $target_player, $info);
             $this_ability->ability_results_reset();
+            $damage_type = '';
+            if ($this_robot->robot_core === 'copy'){ $damage_type = !empty($temp_target_robot->robot_weaknesses[0]) ? $temp_target_robot->robot_weaknesses[0] : ''; }
+            elseif (!empty($this_robot->robot_core)){ $damage_type = $this_robot->robot_core; }
             $this_ability->damage_options_update(array(
 	            'kind' => 'energy',
+                'type' => $damage_type,
 	            'frame' => 'damage',
 	            'modifiers' => false,
-	            'success' => array(0, 0, 0, 0, 'The <strong class="ability_name ability_type type_nature_shield">DevPower</strong> cleared out '.$temp_target_robot->print_name().'!')
+	            'success' => array(0, 0, 0, 0, 'The <strong class="ability_name ability_type type_'.(!empty($damage_type) ? $damage_type : 'none').'">DevPower</strong> cleared out '.$temp_target_robot->print_name().'!')
                 ));
             $this_ability->recovery_options_update(array(
 	            'kind' => 'energy',
+                'type' => $damage_type,
 	            'frame' => 'damage',
 	            'modifiers' => false,
-	            'success' => array(0, 0, 0, 0, 'The <strong class="ability_name ability_type type_nature_shield">DevPower</strong> cleared out '.$temp_target_robot->print_name().'!')
+	            'success' => array(0, 0, 0, 0, 'The <strong class="ability_name ability_type type_'.(!empty($damage_type) ? $damage_type : 'none').'">DevPower</strong> cleared out '.$temp_target_robot->print_name().'!')
                 ));
             $energy_damage_amount = $temp_target_robot->robot_base_energy;
             $temp_target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, false, $trigger_options);
