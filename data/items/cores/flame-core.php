88efa70193a19a0233c909e5637031f582042120
AdrianMarceau <adrian.marceau@gmail.com>
2019-01-07 06:46:07 -0500

    Reduced or revised redundant item descriptions in the database

--- data/items/cores/flame-core.php
+++ data/items/cores/flame-core.php
@@ -10,7 +10,6 @@ $item = array(
     'item_type' => 'flame',
     'item_description' => 'A mysterious elemental core that radiates with the Flame type energy of a defeated robot master.',
     'item_description_hold' => 'When held, this item grants compatibility with any Flame type ability as well as power and weapon energy bonuses similar to that of an internal core.',
-    'item_description_shop' => 'This item appears to be coveted by a certain character and can be traded in for a variable amount of Zenny.',
     'item_energy' => 0,
     'item_speed' => 10,
     'item_damage' => 0,
--- data/items/stars/fusion-star.php
+++ data/items/stars/fusion-star.php
@@ -8,7 +8,7 @@ $item = array(
     'item_class' => 'item',
     'item_subclass' => 'collectible',
     'item_value' => 8000,
-    'item_description' => 'A mysterious elemental star that radiates with the energy of distant planets. These stars appear to form when two different battle fields are fused together and as a result come in a variety of types.  Collecting them may be essential to progressing through the story.  A certain character is also said to be researching these items and would likely trade an impressive amount of Zenny to study one up close.',
+    'item_description' => 'A mysterious elemental star that radiates with the energy of distant planets. These stars appear to form when two different battle fields fuse together and come in a variety of different types. Collecting stars increases one\'s Starforce and makes their robots grow stronger!',
     'item_function' => function($objects){
 
         // Extract all objects into the current scope
