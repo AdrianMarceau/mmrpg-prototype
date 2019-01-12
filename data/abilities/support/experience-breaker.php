228210f4e7042a45e9e81a3368f3492eeb976ca3
AdrianMarceau <adrian.marceau@gmail.com>
2019-01-12 11:18:14 -0500

    Buffed Experience Booster and Breaker abilities

--- data/abilities/support/experience-breaker.php
+++ data/abilities/support/experience-breaker.php
@@ -5,11 +5,9 @@ $ability = array(
     'ability_token' => 'experience-breaker',
     'ability_game' => 'MMRPG',
     'ability_group' => 'MMRPG/Support/Experience',
-    'ability_description' => 'The user alters the conditions of the field to lower experience points earned in battle by {DAMAGE}%! This ability appears to be unaffected by other field multipliers.',
-    'ability_energy' => 6,
+    'ability_description' => 'The user alters the conditions of the field to halve the amount of experience points earned in this battle!',
+    'ability_energy' => 8,
     'ability_speed' => -2,
-    'ability_damage' => 30,
-    'ability_damage_percent' => true,
     'ability_accuracy' => 100,
     'ability_function' => function($objects){
 
@@ -24,7 +22,7 @@ $ability = array(
                 'frame' => 'summon',
                 'success' => array(9, 0, 0, -10,
                     $this_robot->print_name().' activated the '.$this_ability->print_name().'!<br />'.
-                    'But the field\'s experience wont go any lower&hellip;'
+                    'But the battle\'s experience wont go any lower&hellip;'
                     )
                 ));
             $this_robot->trigger_target($this_robot, $this_ability);
@@ -66,7 +64,7 @@ $ability = array(
         // Attach this ability to all robots on this player's side of the field
         $backup_robots_active = $this_player->values['robots_active'];
         $backup_robots_active_count = !empty($backup_robots_active) ? count($backup_robots_active) : 0;
-        if ($backup_robots_active_count > 0){
+        if ($backup_robots_active_count > 0 && $this_player->player_side === 'left'){
             // Loop through the this's benched robots, inflicting les and less damage to each
             $this_key = 0;
             foreach ($backup_robots_active AS $key => $info){
@@ -83,7 +81,7 @@ $ability = array(
         // Attach this ability to all robots on the target's side of the field
         $backup_robots_active = $target_player->values['robots_active'];
         $backup_robots_active_count = !empty($backup_robots_active) ? count($backup_robots_active) : 0;
-        if ($backup_robots_active_count > 0){
+        if ($backup_robots_active_count > 0 && $target_player->player_side === 'left'){
             // Loop through the target's benched robots, inflicting les and less damage to each
             $target_key = 0;
             foreach ($backup_robots_active AS $key => $info){
@@ -97,26 +95,22 @@ $ability = array(
         }
 
         // Create or decrease the experience booster for this field
-        $temp_change_percent = round($this_ability->ability_damage / 100, 1);
-        if (!isset($this_field->field_multipliers['experience'])){ $this_field->field_multipliers['experience'] = 1.0 - $temp_change_percent; }
-        else { $this_field->field_multipliers['experience'] = $this_field->field_multipliers['experience'] - $temp_change_percent; }
-        if ($this_field->field_multipliers['experience'] <= MMRPG_SETTINGS_MULTIPLIER_MIN){
-            $temp_change_percent = $this_field->field_multipliers['experience'] + MMRPG_SETTINGS_MULTIPLIER_MIN;
-            $this_field->field_multipliers['experience'] = MMRPG_SETTINGS_MULTIPLIER_MIN;
-        }
+        if (!isset($this_field->field_multipliers['experience'])){ $this_field->field_multipliers['experience'] = 1.0; }
+        $this_field->field_multipliers['experience'] *= 0.5;
+        if ($this_field->field_multipliers['experience'] < MMRPG_SETTINGS_MULTIPLIER_MIN){ $this_field->field_multipliers['experience'] = MMRPG_SETTINGS_MULTIPLIER_MIN; }
         $this_field->update_session();
 
         // Create the event to show this experience boost
         $random_sayings = array('Oh no!', 'It worked!', 'That\'s not good!');
         $this_battle->events_create($this_robot, false, $this_field->field_name.' Multipliers',
-            $random_sayings[array_rand($random_sayings)].' <span class="ability_name ability_type ability_type_experience">Experience Points</span> were decreased by '.ceil($temp_change_percent * 100).'%!<br />'.
+            $random_sayings[array_rand($random_sayings)].' The <span class="ability_name ability_type ability_type_experience">Experience Points</span> harshly fell!<br />'.
             'The multiplier is now at <span class="ability_name ability_type ability_type_experience">Experience x '.number_format($this_field->field_multipliers['experience'], 1).'</span>!'
             );
 
         // Remove this ability from all robots on this player's side of the field
         $backup_robots_active = $this_player->values['robots_active'];
         $backup_robots_active_count = !empty($backup_robots_active) ? count($backup_robots_active) : 0;
-        if ($backup_robots_active_count > 0){
+        if ($backup_robots_active_count > 0 && $this_player->player_side === 'left'){
             // Loop through the this's benched robots, inflicting les and less damage to each
             $this_key = 0;
             foreach ($backup_robots_active AS $key => $info){
@@ -133,7 +127,7 @@ $ability = array(
         // Remove this ability from all robots on the target's side of the field
         $backup_robots_active = $target_player->values['robots_active'];
         $backup_robots_active_count = !empty($backup_robots_active) ? count($backup_robots_active) : 0;
-        if ($backup_robots_active_count > 0){
+        if ($backup_robots_active_count > 0 && $target_player->player_side === 'left'){
             // Loop through the target's benched robots, inflicting les and less damage to each
             $target_key = 0;
             foreach ($backup_robots_active AS $key => $info){
