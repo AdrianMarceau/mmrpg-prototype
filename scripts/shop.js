119ba938f611983ce96778011e87247d997769dc
AdrianMarceau <adrian.marceau@gmail.com>
2019-01-13 13:51:35 -0500

    Fixed bug where selling copy/neutral cores would break shop

--- scripts/shop.js
+++ scripts/shop.js
@@ -437,16 +437,18 @@ $(document).ready(function(){
                                         var $eventWrap = thisConfirmCell.closest('.event[data-token]');
                                         var $coreGauge = $eventWrap.find('.gauge.cores');
                                         var $coreWrap = $coreGauge.find('.element[data-type="'+coreType+'"]');
-                                        //console.log('coreType =', coreType);
-                                        //console.log('$eventWrap = ', $eventWrap.length, '$coreGauge = ', $coreGauge.length, '$coreWrap =', $coreWrap.length);
-                                        var oldCount = parseInt($coreWrap.attr('data-count'));
-                                        var newCount = oldCount + parseInt(postData.quantity);
-                                        //console.log('oldCount =', oldCount, 'newCount =', newCount);
-                                        $coreWrap.attr('data-count', newCount);
-                                        $coreWrap.attr('data-tooltip', $coreWrap.attr('data-tooltip').replace(/\s[0-9]+$/, ' '+newCount));
-                                        $coreWrap.css({opacity:(0.1 + ((newCount < 3 ? newCount / 3 : 1) * 0.9))});
-                                        if (newCount < 9){ $coreWrap.find('.count').html(newCount); }
-                                        else { $coreWrap.find('.count').html('&bigstar;'); }
+                                        if ($coreWrap.length){
+                                            //console.log('coreType =', coreType);
+                                            //console.log('$eventWrap = ', $eventWrap.length, '$coreGauge = ', $coreGauge.length, '$coreWrap =', $coreWrap.length);
+                                            var oldCount = parseInt($coreWrap.attr('data-count'));
+                                            var newCount = oldCount + parseInt(postData.quantity);
+                                            //console.log('oldCount =', oldCount, 'newCount =', newCount);
+                                            $coreWrap.attr('data-count', newCount);
+                                            $coreWrap.attr('data-tooltip', $coreWrap.attr('data-tooltip').replace(/\s[0-9]+$/, ' '+newCount));
+                                            $coreWrap.css({opacity:(0.1 + ((newCount < 3 ? newCount / 3 : 1) * 0.9))});
+                                            if (newCount < 9){ $coreWrap.find('.count').html(newCount); }
+                                            else { $coreWrap.find('.count').html('&bigstar;'); }
+                                            }
                                     }
 
 
