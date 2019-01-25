5b94d6e8e503b62a0e0aefea9a569592980d8d07
AdrianMarceau <adrian.marceau@gmail.com>
2019-01-24 22:20:26 -0500

    Updated robot editor to show dropdown for robot functions instead of textbox

--- functions/website.php
+++ functions/website.php
@@ -925,4 +925,21 @@ function deleteDir($dirPath) {
         rmdir($dirPath);
 }
 
+// Define a function for getting a list of directory contents, recursively
+// via https://stackoverflow.com/a/24784144/1876397
+function getDirContents($dir, &$results = array()){
+    $files = scandir($dir);
+    foreach($files as $key => $value){
+        $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+        if(!is_dir($path)) {
+            $results[] = $path;
+        } else if($value != "." && $value != "..") {
+            getDirContents($path, $results);
+            $results[] = $path;
+        }
+    }
+    return $results;
+}
+
+
 ?>
