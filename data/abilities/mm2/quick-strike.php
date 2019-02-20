b531862e70f863e07c6bbf29d97369f7dde86141
AdrianMarceau <adrian.marceau@gmail.com>
2019-02-19 21:42:45 -0500

    Tweaked Quick Strike ability to show the item dropped on-screen

--- data/abilities/mm2/quick-strike.php
+++ data/abilities/mm2/quick-strike.php
@@ -9,7 +9,7 @@ $ability = array(
     'ability_type' => 'swift',
     'ability_energy' => 4,
     'ability_speed' => 6,
-    'ability_damage' => 10,
+    'ability_damage' => 12,
     'ability_accuracy' => 100,
     'ability_function' => function($objects){
 
@@ -25,7 +25,7 @@ $ability = array(
         $this_robot->trigger_target($target_robot, $this_ability);
 
         // Move the user forward so it looks like their swining the weapon
-        $this_robot->set_frame('taunt');
+        $this_robot->set_frame('defend');
         $this_robot->set_frame_offset('x', 100);
         $this_robot->set_frame_styles('transform: scaleX(-1); -moz-transform: scaleX(-1); -webkit-transform: scaleX(-1); ');
 
@@ -39,36 +39,56 @@ $ability = array(
         $energy_damage_amount = $this_ability->ability_damage;
         $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
 
-        // Reset the offset and move the user back to their position
-        $this_robot->set_frame('base');
-        $this_robot->set_frame_offset('x', 0);
-        $this_robot->set_frame_styles('');
-
         // If this attack was successful, remove the target's held item from use (not permanently)
         if ($this_ability->ability_results['this_result'] != 'failure'
             && $target_robot->robot_status != 'disabled'
             && !empty($target_robot->robot_item)){
 
-            // Pause for a moment to reset positions
-            $this_battle->events_create(false, false, '', '');
+            // Change this robot's frame to a summon now
+            $this_robot->set_frame('taunt');
+            $this_robot->set_frame_offset('x', 80);
+            $this_robot->set_frame_styles('');
 
-            // Remove the item from the target robot and update
+            // Define this ability's attachment token
+            $this_attachment_token = 'ability_'.$this_ability->ability_token;
+            $this_attachment_info = array(
+                'class' => 'ability',
+                'attachment_token' => $this_attachment_token,
+                'ability_token' => $this_ability->ability_token,
+                'ability_frame' => 2,
+                'ability_frame_animate' => array(2),
+                'ability_frame_offset' => array('x' => 0, 'y' => 60, 'z' => 20)
+                );
+
+            // Remove the item from the target robot and update w/ attachment info
             $old_item_token = $target_robot->robot_item;
             $old_item = rpg_game::get_item($this_battle, $target_player, $target_robot, array('item_token' => $old_item_token));
+            $target_robot->robot_attachments[$this_attachment_token] = $this_attachment_info;
             $target_robot->robot_item = '';
             $target_robot->update_session();
 
             // Update the ability's target options and trigger
-            $this_ability->target_options_update(array(
+            $temp_rotate_amount = 45;
+            $old_item->set_frame_styles('opacity: 0.5; transform: rotate('.$temp_rotate_amount.'deg); -webkit-transform: rotate('.$temp_rotate_amount.'deg); -moz-transform: rotate('.$temp_rotate_amount.'deg); ');
+            $old_item->target_options_update(array(
                 'frame' => 'defend',
-                'success' => array(2, 0, 40, 20,
+                'success' => array(0, -90, 0, 20,
                     $target_robot->print_name().' dropped '.$target_robot->get_pronoun('possessive2').' held item!'.
                     '<br /> The '.$old_item->print_name().' was lost!'
                     )
                 ));
-            $target_robot->trigger_target($target_robot, $this_ability, array('prevent_default_text' => true));
+            $target_robot->trigger_target($target_robot, $old_item, array('prevent_default_text' => true));
+
+            // Remove the visual icon attachment from the target
+            unset($target_robot->robot_attachments[$this_attachment_token]);
+            $target_robot->update_session();
 
         }
+
+        // Reset the offset and move the user back to their position
+        $this_robot->set_frame('base');
+        $this_robot->set_frame_offset('x', 0);
+        $this_robot->set_frame_styles('');
 
         // Return true on success
         return true;
