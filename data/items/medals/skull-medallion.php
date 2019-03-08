4e0d152234cb7dad856de44c72ad8a50a83fa526
AdrianMarceau <adrian.marceau@gmail.com>
2019-03-08 01:38:08 -0500

    Added new Skull Medallion item to database + import script
    This item is a non-specific placeholder for the eventual Bronze/Silver/Gold Skull Medallions that will actually be awarded when completing a challenge mission. This version of the item cannot be collected and acts only as a visual marker for challenge missions.

---
+++ data/items/medals/skull-medallion.php
@@ -0,0 +1,25 @@
+<?
+// ITEM : SKULL MEDALLION
+$item = array(
+    'item_name' => 'Skull Medallion',
+    'item_token' => 'skull-medallion',
+    'item_game' => 'MMRPG',
+    'item_group' => 'MMRPG/Items/Medallions',
+    'item_class' => 'item',
+    'item_subclass' => 'treasure',
+    'item_type' => '',
+    'item_type2' => '',
+    'item_description' => 'An unusually large, skull-marked medallion that appears to mark the locations of prototype challenge missions.  This item is currently immune to interaction and appears to be waiting for some event to take place.',
+    'item_energy' => 0,
+    'item_speed' => 10,
+    'item_accuracy' => 100,
+    'item_price' => 0,
+    'item_value' => 0,
+    'item_function' => function($objects){
+
+        // Return true on success
+        return true;
+
+    }
+    );
+?>
