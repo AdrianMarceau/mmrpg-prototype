05864eeaa7c3f288dabd695cc3bd2a16ffc09997
AdrianMarceau <adrian.marceau@gmail.com>
2019-03-11 17:33:10 -0400

    Added function for static black hole field attachments

--- classes/rpg_ability.php
+++ classes/rpg_ability.php
@@ -2868,5 +2868,69 @@ class rpg_ability extends rpg_object {
         return $this_attachment_info;
     }
 
+    // Define a static function for generating a static field attachment of a "black hole" (from the Galaxy Bomb ability)
+    public static function get_static_black_holes($static_attachment_key, $this_attachment_duration = 99, $existing_attachments = 0){
+        $this_ability_token = 'galaxy-bomb';
+        $this_attachment_token = 'ability_'.$this_ability_token.'_'.$static_attachment_key;
+        $this_attachment_image = $this_ability_token;
+        $this_attachment_destroy_text = 'The crushing <span class="ability_name ability_type ability_type_space_explode">black hole</span> faded away...<br /> ';
+        $this_attachment_destroy_text .= 'That position on the field won\'t take end-of-turn damage any more! ';
+        $this_attachment_repeat_text = 'The <span class="ability_name ability_type ability_type_space_explode">black hole</span> crushed the '.(strstr($static_attachment_key, 'active') ? 'active' : 'benched').' robot!';
+        $this_attachment_info = array(
+            'class' => 'ability',
+            'sticky' => true,
+            'ability_token' => $this_ability_token,
+            'ability_image' => $this_attachment_image,
+            'attachment_token' => $this_attachment_token,
+            'attachment_duration' => $this_attachment_duration,
+            'attachment_energy' => 0,
+            'attachment_energy_base_percent' => 10,
+            'attachment_sticky' => true,
+            'attachment_destroy' => array(
+                'trigger' => 'special',
+                'kind' => '',
+                'type' => '',
+                'percent' => true,
+                'modifiers' => false,
+                'frame' => 'defend',
+                'rates' => array(100, 0, 0),
+                'success' => array(9, -9999, -9999, 10, $this_attachment_destroy_text),
+                'failure' => array(9, -9999, -9999, 10, $this_attachment_destroy_text)
+                ),
+            'attachment_repeat' => array(
+                'kind' => 'energy',
+                'trigger' => 'damage',
+                'type' => 'space',
+                'type2' => 'explode',
+                'energy' => 10,
+                'percent' => true,
+                'modifiers' => true,
+                'frame' => 'damage',
+                'rates' => array(100, 0, 0),
+                'success' => array(1, -5, 5, -10, $this_attachment_repeat_text),
+                'failure' => array(1, -5, 5, -99, $this_attachment_repeat_text),
+                'options' => array(
+                    'apply_modifiers' => true,
+                    'apply_type_modifiers' => true,
+                    'apply_core_modifiers' => true,
+                    'apply_field_modifiers' => true,
+                    'apply_stat_modifiers' => false,
+                    'apply_position_modifiers' => false,
+                    'referred_damage' => true,
+                    'referred_damage_id' => 0,
+                    'referred_damage_stats' => array()
+                    )
+                ),
+            'ability_frame' => 1,
+            'ability_frame_animate' => array(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            'ability_frame_offset' => array(
+                'x' => (-5 + ($existing_attachments * 6)),
+                'y' => (5 + $existing_attachments),
+                'z' => (-10 - $existing_attachments)
+                )
+            );
+        return $this_attachment_info;
+    }
+
 }
 ?>
