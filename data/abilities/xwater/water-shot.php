1e9fbcd286d2f92cfe2a427dd16ef4e01ce7ed9b
AdrianMarceau <adrian.marceau@gmail.com>
2019-03-24 17:45:56 -0400

    Added data + images for Water Shot, Buster, and Overdrive abilities

---
+++ data/abilities/xwater/water-shot.php
@@ -0,0 +1,26 @@
+<?
+// WATER SHOT
+$ability = array(
+    'ability_name' => 'Water Shot',
+    'ability_token' => 'water-shot',
+    'ability_game' => 'MMRPG',
+    'ability_group' => 'MMRPG/Weapons/16/Water',
+    'ability_description' => 'The user fires a small bubble shot at the target to inflict Water type damage. This ability\'s power increases if the user if holding a buster charge of the same element.',
+    'ability_type' => 'water',
+    'ability_energy' => 0,
+    'ability_damage' => 20,
+    'ability_accuracy' => 100,
+    'ability_function' => function($objects){
+
+        // Call the common shot function from here
+        return rpg_ability::ability_function_shot($objects, 'bubble', 'soaked', 'refreshed');
+
+        },
+    'ability_function_onload' => function($objects){
+
+        // Call the common shot onload function from here
+        return rpg_ability::ability_function_onload_shot($objects);
+
+        }
+    );
+?>
