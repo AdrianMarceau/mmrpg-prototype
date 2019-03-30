7ab0d335448344f88e5d2cf24eef1e65b46c3204
AdrianMarceau <adrian.marceau@gmail.com>
2019-03-30 09:45:25 -0400

    Updated Gyro Attack to hit first OR last robot on bench
    Determined by turn number w/ even = top robot and odd = bottom robot

--- data/abilities/mm5/gyro-attack.php
+++ data/abilities/mm5/gyro-attack.php
@@ -35,7 +35,7 @@ $ability = array(
             'kind' => 'energy',
             'frame' => 'taunt',
             'kickback' => array(5, 0, 0),
-            'success' => array(1, -140, 0, 10, 'The '.$this_ability->print_name().'\'s blades honed the target!'),
+            'success' => array(1, -140, 0, 10, 'The '.$this_ability->print_name().'\'s wind invigorated the target!'),
             'failure' => array(2, -160, 0, -10, 'The '.$this_ability->print_name().' missed&hellip;')
             ));
         $energy_damage_amount = $this_ability->ability_damage;
@@ -44,34 +44,59 @@ $ability = array(
         // Ensure there are active robots left to attack
         if (!empty($target_player->values['robots_active'])){
 
+            // If there are no benched robots, we should hit the ACTIVE robot again
+            $is_same_robot = false;
+            if ($target_player->counters['robots_positions']['bench'] < 1){
 
-            // Collect the last robot on the opponent's side of the field
-            $last_target_info = array_pop((array_slice($target_player->values['robots_active'], -1)));
-            $last_target_info = array('robot_id' => $last_target_info['robot_id'], 'robot_token' => $last_target_info['robot_token']);
-            if ($last_target_info['robot_id'] != $target_robot->robot_id){ $last_target_robot = rpg_game::get_robot($this_battle, $target_player, $last_target_info); }
-            else { $last_target_robot = $target_robot; }
+                // Set the target as the opposing robot again
+                $temp_target_robot = $target_robot;
+                $is_same_robot = true;
+
+            }
+            // Otherwise, we should hit the BENCHED robot at the top of bottom of the row
+            else {
+
+                // Collect a list of benched robots from the target
+                $temp_target_robots = rpg_game::find_robots(array(
+                    'player_id' => $target_player->player_id,
+                    'robot_position' => 'bench',
+                    'robot_status' => 'active'
+                    ));
+
+                // Sort the robots by key (very important!)
+                usort($temp_target_robots, function($a, $b){
+                    if ($a->robot_key < $b->robot_key){ return -1; }
+                    elseif ($a->robot_key > $b->robot_key){ return 1; }
+                    else { return 0; }
+                    });
+
+                // Select either the first or last robot in the list
+                if ($this_battle->counters['battle_turn'] % 2 == 0){ $temp_target_robot = $temp_target_robots[0]; }
+                else { $temp_target_robot = $temp_target_robots[count($temp_target_robots) - 1]; }
+
+            }
 
             // If the (possibly) new target is not disabled, strike again
-            if ($last_target_robot->robot_status != 'disabled'){
+            if ($temp_target_robot->robot_status != 'disabled'){
 
-                // Reset any results from previous hit
-                $this_ability->ability_results_reset();
+                // Reset any results from previous hit if not same robot
+                if (!$is_same_robot){ $this_ability->ability_results_reset(); }
 
                 // Inflict damage on the opposing robot
                 $this_ability->damage_options_update(array(
                     'kind' => 'energy',
                     'kickback' => array(-10, 0, 0),
-                    'success' => array(3, 140, 0, 10, 'And there\'s the second hit!'),
+                    'success' => array(3, 140, 0, 10, ($is_same_robot ? 'And there\'s the second hit!' : 'The attack hit a benched robot! ')),
                     'failure' => array(0, 160, 0, -10, 'The second hit missed!')
                     ));
                 $this_ability->recovery_options_update(array(
                     'kind' => 'energy',
                     'kickback' => array(-5, 0, 0),
                     'frame' => 'taunt',
-                    'success' => array(3, 140, 0, 10, 'Oh no! Not again!'),
+                    'success' => array(3, 140, 0, 10, ($is_same_robot ? 'The target was healed again! ' : 'The attack healed a benched robot! ')),
                     'failure' => array(0, 160, 0, -10, 'Oh! The second hit missed!')
                     ));
-                $last_target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, false);
+                $temp_target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, false);
 
             }
 
