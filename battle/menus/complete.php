c8e722dc54a33194c591cd0a5645eef7b8b8c42a
AdrianMarceau <adrian.marceau@gmail.com>
2019-04-19 11:50:56 -0400

    Adjusted end-of-mission button text

--- battle/menus/complete.php
+++ battle/menus/complete.php
@@ -4,31 +4,52 @@ ob_start();
     // If the current robot is not disabled (WE WIN!)
     if ($this_player->counters['robots_active'] > 0
         && $this_battle->battle_result != 'defeat'){
-        // Display available main actions
-        ?><div class="main_actions"><?
+
         // Check to see if there are any battle complete redirects, otherwise print button normally
         if (!empty($this_battle->battle_complete_redirect_token)
             || !empty($this_battle->batle_complete_redirect_seed)){
-            ?><a class="button action_ability" data-action="next" type="button" data-order="1"><label>Next Mission</label></a><?
+
+            // Display available main actions
+            ?><div class="main_actions"><?
+                ?><a class="button action_ability" data-action="next" type="button" data-order="1"><label>Battle Complete!</label></a><?
+            ?></div><?
+            // Display the available sub options
+            ?><div class="sub_actions"><?
+            ?><a class="button action_scan button_disabled" type="button">&nbsp;</a><?
+            if (empty($this_battle->flags['player_battle']) && empty($this_battle->flags['starfield_mission'])){
+                ?><a class="button action_item" data-action="prototype" type="button" data-order="2"><label>Exit Mission</label></a><?
+                ?><a class="button action_option" data-action="restart" type="button" data-order="3"><label>Restart Battle</label></a><?
+            } else {
+                ?><a class="button action_item button_disabled" type="button">&nbsp;</a><?
+                ?><a class="button action_option button_disabled" type="button">&nbsp;</a><?
+            }
+            ?><a class="button action_switch button_disabled" type="button">&nbsp;</a><?
+            ?></div><?
+
+
         } else {
-            ?><a class="button action_ability" data-action="prototype" type="button" data-order="1"><label>Mission Complete!</label></a><?
+
+            // Display available main actions
+            ?><div class="main_actions"><?
+                ?><a class="button action_ability" data-action="prototype" type="button" data-order="1"><label>Mission Complete!</label></a><?
+            ?></div><?
+            // Display the available sub options
+            ?><div class="sub_actions"><?
+            ?><a class="button action_scan button_disabled" type="button">&nbsp;</a><?
+            if (empty($this_battle->flags['player_battle']) && empty($this_battle->flags['starfield_mission'])){
+                ?><a class="button action_item" data-action="prototype" type="button" data-order="2"><label>Exit Mission</label></a><?
+                ?><a class="button action_option" data-action="restart" type="button" data-order="3"><label>Restart Battle</label></a><?
+            } else {
+                ?><a class="button action_item button_disabled" type="button">&nbsp;</a><?
+                ?><a class="button action_option button_disabled" type="button">&nbsp;</a><?
+            }
+            ?><a class="button action_switch button_disabled" type="button">&nbsp;</a><?
+            ?></div><?
+
         }
-        ?></div><?
-        // Display the available sub options
-        ?><div class="sub_actions"><?
-        ?><a class="button action_scan button_disabled" type="button">&nbsp;</a><?
-        if ($target_player->player_id == MMRPG_SETTINGS_TARGET_PLAYERID
-            && empty($this_battle->flags['starfield_mission'])){
-            ?><a class="button action_item" data-action="prototype" type="button" data-order="2"><label>Exit Mission</label></a><?
-            ?><a class="button action_option" data-action="restart" type="button" data-order="3"><label>Restart Mission</label></a><?
-        } else {
-            ?><a class="button action_item button_disabled" type="button">&nbsp;</a><?
-            ?><a class="button action_option button_disabled" type="button">&nbsp;</a><?
-        }
-        ?><a class="button action_switch button_disabled" type="button">&nbsp;</a><?
-        ?></div><?
+
     }
-    // Otherwise if this robot has been disabled (WE LOOSE!)
+    // Otherwise if this robot has been disabled (WE FAIL!)
     else {
         // Display available main actions
         ?><div class="main_actions"><?
@@ -38,7 +59,7 @@ ob_start();
         ?><div class="sub_actions"><?
         ?><a class="button action_scan button_disabled" type="button">&nbsp;</a><?
         ?><a class="button action_item" data-action="prototype" type="button" data-order="1"><label>Exit Mission</label></a><?
-        ?><a class="button action_option" data-action="restart" type="button" data-order="2"><label>Restart Mission</label></a><?
+        ?><a class="button action_option" data-action="restart" type="button" data-order="2"><label>Retry Battle</label></a><?
         ?><a class="button action_switch button_disabled" type="button">&nbsp;</a><?
         ?></div><?
     }
