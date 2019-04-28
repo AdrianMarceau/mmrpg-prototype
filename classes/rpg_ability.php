fd397bcec4c066f91f53ec5a4a4117e6a52b3910
AdrianMarceau <adrian.marceau@gmail.com>
2019-04-28 00:49:36 -0400

    Added Acid Globs and Woolly Clouds to the static attachment generator functions

--- classes/rpg_ability.php
+++ classes/rpg_ability.php
@@ -3408,5 +3408,143 @@ class rpg_ability extends rpg_object {
         return $this_attachment_info;
     }
 
+    // Define a static function for generating a static field attachment of a "woolly cloud" (from the Thunder Wool ability)
+    public static function get_static_woolly_cloud($static_attachment_key, $this_attachment_duration = 99, $existing_attachments = 0){
+        $this_battle = rpg_battle::get_battle();
+        $this_attachment_json = json_encode($this_battle->battle_attachments);
+        $this_ability_token = 'thunder-wool-2';
+        $this_attachment_token = 'ability_'.$this_ability_token.'_'.$static_attachment_key;
+        $this_attachment_image = $this_ability_token;
+        $this_existing = substr_count($this_attachment_json, 'ability_'.$this_ability_token);
+        $this_text_prefix = $this_existing > 0 ? 'One of the' : 'The';
+        $this_text_noun = 'woolly cloud'.($this_existing > 0 ? 's' : '');
+        $this_attachment_destroy_text = $this_text_prefix.' thunderous <span class="ability_name ability_type ability_type_electric">'.$this_text_noun.'</span> faded away...<br /> ';
+        $this_attachment_destroy_text .= 'That position on the field won\'t take end-of-turn damage any more! ';
+        $this_attachment_repeat_text = $this_text_prefix.' <span class="ability_name ability_type ability_type_electric">'.$this_text_noun.'</span> released a lightning bolt!';
+        $this_attachment_info = array(
+            'class' => 'ability',
+            'sticky' => true,
+            'ability_token' => $this_ability_token,
+            'ability_image' => $this_attachment_image,
+            'attachment_token' => $this_attachment_token,
+            'attachment_duration' => $this_attachment_duration,
+            'attachment_energy' => 0,
+            'attachment_energy_base_percent' => 15,
+            'attachment_sticky' => true,
+            'attachment_destroy' => array(
+                'trigger' => 'special',
+                'kind' => '',
+                'type' => '',
+                'percent' => true,
+                'modifiers' => false,
+                'frame' => 'defend',
+                'rates' => array(100, 0, 0),
+                'success' => array(9, -9999, -9999, 10, $this_attachment_destroy_text),
+                'failure' => array(9, -9999, -9999, 10, $this_attachment_destroy_text)
+                ),
+            'attachment_repeat' => array(
+                'kind' => 'energy',
+                'trigger' => 'damage',
+                'type' => 'electric',
+                'type2' => '',
+                'energy' => 15,
+                'percent' => true,
+                'modifiers' => true,
+                'frame' => 'damage',
+                'rates' => array(100, 0, 0),
+                'success' => array(9, -5, 30, 10, $this_attachment_repeat_text),
+                'failure' => array(9, -5, 30, 10, $this_attachment_repeat_text),
+                'options' => array(
+                    'apply_modifiers' => true,
+                    'apply_type_modifiers' => true,
+                    'apply_core_modifiers' => true,
+                    'apply_field_modifiers' => true,
+                    'apply_stat_modifiers' => false,
+                    'apply_position_modifiers' => false,
+                    'referred_damage' => true,
+                    'referred_damage_id' => 0,
+                    'referred_damage_stats' => array()
+                    )
+                ),
+            'ability_frame' => 0,
+            'ability_frame_animate' => array(0, 2, 4, 1, 3, 5),
+            'ability_frame_offset' => array(
+                'x' => (-5 + ($existing_attachments * 2)),
+                'y' => (40 + ($existing_attachments * 1)),
+                'z' => (-8 - $existing_attachments)
+                )
+            );
+        return $this_attachment_info;
+    }
+
+    // Define a static function for generating a static field attachment of an "acid glob" (from the Acid Glob ability)
+    public static function get_static_acid_glob($static_attachment_key, $this_attachment_duration = 99, $existing_attachments = 0){
+        $this_battle = rpg_battle::get_battle();
+        $this_attachment_json = json_encode($this_battle->battle_attachments);
+        $this_ability_token = 'acid-glob';
+        $this_attachment_token = 'ability_'.$this_ability_token.'_'.$static_attachment_key;
+        $this_attachment_image = $this_ability_token;
+        $this_existing = substr_count($this_attachment_json, 'ability_'.$this_ability_token);
+        $this_text_prefix = $this_existing > 0 ? 'One of the' : 'The';
+        $this_text_noun = 'acid glob'.($this_existing > 0 ? 's' : '');
+        $this_attachment_destroy_text = $this_text_prefix.' corrosive <span class="ability_name ability_type ability_type_water">'.$this_text_noun.'</span> faded away...<br /> ';
+        $this_attachment_destroy_text .= 'That position on the field won\'t take end-of-turn damage any more! ';
+        $this_attachment_repeat_text = $this_text_prefix.' <span class="ability_name ability_type ability_type_water">'.$this_text_noun.'</span> corroded its target\'s armor!';
+        $this_attachment_info = array(
+            'class' => 'ability',
+            'sticky' => true,
+            'ability_token' => $this_ability_token,
+            'ability_image' => $this_attachment_image,
+            'attachment_token' => $this_attachment_token,
+            'attachment_duration' => $this_attachment_duration,
+            'attachment_energy' => 0,
+            'attachment_energy_base_percent' => 8,
+            'attachment_sticky' => true,
+            'attachment_destroy' => array(
+                'trigger' => 'special',
+                'kind' => '',
+                'type' => '',
+                'percent' => true,
+                'modifiers' => false,
+                'frame' => 'defend',
+                'rates' => array(100, 0, 0),
+                'success' => array(9, -9999, -9999, 10, $this_attachment_destroy_text),
+                'failure' => array(9, -9999, -9999, 10, $this_attachment_destroy_text)
+                ),
+            'attachment_repeat' => array(
+                'kind' => 'energy',
+                'trigger' => 'damage',
+                'type' => 'water',
+                'type2' => '',
+                'energy' => 8,
+                'percent' => true,
+                'modifiers' => true,
+                'frame' => 'damage',
+                'rates' => array(100, 0, 0),
+                'success' => array(1, -5, 5, -10, $this_attachment_repeat_text),
+                'failure' => array(1, -5, 5, -99, $this_attachment_repeat_text),
+                'options' => array(
+                    'apply_modifiers' => true,
+                    'apply_type_modifiers' => true,
+                    'apply_core_modifiers' => true,
+                    'apply_field_modifiers' => true,
+                    'apply_stat_modifiers' => false,
+                    'apply_position_modifiers' => false,
+                    'referred_damage' => true,
+                    'referred_damage_id' => 0,
+                    'referred_damage_stats' => array()
+                    )
+                ),
+            'ability_frame' => 2,
+            'ability_frame_animate' => array(2, 3),
+            'ability_frame_offset' => array(
+                'x' => (5 + ($existing_attachments * 2)),
+                'y' => (0 + ($existing_attachments * 1)),
+                'z' => (10 - $existing_attachments)
+                )
+            );
+        return $this_attachment_info;
+    }
+
 }
 ?>
