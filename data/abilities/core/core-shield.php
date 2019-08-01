30f24f1792fa2240cbfc06ec814467948d2c4e32
AdrianMarceau <adrian.marceau@gmail.com>
2019-08-01 15:04:41 -0400

    Updated Core Shield ability to work with recent changes
    Also gave it a significant buff in that it'll create TWO shields if the user happens to be holding a core when using it.

--- data/abilities/core/core-shield.php
+++ data/abilities/core/core-shield.php
@@ -5,10 +5,9 @@ $ability = array(
     'ability_token' => 'core-shield',
     'ability_game' => 'MMRPG',
     'ability_group' => 'MMRPG/Weapons/Core',
-    'ability_description' => 'The user generates an elemental barrier that hovers in front of the target and protects it from {RECOVERY2}% of all damage that matches the shield\'s type! This ability\'s typing appears to be influenced by the energy of nearby cores, internal or otherwise...',
+    'ability_description' => 'The user generates an elemental barrier around itself or an ally that protects it from virtually all damage matching the user\'s core type!  If the user is holding a secondary elemental core when this ability is triggered, two shields will be generated instead of just one!',
     'ability_type' => 'shield',
     'ability_energy' => 4,
-    'ability_recovery2' => 99,
     'ability_accuracy' => 100,
     'ability_target' => 'select_this',
     'ability_function' => function($objects){
@@ -24,105 +23,62 @@ $ability = array(
             $current_target_robot = $this_robot;
         }
 
-        // Generate the static attachment info for the robot's elemental Core Shield
+        // Collect the first and second shield type, if applicable
+        $first_shield_type = $this_ability->ability_type;
+        $second_shield_type = false;
+        if (!empty($this_ability->ability_type2)
+            && $this_ability->ability_type2 !== 'shield'){
+            $second_shield_type = $this_ability->ability_type2;
+        }
+
+        // Generate the first static attachment info for the robot's elemental Core Shield
         $existing_shields = !empty($current_target_robot->robot_attachments) ? substr_count(implode('|', array_keys($current_target_robot->robot_attachments)), 'ability_core-shield_') : 0;
-        $this_attachment_info = rpg_ability::get_static_core_shield($this_ability->ability_type, 9, $existing_shields, $this_ability->ability_recovery2);
+        $shield_attachment_info = rpg_ability::get_static_core_shield($first_shield_type, 3, $existing_shields);
+        $shield_attachment_token = $shield_attachment_info['attachment_token'];
+        $shield_attachment = new rpg_ability($this_battle, $this_player, $this_robot, $shield_attachment_info);
+        $shield_attachment = new rpg_ability($this_battle, $this_player, $current_target_robot, $shield_attachment_info);
 
-        // Create the attachment object for this ability
-        $this_attachment = new rpg_ability($this_battle, $this_player, $current_target_robot, $this_attachment_info);
-        $this_attachment->update_session();
-
-        // If the ability flag was not set, attach the ability to the target
-        if (!$current_target_robot->has_attachment($this_attachment_token)){
-
-            // Target this robot's self
-            if ($this_robot->robot_gender == 'female'){ $pronoun = 'herself'; }
-            elseif ($this_robot->robot_gender == 'male'){ $pronoun = 'himself'; }
-            else { $pronoun = 'itself'; }
-            $this_ability->target_options_update(array(
-                'frame' => 'summon',
-                'success' => array(1, -10, 0, -10,
-                    $this_robot->print_name().' generates a '.$this_ability->print_name().'!<br />'.
-                    $this_robot->print_name().' attaches the shield to '.($this_robot->robot_id != $current_target_robot->robot_id ? $current_target_robot->print_name() : $pronoun).'!'
-                    )
-                ));
-            $this_robot->trigger_target($this_robot, $this_ability);
-
-            // If this robot is targetting itself
-            if ($this_robot->robot_id == $current_target_robot->robot_id){
-
-                // Target this robot's self
-                $this_ability->target_options_update($this_attachment_info['attachment_create']);
-                $this_robot->trigger_target($this_robot, $this_ability);
-
-                // Attach this ability attachment to the robot using it
-                $this_attachment_info['ability_frame_animate'] = $base_animation_sequence;
-                $this_robot->set_attachment($this_attachment_token, $this_attachment_info);
-
-            }
-            // Otherwise if targetting a team mate
-            else {
-
-                // Target this robot's self
-                $this_robot->robot_frame = 'base';
-                $this_robot->update_session();
-                $this_attachment->target_options_update($this_attachment_info['attachment_create']);
-                $current_target_robot->trigger_target($current_target_robot, $this_attachment);
-
-                // Attach this ability attachment to the robot using it
-                $this_attachment_info['ability_frame_animate'] = $base_animation_sequence;
-                $current_target_robot->set_attachment($this_attachment_token, $this_attachment_info);
-
-            }
-
-        }
-        // Else if the ability flag was set, reinforce the shield by its base duration value
-        else {
-
-            // If this robot is targetting itself
-            if ($this_robot->robot_id == $current_target_robot->robot_id){
-
-                // Collect the attachment from the robot to back up its info
-                $this_attachment_info = $this_robot->get_attachment($this_attachment_token);
-                $this_attachment_info['attachment_duration'] = $base_attachment_duration;
-                $this_attachment_info['ability_frame_animate'] = $base_animation_sequence;
-                $this_robot->set_attachment($this_attachment_token, $this_attachment_info);
-
-                // Target the opposing robot
-                $this_ability->target_options_update(array(
-                    'frame' => 'summon',
-                    'success' => array(0, -10, 0, -10,
-                        $this_robot->print_name().' reinforced the '.$this_ability->print_name().'!<br /> '.
-                        'The duration of '.$this_robot->print_name().'\'s protection was extended!'
-                        )
-                    ));
-                $this_robot->trigger_target($this_robot, $this_ability);
-
-            }
-            // Otherwise if targetting a team mate
-            else {
-
-                // Collect the attachment from the robot to back up its info
-                $this_attachment_info = $current_target_robot->get_attachment($this_attachment_token);
-                $this_attachment_info['attachment_duration'] = $base_attachment_duration;
-                $this_attachment_info['ability_frame_animate'] = $base_animation_sequence;
-                $current_target_robot->set_attachment($this_attachment_token, $this_attachment_info);
-
-                // Target the opposing robot
-                $this_attachment->target_options_update(array(
-                    'frame' => 'summon',
-                    'success' => array(0, -10, 0, -10, $this_robot->print_name().' reinforced the '.$this_ability->print_name().'!<br /> The duration of '.$current_target_robot->print_name().'\'s protection was extended!')
-                    ));
-                $this_robot->trigger_target($this_robot, $this_attachment);
-
-            }
-
+        // If applicable, generate the second static attachment info for the robot's elemental Core Shield
+        if (!empty($second_shield_type)){
+            $shield2_attachment_info = rpg_ability::get_static_core_shield($second_shield_type, 3, ($existing_shields + 1));
+            $shield2_attachment_token = $shield2_attachment_info['attachment_token'];
+            $shield2_attachment = new rpg_ability($this_battle, $this_player, $this_robot, $shield2_attachment_info);
+            $shield2_attachment = new rpg_ability($this_battle, $this_player, $current_target_robot, $shield2_attachment_info);
         }
 
-        // Either way, update this ability's settings to prevent recovery
-        $this_attachment->damage_options_update($this_attachment_info['attachment_destroy'], true);
-        $this_attachment->recovery_options_update($this_attachment_info['attachment_destroy'], true);
-        $this_attachment->update_session();
+        // Attach temporarily core shields to the user for animation only
+        $this_robot->set_attachment($shield_attachment_token.'_temp', $shield_attachment_info);
+        if (!empty($second_shield_type)){ $this_robot->set_attachment($shield2_attachment_token.'_temp', $shield2_attachment_info); }
+
+        // Show the summoning event for the one or two shields right now
+        $this_ability->target_options_update(array(
+            'frame' => 'summon',
+            'success' => array(0, -9999, -9999, -9999,
+                ($this_robot->robot_id !== $current_target_robot->robot_id ? ($this_robot->print_name().' targets '.$current_target_robot->print_name().'! <br /> ') : '').
+                $this_robot->print_name().' uses the '.$this_ability->print_name().' technique!'
+                )
+            ));
+        $this_robot->trigger_target($this_robot, $this_ability, array('prevent_default_text' => true));
+
+        // Remove temporary core shields from the user for animation only
+        $this_robot->unset_attachment($shield_attachment_token.'_temp');
+        if (!empty($second_shield_type)){ $this_robot->unset_attachment($shield2_attachment_token.'_temp'); }
+
+        // Now add the REAL core shields to the target (user or otherwise) and leave 'em
+        $current_target_robot->set_attachment($shield_attachment_token, $shield_attachment_info);
+        if (!empty($second_shield_type)){ $current_target_robot->set_attachment($shield2_attachment_token, $shield2_attachment_info); }
+
+        // Show the summoning event for the one or two shields right now
+        $secondary_text = (preg_match('/^(a|e|i|o|u)/i', $first_shield_type) ? 'an' : 'a').' '.rpg_type::print_span($first_shield_type).'-type shield';
+        if (!empty($second_shield_type)){ $secondary_text = rpg_type::print_span($first_shield_type).' and '.rpg_type::print_span($second_shield_type).'-type shields'; }
+        $this_ability->target_options_update(array(
+            'frame' => 'taunt',
+            'success' => array(0, -9999, -9999, -9999,
+                'The '.$this_ability->print_name().' generated '.$secondary_text.'! <br /> '.
+                $current_target_robot->print_name().' is now protected from the element'.(!empty($second_shield_type) ? 's' : '').'!'
+                )
+            ));
+        $current_target_robot->trigger_target($current_target_robot, $this_ability, array('prevent_default_text' => true));
 
         // Return true on success
         return true;
@@ -141,14 +97,17 @@ $ability = array(
         // Define the types for this ability
         $ability_types = array();
         $ability_types[] = $ability_base_type;
-        if (!empty($robot_core_type) && $robot_core_type != 'copy'){ $ability_types[] = $robot_core_type; }
-        if (!empty($robot_item_type) && $robot_item_type != 'copy'){ $ability_types[] = $robot_item_type; }
+        if (!empty($robot_core_type) && $robot_core_type != 'copy' && !in_array($robot_core_type, $ability_types)){ $ability_types[] = $robot_core_type; }
+        if (!empty($robot_item_type) && $robot_item_type != 'copy' && !in_array($robot_item_type, $ability_types)){ $ability_types[] = $robot_item_type; }
+        $ability_types = array_unique($ability_types);
         $ability_types = array_reverse($ability_types);
         $ability_types = array_slice($ability_types, 0, 2);
 
         // Collect this robot's primary type and change its image if necessary
         $this_ability->set_image($this_ability->ability_token.'_'.$ability_types[0]);
         $this_ability->set_type($ability_types[0]);
+        if (!empty($ability_types[1])){ $this_ability->set_type2($ability_types[1]); }
+        else { $this_ability->set_type2(''); }
 
         // Return true on success
         return true;
