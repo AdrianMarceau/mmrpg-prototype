e60d9bc6b29696f023c5ac255c5b97d681c49fa3
AdrianMarceau <adrian.marceau@gmail.com>
2019-10-14 22:17:28 -0400

    Updatd API functions w/ new print and return options

--- api/api-functions.php
+++ api/api-functions.php
@@ -1,11 +1,14 @@
 <?
 
+// Define some API error codes for later
+define('MMRPG_API_ERROR_NOTFOUND', 404);
+
 // Define a function for printing out a return array and then exiting
 function print_return_array($return_array, $return_markup = false){
     $new_return_array = array();
     if (isset($return_array['status'])){ $new_return_array['status'] = $return_array['status']; }
-    if (isset($return_array['code'])){ $new_return_array['code'] = $return_array['code']; }
     if (isset($return_array['message'])){ $new_return_array['message'] = $return_array['message']; }
+    if (isset($return_array['code'])){ $new_return_array['code'] = $return_array['code']; }
     if (isset($return_array['status']) && $return_array['status'] === 'success'){
         list($new_cache_date, $new_cache_time) = explode('-', MMRPG_CONFIG_CACHE_DATE);
         $yyyy = substr($new_cache_date, 0, 4); $mm = substr($new_cache_date, 4, 2); $dd = substr($new_cache_date, 6, 2);
@@ -42,18 +45,20 @@ function print_success_and_update_api_cache($success_data){
 }
 
 // Define a function for easily printing a successful request's data and then exiting
-function print_error_and_update_api_cache($error_message){
+function print_error_and_quit($error_message, $error_code = false){
     global $cache_file_path;
-    $return_array = array('status' => 'error', 'message' => $error_message);
-    print_and_update_api_cache($cache_file_path, $return_array);
+    $return_array = array('status' => 'error');
+    $return_array['message'] = $error_message;
+    if (!empty($error_code)){ $return_array['code'] = $error_code; }
+    if ($error_code === MMRPG_API_ERROR_NOTFOUND){ header("HTTP/1.0 404 Not Found"); }
+    print_return_array($return_array);
 }
-
 
 // Define a function for printing a critical error and exiting
 function critical_api_error($error_message, $file = '', $line = 0, $code = 0){
     $return_array = array('status' => 'error');
-    if (!empty($code)){ $return_array['code'] = $code; }
     $return_array['message'] = $error_message;
+    if (!empty($code)){ $return_array['code'] = $code; }
     if (MMRPG_CONFIG_IS_LIVE === false){
         $debug_info = '';
         if (!empty($file)){ $file = str_replace(MMRPG_CONFIG_ROOTDIR, '/', str_replace('\\', '/', $file)); }
