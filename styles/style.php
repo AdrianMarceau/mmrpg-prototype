625c5903e0b37ed4f8c49ad1778f93aada907965
AdrianMarceau <adrian.marceau@gmail.com>
2020-06-27 22:16:29 -0400

    Fixed bug w/ stylesheet generator type classes

--- styles/style.php
+++ styles/style.php
@@ -13,11 +13,12 @@ header("Pragma: cache");
 
 // Collect the cache style name from the headers, else die if not set
 $base_style_dir = MMRPG_CONFIG_ROOTDIR.'styles/';
-$base_style_name = !empty($_REQUEST['file']) && preg_match('/^([-_a-z0-9\.]+)\.css$/i', $_REQUEST['file']) ? $_REQUEST['file'] : false;
-if (empty($base_style_name)){ die('/* -- Invalid file name! -- */'); }
+$base_style_name = !empty($_REQUEST['file']) && preg_match('/^([-_a-z0-9\.]+)\.css$/i', $_REQUEST['file']) ? $_REQUEST['file'] : '';
+$base_style_names = !empty($_REQUEST['files']) && preg_match('/^([,-_a-z0-9\.]+)\.css$/i', $_REQUEST['files']) ? explode(',', $_REQUEST['files']) : array($base_style_name);
+if (empty($base_style_names)){ die('/* -- Invalid file name(s)! -- */'); }
 
 // Define the cache file name and path given everything we've learned
-$cache_file_name = 'cache.'.$base_style_name;
+$cache_file_name = 'cache.'.str_replace('.css', '', implode('-', $base_style_names)).'.css';
 $cache_file_path = MMRPG_CONFIG_CACHE_PATH.'styles/'.$cache_file_name;
 // Check to see if a file already exists and collect its last-modified date
 if (file_exists($cache_file_path)){ $cache_file_exists = true; $cache_file_date = date('Ymd-Hi', filemtime($cache_file_path)); }
@@ -34,17 +35,22 @@ if (MMRPG_CONFIG_CACHE_INDEXES === true && $cache_file_exists && $cache_file_dat
 // Define the variable to hold style markup and prepend to append
 $cache_style_markup = '';
 
-// Require the static CSS file if the exists
-ob_start();
-$static_file_path = $base_style_dir.$base_style_name;
-if (file_exists($static_file_path)){ require_once($static_file_path); }
-$cache_style_markup .= ob_get_clean();
+// Loop through the collected file name(s) and append markup
+foreach ($base_style_names AS $base_style_name){
 
-// Require the dynamic CSS additions if such a file exists
-ob_start();
-$dynamic_file_path = $base_style_dir.'style_'.str_replace('.css', '.php', $base_style_name);
-if (file_exists($dynamic_file_path)){ require_once($dynamic_file_path); }
-$cache_style_markup .= ob_get_clean();
+    // Require the static CSS file if the exists
+    ob_start();
+    $static_file_path = $base_style_dir.$base_style_name;
+    if (file_exists($static_file_path)){ require_once($static_file_path); }
+    $cache_style_markup .= ob_get_clean();
+
+    // Require the dynamic CSS additions if such a file exists
+    ob_start();
+    $dynamic_file_path = $base_style_dir.'style_'.str_replace('.css', '.php', $base_style_name);
+    if (file_exists($dynamic_file_path)){ require_once($dynamic_file_path); }
+    $cache_style_markup .= ob_get_clean();
+
+}
 
 // If there wasn't any markup collected so far, produce a 404 error
 if (empty($cache_style_markup)){
@@ -61,7 +67,7 @@ $cache_style_markup = str_replace('; ', ';', $cache_style_markup);
 $cache_style_headers = '';
 $cache_style_headers .= '/* '.PHP_EOL;
 $cache_style_headers .= ' * MMRPG Prototype Stylesheet '.PHP_EOL;
-$cache_style_headers .= ' * Filename: '.$base_style_name.' '.PHP_EOL;
+$cache_style_headers .= ' * Filename'.(count($base_style_names) > 1 ? 's' : '').': '.implode(', ', $base_style_names).' '.PHP_EOL;
 $cache_style_headers .= ' * Updated: '.($cache_file_exists ? $cache_file_date : date('Ymd-Hi')).' '.PHP_EOL;
 $cache_style_headers .= ' */'.PHP_EOL;
 $cache_style_markup = $cache_style_headers.trim($cache_style_markup);
