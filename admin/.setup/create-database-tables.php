c8619cdb00cf79ff6149470d3973b171481f4e39
AdrianMarceau <adrian.marceau@gmail.com>
2020-06-28 19:52:17 -0400

    Created setup script for creating DB tables

---
+++ admin/.setup/create-database-tables.php
@@ -0,0 +1,79 @@
+<?
+
+// Prevent game-related logic from running
+define('MMRPG_EXCLUDE_GAME_LOGIC', true);
+
+// Require the top file for paths and stuff
+$setup_dir = str_replace('\\', '/', dirname(__FILE__)).'/';
+$base_dir = dirname(dirname($setup_dir)).'/';
+require($base_dir.'top.php');
+
+// Define the header type so it's easier to display stuff
+header('Content-type: text/plain;');
+
+// ONLY allow this file to run in CLI mode
+if (php_sapi_name() !== 'cli'){
+    die('This setup script can ONLY be run in CLI mode!!!');
+}
+
+// Start the output buffer now, we'll flush manually as we go
+ob_implicit_flush(true);
+ob_start();
+
+// Define a quick function for immediately printing an echo statement
+function ob_echo($echo, $silent = false){ if (!$silent){ echo($echo.PHP_EOL); } ob_flush(); }
+
+// Define a quick function for executing a shell command and printing the output
+function ob_echo_shell_exec($cmd){ ob_echo('$ '.$cmd); $output = shell_exec($cmd); ob_echo($output); }
+
+// Define a function for cleaning a path of the root dir for printing
+function clean_path($path){ return str_replace(MMRPG_CONFIG_ROOTDIR, '/', $path); }
+
+// Print the script header for display purposes
+ob_echo('==========================');
+ob_echo('| CREATE DATABASE TABLES |');
+ob_echo('==========================');
+ob_echo('');
+
+// Collect a list of all the database table definitions in the setup directory
+$table_sql_dir = MMRPG_CONFIG_ROOTDIR.'admin/.sql/tables/';
+$table_sql_files = scandir($table_sql_dir);
+$table_sql_files = array_filter($table_sql_files, function($s){ if ($s !== '.' && $s !== '..' && substr($s, -4, 4) === '.sql'){ return true; } else { return false; } });
+
+// Check to make sure datbase table definitions were actually collected
+if (!empty($table_sql_files)){
+
+    // Print out the list of tables that will be created
+    ob_echo('The following database table defintions were found:');
+    ob_echo(implode(PHP_EOL.'- ', $table_sql_files));
+    ob_echo('');
+
+    // Loop through the table files and run them against the database if not exist
+    foreach ($table_sql_files AS $sql_key => $sql_file){
+        $table_name = str_replace('.sql', '', $sql_file);
+        ob_echo('----------');
+        ob_echo('Processing database table "'.$table_name.'" ...');
+        if (!$db->table_exists($table_name)){
+            $sql_query = file_get_contents($table_sql_dir.$sql_file);
+            $db->query($sql_query);
+            ob_echo('- new table created!');
+        } else {
+            ob_echo('- table already exists!');
+        }
+    }
+    ob_echo('----------');
+    ob_echo('');
+
+} else {
+
+    // Print out the list of tables that will be created
+    ob_echo('Unable to find any database tables at '.clean_path($table_sql_dir).'...');
+    ob_echo('');
+
+}
+
+ob_echo('...Done!');
+
+exit();
+
+?>
