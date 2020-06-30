daec87c81d1ff32de068d79272f40fc54c442896
AdrianMarceau <adrian.marceau@gmail.com>
2020-06-30 13:19:45 -0400

    Renamed migrate subrepo file to match prev, updated all refs

--- admin/.migrate/x-all.sh
+++ admin/.migrate/x-all.sh
@@ -8,7 +8,7 @@ for REPO_KIND in "${REPO_KINDS[@]}"
 do
 
     echo "##########################################"
-    "${ADMIN_PATH}.migrate/x-migrate-repo.sh" "${REPO_KIND}"
+    "${ADMIN_PATH}.migrate/x-migrate-subrepo.sh" "${REPO_KIND}"
     "${ADMIN_PATH}.migrate/x-reset-subrepo.sh" "${REPO_KIND}"
 
 done
