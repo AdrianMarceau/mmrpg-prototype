f2f5c88fcd043ab569518482ce82b9a021f19769
AdrianMarceau <adrian.marceau@gmail.com>
2020-07-11 01:28:39 -0400

    Updated migration scripts to user contributor username instead of ID during export

--- admin/.migrate/migrate-objects_xcommon.php
+++ admin/.migrate/migrate-objects_xcommon.php
@@ -4,7 +4,8 @@
 $contributor_fields = rpg_user::get_contributor_index_fields(true, 'contributors');
 $contributor_index = $db->get_array_list("SELECT
     {$contributor_fields},
-    users.user_id
+    users.user_id,
+    users.user_name_clean
     FROM mmrpg_users_contributors AS contributors
     LEFT JOIN mmrpg_users AS users ON users.contributor_id = contributors.contributor_id
     ORDER BY contributor_id ASC
@@ -12,10 +13,10 @@ $contributor_index = $db->get_array_list("SELECT
 
 // Create a cross-reference for translating user to contributor IDs
 $user_ids_to_contributor_ids = array();
-$contributor_ids_to_user_ids = array();
+$user_ids_to_contributor_usernames = array();
 foreach ($contributor_index AS $key => $data){
     $user_ids_to_contributor_ids[$data['user_id']] = $data['contributor_id'];
-    $user_ids_to_contributor_ids[$data['contributor_id']] = $data['user_id'];
+    $user_ids_to_contributor_usernames[$data['user_id']] = $data['user_name_clean'];
 }
 
 
--- admin/.migrate/migrate-objects_xfunctions.php
+++ admin/.migrate/migrate-objects_xfunctions.php
@@ -141,15 +141,18 @@ function clean_json_content_array($kind, $content_json_data){
         }
     }
     // If there are an image editor fields, translate them to contributor IDs
-    global $user_ids_to_contributor_ids;
+    global $user_ids_to_contributor_usernames;
     $image_fields = array($kind.'_image_editor', $kind.'_image_editor2');
     foreach ($image_fields AS $image_field){
+        if (!isset($cleaned_json_data[$image_field])){ continue; }
         if (!empty($cleaned_json_data[$image_field])){
             $user_id = $cleaned_json_data[$image_field];
-            if (!empty($user_ids_to_contributor_ids[$user_id])){
-                $contributor_id = $user_ids_to_contributor_ids[$user_id];
-                $cleaned_json_data[$image_field] = $contributor_id;
+            if (!empty($user_ids_to_contributor_usernames[$user_id])){
+                $contributor_name = $user_ids_to_contributor_usernames[$user_id];
+                $cleaned_json_data[$image_field] = $contributor_name;
             }
+        } else {
+            $cleaned_json_data[$image_field] = '';
         }
     }
     // Return the cleaned JSON data
