b2be017268dd7b8865e3d0af6421b28fdbc06322
AdrianMarceau <adrian.marceau@gmail.com>
2020-07-15 20:07:32 -0400

    Added various git tracking/printing functions to cms admin class

--- classes/cms_admin.php
+++ classes/cms_admin.php
@@ -134,6 +134,7 @@ class cms_admin {
         return $mmrpg_contributors_index;
     }
 
+
     // Define a function for printing the environment relationship header
     public static function print_env_rel_header($env1, $dir, $env2){
         if ($dir === '<' || $dir === 'left'){ $icon = 'angle-double-left'; }
@@ -168,21 +169,49 @@ class cms_admin {
         // Loop through the option and generate item markup for each of them
         ob_start();
         foreach ($this_group_options AS $option_key => $option_info){
+            $repo_status = '';
+            $repo_status_icon = '';
+            if (!empty($option_info['repo'])){
+                $repo_config = $option_info['repo'];
+                $repo_status = self::get_admin_home_group_option_status($repo_config, $git_changes);
+                if (!empty($repo_status)){
+                    $unique_changes = array();
+                    foreach ($git_changes AS $k => $p){ list($t) = explode('/', $p); if (!in_array($t, $unique_changes)){ $unique_changes[] = $t; }  }
+                    $unique_changes_count = count($unique_changes);
+                    $class = 'status has_'.$repo_status;
+                    $title = $unique_changes_count.' '.ucwords(str_replace('_', ' ', $repo_status));
+                    $count = '<span class="count">('.$unique_changes_count.')</span>';
+                    if ($repo_status === 'uncommitted_changes'){ $icon = '<i class="icon fa fa-asterisk"></i>'; }
+                    elseif ($repo_status === 'committed_changes'){ $icon = '<i class="icon fa fa-check"></i>'; }
+                    else { $icon = '<i class="icon fa fa-question"></i>'; }
+                    if (!empty($option_info['link']['url'])){
+                        $href = $option_info['link']['url'].'?subaction=search&'.$repo_config['data']['prefix'].'_flag_changed=1';
+                        $repo_status_icon .= '<a class="'.$class.'" href="'.$href.'" title="'.$title.'">'.$icon.' '.$count.'</a>';
+                    } else {
+                        $repo_status_icon .= '<span class="'.$class.'" title="'.$title.'">'.$icon.' '.$count.'</span>';
+                    }
+                }
+            }
             echo('<li class="item">'.PHP_EOL);
                 $link_url = isset($option_info['link']['url']) ? ' href="'.$option_info['link']['url'].'"' : '';
                 $link_target = isset($option_info['link']['target']) ? ' target="'.$option_info['link']['target'].'"' : '';
                 $link_text = $option_info['link']['text'];
-                echo('<div class="link"><a'.$link_url.$link_target.'>'.$link_text.'</a></div>'.PHP_EOL);
+                echo('<div class="link"><a'.$link_url.$link_target.'>'.$link_text.'</a>'.$repo_status_icon.'</div>'.PHP_EOL);
                 $desc_text = $option_info['desc'];
                 echo('<div class="desc"><em>'.$desc_text.'</em></div>'.PHP_EOL);
                 if (!empty($option_info['buttons'])){
-                    echo('<div class="buttons">'.PHP_EOL);
+                    $buttons_markup = array();
                     foreach ($option_info['buttons'] AS $button_key => $button_info){
+                        if (isset($button_info['condition']['status']) && $button_info['condition']['status'] !== $repo_status){ continue; }
                         $button_action = $button_info['action'];
                         $button_text = $button_info['text'];
-                        echo('<a class="button" data-action="'.$button_action.'">'.$button_text.'</a>'.PHP_EOL);
+                        $buttons_markup[] = '<a class="button" data-action="'.$button_action.'">'.$button_text.'</a>';
                     }
-                    echo('</div>'.PHP_EOL);
+                    if (!empty($buttons_markup)){
+                        echo('<div class="buttons">'.PHP_EOL);
+                            echo(trim(implode(PHP_EOL, $buttons_markup)).PHP_EOL);
+                        echo('</div>'.PHP_EOL);
+                    }
                 }
             echo('</li>'.PHP_EOL);
         }
@@ -205,6 +234,165 @@ class cms_admin {
         return $this_group_list_markup;
 
     }
+
+    // Define a function for checking if a given home page option has uncommit (unstage or untracked) changes
+    public static function get_admin_home_group_option_status($repo_config, &$git_changes){
+        if (empty($repo_config['path'])){ return false; }
+        $git_changes = cms_admin::git_get_changes($repo_config['path']);
+        if (!empty($repo_config['filter'])){ $git_changes = self::git_filter_list_by_data($git_changes, $repo_config['filter']); }
+        if (!empty($git_changes)){ return 'uncommitted_changes'; }
+        else { return ''; }
+    }
+
+    // Define a function for printing git change buttons and list status in an object editor form
+    public static function print_object_editor_git_footer_buttons($repo_kind, $path_token, $mmrpg_git_changes, $mmrpg_git_changes_tokens = array()){
+
+        // Compensate if individual tokens were not provided
+        if (empty($mmrpg_git_changes_tokens)){
+            $mmrpg_git_changes_tokens = array();
+            foreach ($mmrpg_git_changes AS $key => $path){ list($token) = explode('/', $path); $mmrpg_git_changes_tokens[] = $token; }
+            $mmrpg_git_changes_tokens = array_unique($mmrpg_git_changes_tokens);
+        }
+
+        // Generate a list of print-friendly items showing the changes
+        $print_changes = $mmrpg_git_changes;
+        $print_changes = array_filter($print_changes, function($path) use($path_token){ list($token) = explode('/', $path); return $token === $path_token ? true : false; });
+        $print_changes = array_map(function($path) use($path_token){ return str_replace($path_token.'/', '', $path); }, $print_changes);
+
+        // Generate button markup if applicable to this object
+        ob_start();
+        if (in_array($path_token, $mmrpg_git_changes_tokens)){
+            ?>
+            <div class="buttons git-buttons">
+                <input class="button revert" type="button" value="Revert Changes" />
+                <input class="button commit" type="button" value="Commit &amp; Publish Changes" />
+                <div class="git-changes">
+                    <strong>Uncommitted Changes</strong>
+                    <ul><li><?= implode('</li><li>', $print_changes) ?></li></ul>
+                </div>
+            </div>
+            <?
+        }
+        $temp_markup = ob_get_clean();
+
+        // Return the generate markup
+        return $temp_markup;
+    }
+
+
+    /* -- Git Functions -- */
+
+    // Define a function for checking if there are any unstaged and/or untracked files in a given repo (w/ optional path filter)
+    public static function git_get_changes($repo_base_path, $filter_path = '', $filter_data = array()){
+        static $index;
+        if (!is_array($index)){ $index = array(); }
+        if (!isset($index[$repo_base_path])){
+            $unstaged = self::git_get_unstaged($repo_base_path);
+            $untracked = self::git_get_untracked($repo_base_path);
+            $changes = array_merge($unstaged, $untracked);
+            $index[$repo_base_path] = $changes;
+        } else {
+            $changes = $index[$repo_base_path];
+        }
+        if (!empty($filter_path)){ $changes = self::git_filter_list_by_path($changes, $filter_path); }
+        if (!empty($filter_data)){ $changes = self::git_filter_data($changes, $filter_data); }
+        return array_values($changes);
+    }
+
+    // Define a function for checking if there are any unstaged files in a given repo (w/ optional path filter)
+    public static function git_get_unstaged($repo_base_path, $filter_path = ''){
+        static $index;
+        if (!is_array($index)){ $index = array(); }
+        if (!isset($index[$repo_base_path])){
+            $unstaged = shell_exec('cd '.$repo_base_path.' && git diff --name-only');
+            //echo('$unstaged = '.print_r($unstaged, true).PHP_EOL);
+            $unstaged = !empty($unstaged) ? explode("\n", trim($unstaged)) : array();
+            $index[$repo_base_path] = $unstaged;
+        } else {
+            $unstaged = $index[$repo_base_path];
+        }
+        if (!empty($filter_path)){ $unstaged = self::git_filter_list_by_path($unstaged, $filter_path); }
+        return array_values($unstaged);
+    }
+
+    // Define a function for checking if there are any untracked files in a given repo (w/ optional path filter)
+    public static function git_get_untracked($repo_base_path, $filter_path = ''){
+        static $index;
+        if (!is_array($index)){ $index = array(); }
+        if (!isset($index[$repo_base_path])){
+            $untracked = shell_exec('cd '.$repo_base_path.' && git ls-files --others --exclude-standard');
+            //echo('$untracked = '.print_r($untracked, true).PHP_EOL);
+            $untracked = !empty($untracked) ? explode("\n", trim($untracked)) : array();
+            $index[$repo_base_path] = $untracked;
+        } else {
+            $untracked = $index[$repo_base_path];
+        }
+        if (!empty($filter_path)){ $untracked = self::git_filter_list_by_path($untracked, $filter_path); }
+        return array_values($untracked);
+    }
+
+    // Define a function for checking if there are any commited (but unpushed) files in a given repo (w/ optional path filter)
+    public static function git_get_committed($repo_base_path, $filter_path = ''){
+        static $index;
+        if (!is_array($index)){ $index = array(); }
+        if (!isset($index[$repo_base_path])){
+            $committed = shell_exec('cd '.$repo_base_path.' && git log origin/master..HEAD --name-only --oneline');
+            //echo('$committed = '.print_r($committed, true).PHP_EOL);
+            $committed = !empty($committed) ? explode("\n", trim($committed)) : array();
+            foreach ($committed AS $key => $path){ if (strstr($path, ' ')){ unset($committed[$key]); } }
+            $index[$repo_base_path] = $committed;
+        } else {
+            $committed = $index[$repo_base_path];
+        }
+        if (!empty($filter_path)){ $committed = self::git_filter_list_by_path($committed, $filter_path); }
+        return array_values($committed);
+    }
+
+    // Define a quick function for filtering a given list of git changes by base path
+    public static function git_filter_list_by_path($list, $filter_path){
+        if (empty($list)){ return array(); }
+        elseif (empty($filter_path)){ return $list; }
+        foreach ($list AS $key => $path){ if (substr($path, 0, strlen($filter_path)) !== $filter_path){ unset($list[$key]); } }
+        return array_values($list);
+    }
+
+    // Define a quick function for filtering a given list of git changes by its data
+    public static function git_filter_list_by_data($list, $filter_data){
+        if (empty($list)){ return array(); }
+        elseif (empty($filter_data)){ return $list; }
+        elseif (empty($filter_data['table']) || empty($filter_data['token'])){ return $list; }
+        global $db;
+        $filter_by_table = $filter_data['table'];
+        $filter_by_token = $filter_data['token'];
+        $filter_by_extra = !empty($filter_data['extra']) ? $filter_data['extra'] : array();
+        //echo('<pre>git_filter_list_by_data()</pre>'.PHP_EOL);
+        //echo('<pre>$list = '.print_r($list, true).'</pre>'.PHP_EOL);
+        //echo('<pre>$filter_by_table = '.print_r($filter_by_table, true).'</pre>'.PHP_EOL);
+        //echo('<pre>$filter_by_token = '.print_r($filter_by_token, true).'</pre>'.PHP_EOL);
+        //echo('<pre>$filter_by_extra = '.print_r($filter_by_extra, true).'</pre>'.PHP_EOL);
+        $filter_tokens = array();
+        foreach ($list AS $key => $path){ list($token) = explode('/', $path); $filter_tokens[] = $token; }
+        $filter_tokens_string = "'".implode("', '", array_unique($filter_tokens))."'";
+        //echo('<pre>$filter_tokens = '.print_r($filter_tokens, true).'</pre>'.PHP_EOL);
+        //echo('<pre>$filter_tokens_string = '.print_r($filter_tokens_string, true).'</pre>'.PHP_EOL);
+        $filter_extras = array();
+        if (!empty($filter_by_extra)){ foreach ($filter_by_extra AS $f => $v){ $filter_extras[] = "{$f} = '".(is_numeric($v) ? $v : str_replace("'", "\\'", $v))."'"; } }
+        $filter_extras_string = !empty($filter_extras) ? ' AND '.implode(' AND ', $filter_extras) : '';
+        //echo('<pre>$filter_extras = '.print_r($filter_extras, true).'</pre>'.PHP_EOL);
+        //echo('<pre>$filter_extras_string = '.print_r($filter_extras_string, true).'</pre>'.PHP_EOL);
+        $filter_query = "SELECT {$filter_by_token} FROM {$filter_by_table} WHERE {$filter_by_token} IN ($filter_tokens_string){$filter_extras_string};";
+        $filter_query_results = $db->get_array_list($filter_query, $filter_by_token);
+        //echo('<pre>$filter_query = '.print_r($filter_query, true).'</pre>'.PHP_EOL);
+        //echo('<pre>$filter_query_results = '.print_r($filter_query_results, true).'</pre>'.PHP_EOL);
+        $allowed_tokens = is_array($filter_query_results) ? array_keys($filter_query_results) : array();
+        //echo('<pre>$allowed_tokens = '.print_r($allowed_tokens, true).'</pre>'.PHP_EOL);
+        foreach ($list AS $key => $path){ list($token) = explode('/', $path); if (!in_array($token, $allowed_tokens)){ unset($list[$key]); } }
+        $list = array_values($list);
+        //echo('<pre>$list = '.print_r($list, true).'</pre>'.PHP_EOL);
+        //exit();
+        return array_values($list);
+    }
+
 
 }
 ?>
