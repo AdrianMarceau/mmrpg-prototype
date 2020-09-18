f7308108ffdca561bb288a9c0df22a638cefa277
AdrianMarceau <adrian.marceau@gmail.com>
2020-09-18 10:17:13 -0400

    Fixed bug in battle restart action

--- battle/actions/restart.php
+++ battle/actions/restart.php
@@ -8,15 +8,7 @@ $restart_player_id = $battle_players_index[$this_player->player_token]['player_i
 $restart_player_token = $this_player->player_token;
 
 // Redefine the player's robots string
-$this_player_robots = array();
-if (!empty($this_player->player_robots)){
-    foreach ($this_player->player_robots AS $key => $temp_robotinfo){
-        $temp_token = $temp_robotinfo['robot_token'];
-        $temp_id = $battle_robots_index[$temp_token]['robot_id'];
-        $this_player_robots[] = $temp_id.'_'.$temp_token;
-    }
-}
-$this_player_robots = implode(',', $this_player_robots);
+$this_player_robots = !empty($_REQUEST['this_player_robots']) ? $_REQUEST['this_player_robots'] : '00_robot';
 
 // Redirect the user back to the prototype screen
 $this_redirect = 'battle.php?'.
@@ -33,13 +25,5 @@ $this_redirect = 'battle.php?'.
     //'&target_player_token='.$target_player->player_token.
     (!empty($_SESSION['BATTLES_CHAIN']) ? '&flag_skip_fadein=true' : '').
     '';
-
-// Automatically empty all temporary battle variables
-$_SESSION['BATTLES'] = array();
-$_SESSION['FIELDS'] = array();
-$_SESSION['PLAYERS'] = array();
-$_SESSION['ROBOTS'] = array();
-$_SESSION['ABILITIES'] = array();
-$_SESSION['ITEMS'] = array();
 
 ?>
