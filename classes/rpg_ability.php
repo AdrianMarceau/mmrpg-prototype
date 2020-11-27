169003d5ab0229e22b57a3891eb1f0247f40d174
Adrian Marceau <adrian.marceau@gmail.com>
2020-11-26 23:44:08 -0500

    Updated elemental shot abilities to fire multiple times based on relative speed

--- classes/rpg_ability.php
+++ classes/rpg_ability.php
@@ -2833,32 +2833,54 @@ class rpg_ability extends rpg_object {
         $options = rpg_game::new_options_object();
         $extra_objects = array('this_ability' => $this_ability, 'options' => $options);
 
+        // Check speed to see how many times buster shot can hit
+        $options->num_buster_shots = 1;
+        if ($this_robot->robot_speed > $target_robot->robot_speed){
+            $options->num_buster_shots = floor($this_robot->robot_speed / $target_robot->robot_speed);
+        }
+
         // Trigger this robot's item function if one has been defined for this context
         $this_robot->trigger_custom_function('rpg-ability_elemental-shot_before', $extra_objects);
         if ($options->return_early){ return $options->return_value; }
 
-        // Update the ability's target options and trigger
-        $this_ability->target_options_update(array(
-            'frame' => 'shoot',
-            'success' => array(0, 105, 0, 10, $this_robot->print_name().' fires '.(preg_match('/^(a|e|i|o|u)/i', $this_ability->ability_token) ? 'an' : 'a').'  '.$this_ability->print_name().'!')
-            ));
-        $this_robot->trigger_target($target_robot, $this_ability);
+        // Loop through the allowed number of shots and fire that many times
+        for ($num_shot = 1; $num_shot <= $options->num_buster_shots; $num_shot++){
 
-        // Inflict damage on the opposing robot
-        $this_ability->damage_options_update(array(
-            'kind' => 'energy',
-            'kickback' => array(10, 0, 0),
-            'success' => array(0, -60, 0, 10, 'The '.$shot_text.' shot '.$damage_text.' the target!'),
-            'failure' => array(0, -60, 0, -10, 'The '.$this_ability->print_name().' missed&hellip;')
-            ));
-        $this_ability->recovery_options_update(array(
-            'kind' => 'energy',
-            'kickback' => array(10, 0, 0),
-            'success' => array(0, -60, 0, 10, 'The '.$shot_text.' shot '.$recovery_text.' the target!'),
-            'failure' => array(0, -60, 0, -10, 'The '.$this_ability->print_name().' missed&hellip;')
-            ));
-        $energy_damage_amount = $this_ability->ability_damage;
-        $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+            // Update the ability's target options and trigger
+            $target_text = '';
+            $target_options = array();
+            if ($num_shot === 1){
+                $target_text = $this_robot->print_name().' fires '.(preg_match('/^(a|e|i|o|u)/i', $this_ability->ability_token) ? 'an' : 'a').'  '.$this_ability->print_name().'!';
+            } else {
+                $target_text = $this_robot->print_name().' fires another '.$this_ability->print_name().'!';
+                $target_options['prevent_default_text'] = true;
+            }
+            $this_ability->target_options_update(array(
+                'frame' => 'shoot',
+                'success' => array(0, 105, 0, 10, $target_text)
+                ));
+            $this_robot->trigger_target($target_robot, $this_ability, $target_options);
+
+            // Inflict damage on the opposing robot
+            $this_ability->damage_options_update(array(
+                'kind' => 'energy',
+                'kickback' => array(10, 0, 0),
+                'success' => array(0, -60, 0, 10, 'The '.$shot_text.' shot '.$damage_text.' the target!'),
+                'failure' => array(0, -60, 0, -10, 'The '.$this_ability->print_name().' missed&hellip;')
+                ));
+            $this_ability->recovery_options_update(array(
+                'kind' => 'energy',
+                'kickback' => array(10, 0, 0),
+                'success' => array(0, -60, 0, 10, 'The '.$shot_text.' shot '.$recovery_text.' the target!'),
+                'failure' => array(0, -60, 0, -10, 'The '.$this_ability->print_name().' missed&hellip;')
+                ));
+            $energy_damage_amount = $this_ability->ability_damage;
+            $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+
+            // Break early if the target has been disabled
+            if ($target_robot->robot_energy < 1 || $target_robot->robot_status === 'disabled'){ break; }
+
+        }
 
         // Trigger this robot's item function if one has been defined for this context
         $this_robot->trigger_custom_function('rpg-ability_elemental-shot_after', $extra_objects);
