4f1447a18f955476fa66ecfa359d7e4def0dc446
Adrian Marceau <adrian.marceau@gmail.com>
2021-01-28 23:43:54 -0500

    Duplicated formatting guide to v2 so we can make tweaks

---
+++ functions/website_formatting.v2.php
@@ -0,0 +1,79 @@
+-----
+
+[align-left][code][b]bold[/b] | [i]italic[/i] | [u]underline[/u] | [s]strike[/s] | [tab][/code][/align-left]
+[align-left][b]bold[/b] | [i]italic[/i] | [u]underline[/u] | [s]strike[/s] | [tab][/align-left]
+
+
+-----
+
+[align-left][code][link](http://google.ca/) | [image](images/assets/favicon.png)[/code][/align-left]
+[align-left][link](http://google.ca/) | [image](images/assets/favicon.png)[/align-left]
+
+-----
+
+[align-left][code][type]{freeze} | [dual type]{freeze_flame} | [secret text]{spoiler}[/code][/align-left]
+[align-left][type]{freeze} | [dual type]{freeze_flame} | [secret text]{spoiler}[/align-left]
+
+[align-left][code][type-freeze]
+freeze block with lots of text
+spanning multiples lines
+[/type-freeze][/code][/align-left]
+[align-left][type-freeze]freeze block with lots of text
+spanning multiples lines
+[/type-freeze][/align-left]
+
+-----
+
+[align-left][code][size-large]text[/size-large] | [size-medium]text[/size-medium] | [size-small]text[/size-small][/code][/align-left]
+[align-left][size-large]large text[/size-large] | [size-medium]medium text[/size-medium] | [size-small]small text[/size-small][/align-left]
+
+-----
+
+[align-left][code][align-left]left aligned text[/align-left][/code][/align-left]
+[align-left]left aligned text[/align-left]
+
+[align-center][code][align-center]center aligned text[/align-center][/code][/align-center]
+[align-center]center aligned text[/align-center]
+
+[align-left][code][align-right]right aligned text[/align-right][/code][/align-left]
+[align-right]right aligned text[/align-right]
+
+-----
+
+[align-left][code][color=#B25A5A]color by hex[/color] | [color=178,90,90]color by rgb[/color][/code][/align-left]
+[align-left][color=#B25A5A]color by hex[/color] | [color=178,90,90]color by rgb[/color][/align-left]
+
+-----
+
+[align-left][code][code][display code]{electric} [b]without[/b] [u]formatting[/u][/code][/code][/align-left]
+[align-left][code][display code]{electric} [b]without[/b] [u]formatting[/u][/code][/align-left]
+
+-----
+
+[align-left][code][robot]{bubble-man} [robot:right]{bubble-man}[/code][/align-left]
+[align-left][robot]{bubble-man} [robot:right]{bubble-man}[/align-left]
+
+[align-left][code][robot:left:base]{flame-man} [robot:right:taunt]{heat-man} [robot:left:shoot]{fire-man}[/code][/align-left]
+[align-left][robot:left:base]{flame-man} [robot:right:taunt]{heat-man} [robot:left:shoot]{fire-man}[/align-left]
+
+[align-left][code][player:left:base]{dr-light} [mecha:right:taunt]{picket-man}[/code][/align-left]
+[align-left][player:left:base]{dr-light} [mecha:right:taunt]{picket-man}[/align-left]
+
+[align-left][code][ability:left:04]{mega-buster} [item:right:01]{screw-large}[/code][/align-left]
+[align-left][ability:left:04]{mega-buster} [item:right:01]{screw-large}[/align-left]
+
+-----
+
+[align-left][code][foreground-gentle-countryside] [/foreground-gentle-countryside][/code][/align-left]
+[align-left][foreground-gentle-countryside] [/foreground-gentle-countryside][/align-left]
+
+[align-left][code][background-gentle-countryside] [/background-gentle-countryside][/code][/align-left]
+[align-left][background-gentle-countryside] [/background-gentle-countryside][/align-left]
+
+[align-left][code][background-gentle-countryside-top] [/background-gentle-countryside-top][/code][/align-left]
+[align-left][background-gentle-countryside-top] [/background-gentle-countryside-top][/align-left]
+
+[align-left][code][background-gentle-countryside-bottom] [/background-gentle-countryside-bottom][/code][/align-left]
+[align-left][background-gentle-countryside-bottom] [/background-gentle-countryside-bottom][/align-left]
+
+-----
