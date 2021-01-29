3a4bacf6469aaeb635fd2d614df1417891661197
Adrian Marceau <adrian.marceau@gmail.com>
2021-01-28 23:44:46 -0500

    Created new script for getting misc markup over ajax

---
+++ scripts/get-markup.php
@@ -0,0 +1,23 @@
+<?php
+
+// Require the global config file
+require('../top.php');
+
+// Return markup based on provided arguments
+$request_kind = !empty($_REQUEST['kind']) ? $_REQUEST['kind'] : false;
+if ($request_kind === 'community-formatting-help'){
+
+    // Return the markup for the community formatting guide
+    header('Content-type: text/html; charset=UTF-8');
+    echo(mmrpg_formatting_help_markup());
+    exit();
+
+} else {
+
+    // Return a 404 header as this is an undefined request
+    http_response_code(404);
+    exit();
+
+}
+
+?>
