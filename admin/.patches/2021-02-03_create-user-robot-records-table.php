4ee0d484f6f58956bfa58ea74a6b91a9fad9ce3b
Adrian Marceau <adrian.marceau@gmail.com>
2021-02-03 21:56:25 -0500

    Split latest patch into two parts so it would work properly on pull

--- admin/.patches/2021-02-03_create-user-robot-records-table.php
+++ admin/.patches/2021-02-03_create-user-robot-records-table.php
@@ -24,40 +24,6 @@ if (!in_array('mmrpg_users_records_robots', $db_tables_list)){
         ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC
         ;");
 
-    // Refresh the list of tables from the database
-    $db_tables_list = $db->table_list();
-
-    // If `mmrpg_users_records_robots` table exists now
-    if (in_array('mmrpg_users_records_robots', $db_tables_list)){
-
-        // Collect a list of all users and their save data so we can populate the table
-        echo('- populating `mmrpg_users_records_robots` table with user data'.PHP_EOL);
-        $user_robot_database_records = $db->get_array_list("SELECT
-            saves.user_id,
-            saves.save_values_robot_database
-            FROM mmrpg_saves AS saves
-            LEFT JOIN mmrpg_leaderboard AS board ON board.user_id = saves.user_id
-            WHERE board.board_points > 0 AND saves.user_id > 0
-            ORDER BY board.board_date_modified DESC
-            ;", 'user_id');
-
-        // Loop through all the users found and update their database tables
-        if (!empty($user_robot_database_records)){
-            foreach ($user_robot_database_records AS $user_id => $save_data){
-                if (empty($save_data['save_values_robot_database'])){ continue; }
-                $user_robot_database = json_decode($save_data['save_values_robot_database'], true);
-                if (!empty($user_robot_database)){
-                    echo('- adding records for user_id '.$user_id.PHP_EOL);
-                    //error_log('$user_id = '.print_r($user_id, true));
-                    //error_log('$user_robot_database = '.print_r($user_robot_database, true));
-                    mmrpg_update_user_robot_records($user_id, $user_robot_database);
-                    //break;
-                }
-            }
-        }
-
-    }
-
 }
 
 ?>
---
+++ admin/.patches/2021-02-03_create-user-robot-records-table_pt2.php
@@ -0,0 +1,50 @@
+<?php
+
+// Collect a list of tables from the database
+$db_tables_list = $db->table_list();
+
+// If `mmrpg_users_records_robots` table exists now
+if (in_array('mmrpg_users_records_robots', $db_tables_list)
+    && function_exists('mmrpg_update_user_robot_records')){
+
+    // Check to see if the newly created table is completely empty
+    $existing_records = $db->get_value("SELECT
+        COUNT(*) AS num_records
+        FROM mmrpg_users_records_robots
+        WHERE user_id > 0
+        ;", 'num_records');
+
+    // If there are NO records, we should populate them now
+    if (empty($existing_records)){
+
+        // Collect a list of all users and their save data so we can populate the table
+        echo('- populating `mmrpg_users_records_robots` table with user data'.PHP_EOL);
+        $user_robot_database_records = $db->get_array_list("SELECT
+            saves.user_id,
+            saves.save_values_robot_database
+            FROM mmrpg_saves AS saves
+            LEFT JOIN mmrpg_leaderboard AS board ON board.user_id = saves.user_id
+            WHERE board.board_points > 0 AND saves.user_id > 0
+            ORDER BY board.board_date_modified DESC
+            ;", 'user_id');
+
+        // Loop through all the users found and update their database tables
+        if (!empty($user_robot_database_records)){
+            foreach ($user_robot_database_records AS $user_id => $save_data){
+                if (empty($save_data['save_values_robot_database'])){ continue; }
+                $user_robot_database = json_decode($save_data['save_values_robot_database'], true);
+                if (!empty($user_robot_database)){
+                    echo('- adding records for user_id '.$user_id.PHP_EOL);
+                    //error_log('$user_id = '.print_r($user_id, true));
+                    //error_log('$user_robot_database = '.print_r($user_robot_database, true));
+                    mmrpg_update_user_robot_records($user_id, $user_robot_database);
+                    //break;
+                }
+            }
+        }
+
+    }
+
+}
+
+?>
