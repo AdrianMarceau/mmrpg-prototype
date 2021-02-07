fb2be17776e3758a14cec1fa95889f1bc2189b03
Adrian Marceau <adrian.marceau@gmail.com>
2021-02-06 22:50:31 -0500

    Added patch that populates user-unlocked-abilities table with initial data

---
+++ admin/.patches/2021-02-06b_create-user-unlocked-abilities-table_pt2.php
@@ -0,0 +1,50 @@
+<?php
+
+// Collect a list of tables from the database
+$db_tables_list = $db->table_list();
+
+// If `mmrpg_users_unlocked_abilities` table exists now
+if (in_array('mmrpg_users_unlocked_abilities', $db_tables_list)
+    && method_exists('rpg_user', 'update_unlocked_abilities')){
+
+    // Check to see if the newly created table is completely empty
+    $existing_records = $db->get_value("SELECT
+        COUNT(*) AS num_records
+        FROM mmrpg_users_unlocked_abilities
+        WHERE user_id > 0
+        ;", 'num_records');
+
+    // If there are NO records, we should populate them now
+    if (empty($existing_records)){
+
+        // Collect a list of all users and their save data so we can populate the table
+        echo('- populating `mmrpg_users_unlocked_abilities` table with user data'.PHP_EOL);
+        $user_unlocked_abilities_records = $db->get_array_list("SELECT
+            saves.user_id,
+            saves.save_values_battle_abilities
+            FROM mmrpg_saves AS saves
+            LEFT JOIN mmrpg_leaderboard AS board ON board.user_id = saves.user_id
+            WHERE board.board_points > 0 AND saves.user_id > 0
+            ORDER BY board.board_date_modified DESC
+            ;", 'user_id');
+
+        // Loop through all the users found and update their database tables
+        if (!empty($user_unlocked_abilities_records)){
+            foreach ($user_unlocked_abilities_records AS $user_id => $save_data){
+                if (empty($save_data['save_values_battle_abilities'])){ continue; }
+                $user_unlocked_abilities = json_decode($save_data['save_values_battle_abilities'], true);
+                if (!empty($user_unlocked_abilities)){
+                    echo('- adding records for user_id '.$user_id.PHP_EOL);
+                    //error_log('$user_id = '.print_r($user_id, true));
+                    //error_log('$user_unlocked_abilities = '.print_r($user_unlocked_abilities, true));
+                    rpg_user::update_unlocked_abilities($user_id, $user_unlocked_abilities);
+                    //break;
+                }
+            }
+        }
+
+    }
+
+}
+
+?>
