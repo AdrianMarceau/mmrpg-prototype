fb7222b093c08774c0bb7d92f387895cfcfd364e
Adrian Marceau <adrian.marceau@gmail.com>
2023-03-27 02:37:02 -0400

    Minor markup changes to admin home for accessibility

--- classes/cms_admin.php
+++ classes/cms_admin.php
@@ -410,9 +410,9 @@ class cms_admin {
                 if (isset($option_info['link']['icon'])){ $icon_tokens = array($option_info['link']['icon']); }
                 elseif (isset($option_info['link']['icons'])){ $icon_tokens = $option_info['link']['icons']; }
                 if (!empty($icon_tokens)){ foreach ($icon_tokens AS $token){ $link_icons .= '<i class="icon fa fa-'.$token.'"></i>'; } }
-                echo('<div class="link">'.$link_bullet.'<a'.$link_url.$link_target.$link_class.'>'.$link_text.'</a>'.$link_icons.$repo_icons.'</div>'.PHP_EOL);
+                echo('<div class="link">'.$link_bullet.'<a'.$link_url.$link_target.$link_class.'>'.$link_text.'</a>'.$link_icons.$repo_icons.'&nbsp;</div>'.PHP_EOL);
                 $desc_text = $option_info['desc'];
-                echo('<div class="desc"><em>'.$desc_text.'</em></div>'.PHP_EOL);
+                echo('<div class="desc">:&nbsp;<em>'.$desc_text.'</em></div>'.PHP_EOL);
                 if (!empty($option_info['buttons'])){
                     $buttons_markup = array();
                     foreach ($option_info['buttons'] AS $button_key => $button_info){
