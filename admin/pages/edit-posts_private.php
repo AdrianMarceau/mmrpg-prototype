b74df53634f602a6c88150aaae69c88be5386add
Adrian Marceau <adrian.marceau@gmail.com>
2023-03-27 02:50:07 -0400

    Added basic admin post editor, may need more work

---
+++ admin/pages/edit-posts_private.php
@@ -0,0 +1,13 @@
+<?
+// Define the global post values for this page
+$this_post_class = 'private';
+$this_post_xclass = 'private';
+$this_post_class_name = 'reply';
+$this_post_xclass_name = 'replies';
+$this_post_parentclass_name = 'message';
+$this_post_xparentclass_name = 'messages';
+$this_post_page_basename = 'Moderate Message Replies';
+$this_post_page_baseurl = 'admin/edit-message-replies/';
+// Require the main challenge editor script with above values preset
+require_once('edit-posts.php');
+?>
