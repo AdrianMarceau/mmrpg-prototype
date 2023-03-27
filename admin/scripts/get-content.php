decfc7902d8d91e7cdeee9aaceff7b43cdfcd922
Adrian Marceau <adrian.marceau@gmail.com>
2023-03-27 03:15:38 -0400

    Updated get-content and cms_thread_post w/ new functions for pulling posts

--- admin/scripts/get-content.php
+++ admin/scripts/get-content.php
@@ -16,7 +16,7 @@ switch ($request_type) {
     // Collect threads if requested
     case 'get-threads': {
 
-        // Collect the category ID if it's been provided
+        // Collect the category ID and kind if they've been provided
         $category_id = isset($_GET['category']) && is_numeric($_GET['category']) ? (int)$_GET['category'] : -1;
         $category_kind = isset($_GET['kind']) && ($_GET['kind'] === 'public' || $_GET['kind'] === 'private') ? $_GET['kind'] : '';
         if ($category_id === 0 || $category_kind === 'private'){ $category_id = 0; $category_kind = 'private'; }
@@ -30,6 +30,9 @@ switch ($request_type) {
 
         // Collect the target value if 'kind' is 'private' and the target is provided
         $thread_target = ($category_kind === 'private' && isset($_GET['target']) && is_numeric($_GET['target'])) ? (int)$_GET['target'] : null;
+
+        // Collect the variable for fetching all fields if provided
+        $fetch_all_fields = isset($_GET['full']) && $_GET['full'] === 'true' ? true : false;
 
         // Collect the index of threads given the provided category info
         $filter_array = array();
@@ -47,7 +50,7 @@ switch ($request_type) {
         $sorting_array = array('threads.thread_date' => 'DESC', 'categories.category_order' => 'ASC');
 
         // Call the function with filters and pagination parameters
-        $community_threads_index = cms_thread::get_community_threads_index($filter_array, $pagination_array, $sorting_array);
+        $community_threads_index = cms_thread::get_community_threads_index($filter_array, $pagination_array, $sorting_array, $fetch_all_fields);
         $community_threads_array = array_values($community_threads_index);
         $num_community_threads = count($community_threads_array);
 
@@ -60,12 +63,50 @@ switch ($request_type) {
     // Collect thread comments if requested
     case 'get-threads-posts': {
 
-        $community_threads_posts_index = array();
-        // ... rest of code for getting threads and their posts ...
-        exit_action('success|' . json_encode($community_threads_posts_index));
+        // Collect the category ID and kind if they've been provided
+        $category_id = isset($_GET['category']) && is_numeric($_GET['category']) ? (int)$_GET['category'] : -1;
+        $category_kind = isset($_GET['kind']) && ($_GET['kind'] === 'public' || $_GET['kind'] === 'private') ? $_GET['kind'] : '';
+        if ($category_id === 0 || $category_kind === 'private'){ $category_id = 0; $category_kind = 'private'; }
+
+        // Collect the thread ID if it's been provided
+        $thread_id = isset($_GET['thread']) && is_numeric($_GET['thread']) ? (int)$_GET['thread'] : null;
+
+        // Collect the pagination fields if provided
+        $limit = isset($_GET['limit']) && is_numeric($_GET['limit']) ? (int)$_GET['limit'] : null;
+        $offset = isset($_GET['offset']) && is_numeric($_GET['offset']) ? (int)$_GET['offset'] : null;
+
+        // Collect the author ID if it's been provided
+        $author_id = isset($_GET['author']) && is_numeric($_GET['author']) ? (int)$_GET['author'] : null;
+
+        // Collect the variable for fetching all fields if provided
+        $fetch_all_fields = isset($_GET['full']) && $_GET['full'] === 'true' ? true : false;
+
+        // Collect the index of posts given the provided thread and category info
+        $filter_array = array();
+        $filter_array['category_kind'] = $category_kind;
+        if (!empty($category_id) && $category_id > 0){ $filter_array['category_id'] = $category_id; }
+        if ($thread_id !== null) { $filter_array['thread_id'] = $thread_id; }
+        if ($author_id !== null) { $filter_array['user_id'] = $author_id; }
+
+        // Prepare the pagination array based on provided values
+        $pagination_array = array();
+        if ($limit !== null) { $pagination_array['limit'] = $limit; }
+        if ($offset !== null) { $pagination_array['offset'] = $offset; }
+
+        // Define your sorting criteria
+        $sorting_array = array('posts.post_date' => 'ASC');
+
+        // Call the function with filters and pagination parameters
+        $community_thread_posts_index = cms_thread_post::get_community_thread_posts_index($filter_array, $pagination_array, $sorting_array, $fetch_all_fields);
+        $community_thread_posts_array = array_values($community_thread_posts_index);
+        $num_community_thread_posts = count($community_thread_posts_array);
+
+        // Print out the success message now that we have our data and exit
+        exit_action('success|found ' . $num_community_thread_posts . ' posts', $community_thread_posts_array);
         break;
 
     }
+
 
     // Undefined request type triggers error
     default: {
--- classes/cms_thread_post.php
+++ classes/cms_thread_post.php
@@ -46,5 +46,111 @@ class cms_thread_post {
 
     }
 
+    // Define a function for returning a list of thread posts from the database
+    public static function get_community_thread_posts($filters = array(), $pagination = array(), $sorting = array(), $fetch_all_fields = false, $index_by_id = false) {
+        $db = cms_database::get_database();
+
+        $select_fields = self::get_index_fields(false, 'posts');
+
+        if (!$fetch_all_fields) {
+            $remove = array('posts.post_body');
+            $select_fields = array_filter($select_fields, function($val) use($remove){ return !in_array($val, $remove); });
+        }
+
+        $select_fields = array_merge($select_fields, array(
+            'users.user_id AS author_id',
+            'users.user_name AS author_name',
+            'users.user_name_clean AS author_name_clean',
+            'users.user_name_public AS author_name_public',
+            'users.user_image_path AS author_image_path',
+            'users.user_colour_token AS author_colour_token',
+            'users.user_colour_token2 AS author_colour_token2'
+        ));
+
+        if ($fetch_all_fields) {
+            $select_fields[] = 'posts.post_body';
+        }
+
+        $select_fields_imploded = implode(', ', $select_fields);
+
+        $sql = "SELECT
+                {$select_fields_imploded}
+                FROM mmrpg_posts AS posts
+                LEFT JOIN mmrpg_users AS users ON users.user_id = posts.user_id
+                WHERE posts.post_deleted = 0 ";
+
+        if (!empty($filters)) {
+            if (isset($filters['category_id'])) { $sql .= "AND posts.category_id = {$filters['category_id']} "; }
+            if (isset($filters['thread_id'])) { $sql .= "AND posts.thread_id = {$filters['thread_id']} "; }
+            if (isset($filters['user_id'])) { $sql .= "AND posts.user_id = {$filters['user_id']} "; }
+            // Add any other filters here as needed
+        }
+
+        if (!empty($sorting)) {
+            $sorting_array = array();
+            foreach ($sorting as $field => $direction) {
+                $direction = strtoupper($direction) === 'ASC' ? 'ASC' : 'DESC';
+                $sorting_array[] = "{$field} {$direction}";
+            }
+            $sorting_string = implode(', ', $sorting_array);
+            $sql .= "ORDER BY {$sorting_string} ";
+        } else {
+            $sql .= "ORDER BY posts.post_date DESC ";
+        }
+
+        if (!empty($pagination) && isset($pagination['limit']) && isset($pagination['offset'])) {
+            $sql .= "LIMIT {$pagination['limit']} OFFSET {$pagination['offset']} ";
+        }
+
+        $sql .= "; ";
+
+        if ($index_by_id){
+            $community_thread_posts_index = $db->get_array_list($sql, 'post_id');
+        } else {
+            $community_thread_posts_index = $db->get_array_list($sql);
+        }
+
+        if (empty($community_thread_posts_index)) {
+            $community_thread_posts_index = array();
+        }
+
+        return $community_thread_posts_index;
+    }
+
+    // Define a function for returning an indexed list of thread posts from the database
+    public static function get_community_thread_posts_index($filters = array(), $pagination = array(), $sorting = array(), $fetch_all_fields = false) {
+        return self::get_community_thread_posts($filters, $pagination, $sorting, $fetch_all_fields, true);
+    }
+
+    // Define a function for generating select option markup given post info
+    public static function generate_thread_post_option_markup($post_info, $include_user_name = true, $include_post_date = true)
+    {
+        $post_id = $post_info['post_id'];
+        $user_name = !empty($post_info['author_name_public']) && $post_info['author_name_public'] !== $post_info['author_name']
+            ? $post_info['author_name_public'] . ' / ' . $post_info['author_name']
+            : $post_info['author_name_public'] ?: $post_info['author_name'];
+        $post_label = "Post ID {$post_id}";
+        if ($include_user_name) {
+            $post_label .= ' (by ' . $user_name . ')';
+        }
+        if ($include_post_date) {
+            $post_label .= ' (' . date('Y-m-d', $post_info['post_date']) . ')';
+        }
+        return '<option value="' . $post_id . '">' . $post_label . '</option>';
+    }
+
+    // Define a function for generating select options markup given an array of post info
+    public static function generate_thread_post_options_markup($community_thread_posts_index, $include_user_name = true, $include_post_date = true)
+    {
+        $post_options_markup = array();
+        //$post_options_markup[] = '<option value="">-</option>';
+        foreach ($community_thread_posts_index as $this_post_key => $post_info) {
+            $post_options_markup[] = self::generate_thread_post_option_markup($post_info, $include_user_name, $include_post_date);
+        }
+        $post_options_markup = implode(PHP_EOL, $post_options_markup);
+        return $post_options_markup;
+    }
+
+
 }
 ?>
