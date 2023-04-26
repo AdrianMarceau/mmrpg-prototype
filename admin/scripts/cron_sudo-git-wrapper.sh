5a5e606dc3dbb0659b9af972a4ae9e4b76094bf2
Adrian Marceau <adrian.marceau@gmail.com>
2023-04-26 13:40:44 -0400

    Another attempted output fix for admin update script r9

--- admin/scripts/cron_sudo-git-wrapper.sh
+++ admin/scripts/cron_sudo-git-wrapper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Get the script's directory
+current_user="$USER"
 current_script_path="$(readlink -f "$BASH_SOURCE")"
 project_base_path="$(dirname "$(dirname "$(dirname "$current_script_path")")")"
 log_base_path="$(dirname "$project_base_path")"
@@ -21,21 +22,21 @@ PUSH_LIST_FILE="$project_base_path/.cache/admin/cron_git-push-pending.list"
 PULL_LIST_FILE="$project_base_path/.cache/admin/cron_git-pull-pending.list"
 
 #echo "=========="
-#echo "Running the sudo-git-wrapper script as '$1' on $(date '+%Y-%m-%d @ %l:%M%p')"
-echo "Running the '$BASH_SOURCE' script as '$1' on $(date '+%Y-%m-%d @ %l:%M%p')"
+echo "Running the '$BASH_SOURCE' script as '$current_user' on $(date '+%Y-%m-%d @ %l:%M%p')"
 #echo "-----"
 
 # Check if the list files exist and process them
 for LIST_FILE in "$PUSH_LIST_FILE" "$PULL_LIST_FILE"; do
     if [ -f "$LIST_FILE" ]; then
 
+        echo -e "(!) Found a list file: '$LIST_FILE'"
         echo "-----"
-        echo -e "Found a list file: '$LIST_FILE'"
 
         # Read the directories from the list file and process them
         processed_successfully=false
         while IFS= read -r repo_path; do
 
+            # Collect the relative path and action token
             repo_rel_path=$(echo "$repo_path" | sed "s|$project_base_path/||")
             filename="$(basename "$LIST_FILE")"
             action_token="$(echo "$filename" | sed 's/-pending.list//')"
@@ -45,24 +46,25 @@ for LIST_FILE in "$PUSH_LIST_FILE" "$PULL_LIST_FILE"; do
             cd "$repo_path"
             if [ "$action_token" = "cron_git-pull" ]; then
                 echo "Output from git pull:"
-                git_output=$(sudo -u "$1" git pull -s recursive -X theirs --no-edit 2>&1)
+                git_output=$(git pull -s recursive -X theirs --no-edit 2>&1)
                 echo "$git_output"
                 processed_successfully=true
             elif [ "$action_token" = "cron_git-push" ]; then
                 echo "First, output from git pull:"
-                git_output=$(sudo -u "$1" git pull -s recursive -X theirs --no-edit 2>&1)
+                git_output=$(git pull -s recursive -X theirs --no-edit 2>&1)
                 echo "$git_output"
                 echo "Then, output from git push:"
-                git_output=$(sudo -u "$1" git push origin master 2>&1)
+                git_output=$(git push origin master 2>&1)
                 echo "$git_output"
                 processed_successfully=true
             else
-                echo "Cron Error (1/2): Something went wrong running the sudo-git-wrapper script as '$1' on $(date '+%Y-%m-%d @ %l:%M%p')" >&2
+                echo "Cron Error (1/2): Something went wrong running the sudo-git-wrapper script as '$current_user' on $(date '+%Y-%m-%d @ %l:%M%p')" >&2
                 echo "Cron Error (2/2): Unknown list file: $LIST_FILE" >&2
                 echo "Unknown list file: $LIST_FILE" >&1
                 break
             fi
 
+            # If processed successfully, then print a message
             if [ "$processed_successfully" = true ]; then
                 echo -e "Finished executing '$(basename "$LIST_FILE" | sed 's/-pending.list//')' on path\n"
             fi
@@ -76,12 +78,8 @@ for LIST_FILE in "$PUSH_LIST_FILE" "$PULL_LIST_FILE"; do
         fi
         echo "-----"
 
-    #else
-        #echo "Nothing to '$(basename "$LIST_FILE" | sed 's/-pending.list//')' yet."
-
     fi
 done
 
-#echo "=========="
-#echo "-----"
+echo "Done."
 exit 0
