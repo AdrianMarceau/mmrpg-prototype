53d559a0c9009da496ce86af46e3f68e5d57bf88
Adrian Marceau <adrian.marceau@gmail.com>
2023-05-12 12:36:41 -0400

    Converted galaxy-bomb/black-hole and ice-slasher/frozen-foothold to new attachment format

--- classes/rpg_ability.php
+++ classes/rpg_ability.php
@@ -3686,6 +3686,8 @@ class rpg_ability extends rpg_object {
             array_unshift($static_attachment_function_args, $objects);
             $new_attachment_info = call_user_func_array($static_attachment_function, $static_attachment_function_args);
             if (!empty($new_attachment_info)){ $this_attachment_info = array_merge($this_attachment_info, $new_attachment_info); }
+        } else {
+            error_log('Unable to get `static_attachment_function_'.$attachment_token.'` from ability `'.$ability_token.'`');
         }
 
         // Return generated attachment info, whatever it is
@@ -3839,108 +3841,12 @@ class rpg_ability extends rpg_object {
 
     // Define a static function for generating a static field attachment of a "black hole" (from the Galaxy Bomb ability)
     public static function get_static_black_hole($static_attachment_key, $this_attachment_duration = 99){
-        $this_battle = rpg_battle::get_battle();
-        $existing_attachments = isset($this_battle->battle_attachments[$static_attachment_key]) ? count($this_battle->battle_attachments[$static_attachment_key]) : 0;
-        $this_ability_token = 'galaxy-bomb';
-        $this_attachment_token = 'ability_'.$this_ability_token.'_'.$static_attachment_key;
-        $this_attachment_image = $this_ability_token;
-        $this_attachment_destroy_text = 'The crushing <span class="ability_name ability_type ability_type_space_explode">Black Hole</span> behind {this_robot} faded away... ';
-        $this_attachment_repeat_text = 'The <span class="ability_name ability_type ability_type_space_explode">Black Hole</span> behind {this_robot} exherted its power! ';
-        $this_attachment_info = array(
-            'class' => 'ability',
-            'sticky' => true,
-            'ability_token' => $this_ability_token,
-            'ability_image' => $this_attachment_image,
-            'attachment_token' => $this_attachment_token,
-            'attachment_duration' => $this_attachment_duration,
-            'attachment_energy' => 0,
-            'attachment_energy_base_percent' => 10,
-            'attachment_sticky' => true,
-            'attachment_destroy' => array(
-                'trigger' => 'special',
-                'kind' => '',
-                'type' => '',
-                'percent' => true,
-                'modifiers' => false,
-                'frame' => 'defend',
-                'rates' => array(100, 0, 0),
-                'success' => array(9, -9999, -9999, 10, $this_attachment_destroy_text),
-                'failure' => array(9, -9999, -9999, 10, $this_attachment_destroy_text)
-                ),
-            'attachment_repeat' => array(
-                'kind' => 'energy',
-                'trigger' => 'damage',
-                'type' => 'space',
-                'type2' => 'explode',
-                'energy' => 10,
-                'percent' => true,
-                'modifiers' => true,
-                'frame' => 'damage',
-                'rates' => array(100, 0, 0),
-                'success' => array(1, -5, 5, -10, $this_attachment_repeat_text),
-                'failure' => array(1, -5, 5, -99, $this_attachment_repeat_text),
-                'options' => array(
-                    'apply_modifiers' => true,
-                    'apply_type_modifiers' => true,
-                    'apply_core_modifiers' => true,
-                    'apply_field_modifiers' => true,
-                    'apply_stat_modifiers' => false,
-                    'apply_position_modifiers' => false,
-                    'referred_damage' => true,
-                    'referred_damage_id' => 0,
-                    'referred_damage_stats' => array()
-                    )
-                ),
-            'ability_frame' => 1,
-            'ability_frame_animate' => array(1, 2, 3, 4, 5, 6, 7, 8, 9),
-            'ability_frame_offset' => array(
-                'x' => (-5 + ($existing_attachments * 6)),
-                'y' => (5 + $existing_attachments),
-                'z' => (-10 - $existing_attachments)
-                )
-            );
-        return $this_attachment_info;
+        return self::get_static_attachment('galaxy-bomb', 'black-hole', $static_attachment_key, $this_attachment_duration);
     }
 
     // Define a static function for generating a static field attachment of a "frozen foothold" (from the Ice Breath ability)
     public static function get_static_frozen_foothold($static_attachment_key, $this_attachment_duration = 99){
-        $this_battle = rpg_battle::get_battle();
-        $existing_attachments = isset($this_battle->battle_attachments[$static_attachment_key]) ? count($this_battle->battle_attachments[$static_attachment_key]) : 0;
-        $this_ability_token = 'ice-breath';
-        $this_attachment_token = 'ability_'.$this_ability_token.'_'.$static_attachment_key;
-        $this_attachment_image = $this_ability_token;
-        $this_attachment_destroy_text = 'The <span class="ability_name ability_type ability_type_freeze">Frozen Foothold</span> trapping {this_robot} faded away... ';
-        $this_attachment_info = array(
-            'class' => 'ability',
-            'sticky' => true,
-            'ability_token' => $this_ability_token,
-            'ability_image' => $this_attachment_image,
-            'attachment_token' => $this_attachment_token,
-            'attachment_duration' => $this_attachment_duration,
-            'attachment_sticky' => true,
-            'attachment_switch_disabled' => true,
-            'attachment_weaknesses' => array('flame', 'laser'),
-            'attachment_weaknesses_trigger' => 'either',
-            'attachment_destroy' => array(
-                'trigger' => 'special',
-                'kind' => '',
-                'type' => '',
-                'percent' => true,
-                'modifiers' => false,
-                'frame' => 'defend',
-                'rates' => array(100, 0, 0),
-                'success' => array(9, -9999, -9999, 10, $this_attachment_destroy_text),
-                'failure' => array(9, -9999, -9999, 10, $this_attachment_destroy_text)
-                ),
-            'ability_frame' => 2,
-            'ability_frame_animate' => array(2, 3),
-            'ability_frame_offset' => array(
-                'x' => (0 + ($existing_attachments * 8)),
-                'y' => (-5 + $existing_attachments),
-                'z' => (8 + $existing_attachments)
-                )
-            );
-        return $this_attachment_info;
+        return self::get_static_attachment('ice-slasher', 'frozen-foothold', $static_attachment_key, $this_attachment_duration);
     }
 
     // Define a static function for generating a static field attachment of a "super block" (from the Super Arm ability)
