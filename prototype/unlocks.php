4566ab20c4d4cec46ea07b402de76e8a95336272
Adrian Marceau <adrian.marceau@gmail.com>
2023-06-30 02:48:29 -0400

    Updated hero robot unlocks to respect level settings

--- prototype/unlocks.php
+++ prototype/unlocks.php
@@ -272,7 +272,7 @@ if ($battle_failure_counter_light >= 1 && !mmrpg_prototype_robot_unlocked(false,
     // Unlock Roll as a playable character
     $unlock_player_info = $mmrpg_index_players['dr-light'];
     $unlock_robot_info = rpg_robot::get_index_info('roll');
-    $unlock_robot_info['robot_level'] = 1;
+    $unlock_robot_info['robot_level'] = MMRPG_SETTINGS_GAMESTORY1_STARTLEVEL;
     $unlock_robot_info['robot_experience'] = 999;
     mmrpg_game_unlock_robot($unlock_player_info, $unlock_robot_info, true, true);
 
@@ -354,7 +354,7 @@ if ($battle_complete_counter_light >= MMRPG_SETTINGS_CHAPTER3_MISSIONCOUNT && !$
     if (!mmrpg_prototype_robot_unlocked(false, 'bass')){
         // Unlock Bass as a playable character
         $unlock_robot_info = rpg_robot::get_index_info('bass');
-        $unlock_robot_info['robot_level'] = 11;
+        $unlock_robot_info['robot_level'] = MMRPG_SETTINGS_GAMESTORY2_STARTLEVEL;
         $unlock_robot_info['robot_experience'] = 999;
         //$unlock_robot_info['robot_experience'] = 4000;
         mmrpg_game_unlock_robot($unlock_player_info, $unlock_robot_info, true, false);
@@ -400,7 +400,7 @@ if ($battle_failure_counter_wily >= 2 && !mmrpg_prototype_robot_unlocked(false, 
     // Unlock Disco as a playable character
     $unlock_player_info = $mmrpg_index_players['dr-wily'];
     $unlock_robot_info = rpg_robot::get_index_info('disco');
-    $unlock_robot_info['robot_level'] = 11;
+    $unlock_robot_info['robot_level'] = MMRPG_SETTINGS_GAMESTORY2_STARTLEVEL;
     $unlock_robot_info['robot_experience'] = 999;
     mmrpg_game_unlock_robot($unlock_player_info, $unlock_robot_info, true, true);
 
@@ -469,7 +469,7 @@ if ($battle_complete_counter_wily >= MMRPG_SETTINGS_CHAPTER3_MISSIONCOUNT && !$u
     if (!mmrpg_prototype_robot_unlocked(false, 'proto-man')){
         // Unlock Proto Man as a playable character
         $unlock_robot_info = rpg_robot::get_index_info('proto-man');
-        $unlock_robot_info['robot_level'] = 21;
+        $unlock_robot_info['robot_level'] = MMRPG_SETTINGS_GAMESTORY3_STARTLEVEL;
         $unlock_robot_info['robot_experience'] = 999;
         //$unlock_robot_info['robot_experience'] = 4000;
         mmrpg_game_unlock_robot($unlock_player_info, $unlock_robot_info, true, false);
@@ -514,7 +514,7 @@ if ($battle_failure_counter_cossack >= 3 && !mmrpg_prototype_robot_unlocked(fals
     // Unlock Rhythm as a playable character
     $unlock_player_info = $mmrpg_index_players['dr-cossack'];
     $unlock_robot_info = rpg_robot::get_index_info('rhythm');
-    $unlock_robot_info['robot_level'] = 21;
+    $unlock_robot_info['robot_level'] = MMRPG_SETTINGS_GAMESTORY3_STARTLEVEL;
     $unlock_robot_info['robot_experience'] = 999;
     mmrpg_game_unlock_robot($unlock_player_info, $unlock_robot_info, true, true);
 
@@ -913,7 +913,7 @@ if ($unlock_flag_wily){
         // Unlock Bass as a playable character
         $unlock_player_info = $mmrpg_index_players['dr-wily'];
         $unlock_robot_info = rpg_robot::get_index_info('bass');
-        $unlock_robot_info['robot_level'] = 11;
+        $unlock_robot_info['robot_level'] = MMRPG_SETTINGS_GAMESTORY2_STARTLEVEL;
         $unlock_robot_info['robot_experience'] = 999;
         mmrpg_game_unlock_robot($unlock_player_info, $unlock_robot_info, true, true);
     }
@@ -947,7 +947,7 @@ if ($unlock_flag_cossack){
         // Unlock Proto Man as a playable character
         $unlock_player_info = $mmrpg_index_players['dr-cossack'];
         $unlock_robot_info = rpg_robot::get_index_info('proto-man');
-        $unlock_robot_info['robot_level'] = 21;
+        $unlock_robot_info['robot_level'] = MMRPG_SETTINGS_GAMESTORY3_STARTLEVEL;
         $unlock_robot_info['robot_experience'] = 999;
         mmrpg_game_unlock_robot($unlock_player_info, $unlock_robot_info, true, true);
     }
