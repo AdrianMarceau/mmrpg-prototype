017712d18f3feb0167d7017afe0754d26f8aed2a
Adrian Marceau <adrian.marceau@gmail.com>
2023-07-09 04:07:25 -0400

    Reloaded sound effect aliases to a separate file

--- index_game.php
+++ index_game.php
@@ -240,6 +240,20 @@ $mmrpg_music_path = 'prototype/sounds/';
 $mmrpg_music_rootdir = MMRPG_CONFIG_CDN_ROOTDIR.$mmrpg_music_path;
 $mmrpg_music_rooturl = MMRPG_CONFIG_CDN_ROOTURL.$mmrpg_music_path;
 
+// Collect the music index from the database and then output to the JS
+$this_music_track_index = rpg_music_track::get_index(true, false, 'music_token', 'music_album+/+music_token');
+$this_music_track_index = array_map(function($info){ return array(
+    'token' => $info['music_token'],
+    'album' => $info['music_album'],
+    'game' => $info['music_game'],
+    'name' => $info['music_name'],
+    'link' => $info['music_link'],
+    'loop' => $info['music_loop'],
+    'order' => $info['music_order']
+    ); }, $this_music_track_index);
+//error_log('$this_music_track_index ='.print_r($this_music_track_index, true));
+echo 'gameSettings.customIndex.musicIndex = '.json_encode($this_music_track_index).';'.PHP_EOL;
+
 // Collect the sound effects index from the file and then output to the JS
 $this_sound_effects_path = $mmrpg_music_rootdir.'misc/sound-effects-curated/';
 $this_sound_effects_index = array();
@@ -261,23 +275,14 @@ if (!empty($this_sound_effects_index_raw['resources'])
         $this_sound_effects_index['sprite'][$token] = $sprite;
     }
 }
-
 //error_log('$this_sound_effects_index ='.print_r($this_sound_effects_index, true));
 echo 'gameSettings.customIndex.soundsIndex = '.json_encode($this_sound_effects_index).';'.PHP_EOL;
 
-// Collect the music index from the database and then output to the JS
-$this_music_track_index = rpg_music_track::get_index(true, false, 'music_token', 'music_album+/+music_token');
-$this_music_track_index = array_map(function($info){ return array(
-    'token' => $info['music_token'],
-    'album' => $info['music_album'],
-    'game' => $info['music_game'],
-    'name' => $info['music_name'],
-    'link' => $info['music_link'],
-    'loop' => $info['music_loop'],
-    'order' => $info['music_order']
-    ); }, $this_music_track_index);
-//error_log('$this_music_track_index ='.print_r($this_music_track_index, true));
-echo 'gameSettings.customIndex.musicIndex = '.json_encode($this_music_track_index).';'.PHP_EOL;
+// Collect the sound effect aliases index from teh file and then outpyt to the JS
+$sound_effects_aliases_index = array();
+require(MMRPG_CONFIG_ROOTDIR.'includes/sounds.php');
+error_log('$sound_effects_aliases_index ='.print_r($sound_effects_aliases_index, true));
+echo 'gameSettings.customIndex.soundsAliasesIndex = '.json_encode($sound_effects_aliases_index).';'.PHP_EOL;
 
 ?>
 </script>
