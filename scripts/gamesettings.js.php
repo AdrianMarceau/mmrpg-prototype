81eaabbd4772598dfee529f66796157883f77111
Adrian Marceau <adrian.marceau@gmail.com>
2023-08-22 02:17:56 -0400

    Fixed incorrecet flag value for allowEditing in gamesettings JS file

--- scripts/gamesettings.js.php
+++ scripts/gamesettings.js.php
@@ -8,7 +8,7 @@ echo('gameSettings.baseHref = "'.MMRPG_CONFIG_ROOTURL.'";'.PHP_EOL);
 echo('gameSettings.wapFlag = '.($flag_wap ? 'true' : 'false').';'.PHP_EOL);
 echo('gameSettings.cacheTime = "'.MMRPG_CONFIG_CACHE_DATE.'";'.PHP_EOL);
 echo('gameSettings.fadeIn = '.(isset($_GET['fadein']) ? $_GET['fadein'] : 'false').';'.PHP_EOL);
-echo('gameSettings.allowEditing = '.(!defined('MMRPG_REMOTE_GAME_ID') ? 'true' : 'false').';'.PHP_EOL);
+echo('gameSettings.allowEditing = '.(!defined('MMRPG_REMOTE_GAME_ID') || MMRPG_REMOTE_GAME_ID === 0 ? 'true' : 'false').';'.PHP_EOL);
 if (defined('MMRPG_CONFIG_CDN_ENABLED') && MMRPG_CONFIG_CDN_ENABLED === true){
     $audioBaseHref = MMRPG_CONFIG_CDN_ROOTURL.MMRPG_CONFIG_CDN_PROJECT.'/';
     echo('gameSettings.audioBaseHref = "'.$audioBaseHref.'";'.PHP_EOL);
