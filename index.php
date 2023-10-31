5d80ffc1e1df7cf8a70094557689fb80706c9203
Adrian Marceau <adrian.marceau@gmail.com>
2023-10-31 13:41:20 -0400

    Updated global index to halt 404 requests for non-existant images/sounds before they preload anything

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
+
+// If a 404 NOT FOUND FILE request has someone made it to this page, stop it here
+if (!empty($_SERVER['REQUEST_URI'])
+    && preg_match('/^\/(images|sounds)\//i', $_SERVER['REQUEST_URI'])
+    && preg_match('/(\.[a-z0-9]{3,4})($|\?)/i', $_SERVER['REQUEST_URI'])){
+    http_response_code(404);
+    exit();
+}
+
 // Include the TOP file
 require_once('top.php');
+
 // Require the appropriate index file
 if ($this_current_page == 'prototype'){ define('MMRPG_INDEX_GAME', true); require_once('index_game.php'); }
 else { define('MMRPG_INDEX_BASE', true); require_once('index_base.php'); }
+
 // Unset the database variable
 unset($db);
+
 ?>
