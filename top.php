afdd3960dbdd81ad33191d29c1804136c9baac39
Adrian Marceau <adrian.marceau@gmail.com>
2023-12-01 04:53:05 -0500

    MMRPG Version 3.5.143

--- top.php
+++ top.php
@@ -2,7 +2,7 @@
 
 // Include mandatory config files
 define('MMRPG_BUILD', 'mmrpg2k23');
-define('MMRPG_VERSION', '3.5.124');
+define('MMRPG_VERSION', '3.5.143');
 require('includes/config.php');
 
 // Update the timezone before starting the session
