1c758c82156d181932d9c42c84642e9c62778dc1
Adrian Marceau <adrian.marceau@gmail.com>
2023-12-02 12:31:37 -0500

    Hotfix: MMRPG Version 3.6.4

--- top.php
+++ top.php
@@ -2,7 +2,7 @@
 
 // Include mandatory config files
 define('MMRPG_BUILD', 'mmrpg2k23');
-define('MMRPG_VERSION', '3.6.0');
+define('MMRPG_VERSION', '3.6.4');
 require('includes/config.php');
 
 // Update the timezone before starting the session
