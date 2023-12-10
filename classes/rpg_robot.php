ac97d193b6eb18d7ae3508382b17b3ed95f29f65
Adrian Marceau <adrian.marceau@gmail.com>
2023-12-10 01:01:07 -0500

    Added missing function reset_frame_classes() to rpg_robot class

--- classes/rpg_robot.php
+++ classes/rpg_robot.php
@@ -1106,6 +1106,7 @@ class rpg_robot extends rpg_object {
 
     public function get_frame_classes(){ return $this->get_info('robot_frame_classes'); }
     public function set_frame_classes($value){ $this->set_info('robot_frame_classes', $value); }
+    public function reset_frame_classes(){ $this->set_info('robot_frame_classes', ''); }
 
     public function get_frame_styles(){ return $this->get_info('robot_frame_styles'); }
     public function set_frame_styles($value){ $this->set_info('robot_frame_styles', $value); }
