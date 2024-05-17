2e38660ded5ab2eb9f2f41bf3eacf5714b59cf52
Adrian Marceau <adrian.marceau@gmail.com>
2024-05-17 11:48:06 -0400

    Refined limit hearts display code in player selection

--- prototype/players.php
+++ prototype/players.php
@@ -39,16 +39,26 @@ else {
     // Define a quick function for getting the current limit heart markup
     $get_current_limit_hearts = function($player_token, $player_chapters_unlocked){
         $max_hearts = 1;
-        $num_hearts = mmrpg_prototype_limit_hearts_earned($player_token, $max_hearts);
+        $extra_hearts = 0;
+        $num_hearts = mmrpg_prototype_limit_hearts_earned($player_token, $max_hearts, $extra_hearts);
+        $num_hearts_to_show = $num_hearts - $extra_hearts;
+        $max_hearts_to_show = $max_hearts - $extra_hearts;
         $hearts_markup = '';
         $anti_hearts_markup = '';
-        for ($i = 1; $i <= $max_hearts; $i++){
-            if ($num_hearts >= $i){ $hearts_markup .= '<i class="fa fas fa-heart"></i>'; }
+        $extra_hearts_markup = '';
+        for ($i = 1; $i <= $max_hearts_to_show; $i++){
+            if ($num_hearts_to_show >= $i){ $hearts_markup .= '<i class="fa fas fa-heart"></i>'; }
             else { $anti_hearts_markup .= '<i class="fa fas fa-heart-broken"></i>'; }
         }
+        if (!empty($extra_hearts)){
+            for ($i = 1; $i <= $extra_hearts; $i++){
+                $extra_hearts_markup .= '<i class="fa fas fa-heart"></i>';
+            }
+        }
         $limit_hearts_markup = '';
-        $limit_hearts_markup .= '<span class="limit-hearts earned" style="font-size: 70%;">'.$hearts_markup.'</span>';
-        $limit_hearts_markup .= '<span class="limit-hearts unclaimed" style="font-size: 40%; opacity: 0.6; position: relative; bottom: 2px; left: 2px;">'.$anti_hearts_markup.'</span>';
+        if (!empty($hearts_markup)){ $limit_hearts_markup .= '<span class="limit-hearts earned" style="font-size: 70%;">'.$hearts_markup.'</span>'; }
+        if (!empty($anti_hearts_markup)){ $limit_hearts_markup .= '<span class="limit-hearts unclaimed" style="font-size: 40%; opacity: 0.6; position: relative; bottom: 2px; left: 2px; margin-right: 4px;">'.$anti_hearts_markup.'</span>'; }
+        if (!empty($extra_hearts_markup)){ $limit_hearts_markup .= '<span class="limit-hearts earned" style="font-size: 70%;">'.$extra_hearts_markup.'</span>'; }
         return $limit_hearts_markup;
         };
 
