eea238cfac965052a0c9c1e7a8d9b33fbb3d434a
Adrian Marceau <adrian.marceau@gmail.com>
2024-12-23 16:52:23 -0500

    Removed error-log debug from lib autoload files

--- .libs/minify.php
+++ .libs/minify.php
@@ -2,8 +2,8 @@
 // https://github.com/matthiasmullie/minify
 $lib = basename(__FILE__, '.php');
 $path = rtrim(dirname(__FILE__), '/').'/'.$lib.'/';
-error_log('$lib = '.print_r($lib, true));
-error_log('$path = '.print_r($path, true));
+//error_log('$lib = '.print_r($lib, true));
+//error_log('$path = '.print_r($path, true));
 require_once($path.'src/Minify.php');
 require_once($path.'src/CSS.php');
 require_once($path.'src/JS.php');
--- .libs/path-converter.php
+++ .libs/path-converter.php
@@ -2,8 +2,8 @@
 // https://github.com/matthiasmullie/path-converter
 $lib = basename(__FILE__, '.php');
 $path = rtrim(dirname(__FILE__), '/').'/'.$lib.'/';
-error_log('$lib = '.print_r($lib, true));
-error_log('$path = '.print_r($path, true));
+//error_log('$lib = '.print_r($lib, true));
+//error_log('$path = '.print_r($path, true));
 require_once($path.'src/ConverterInterface.php');
 require_once($path.'src/Converter.php');
 
