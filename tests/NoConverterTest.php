86b7274fa13559f522d95792c33bcff35db0ec9a
Adrian Marceau <adrian.marceau@gmail.com>
2024-12-23 16:44:34 -0500

    Squashed '.libs/path-converter/' content from commit 0e77b521
    git-subtree-dir: .libs/path-converter
    git-subtree-split: 0e77b521554de4cec4c222862e899a154b726a85

---
+++ tests/NoConverterTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace MatthiasMullie\PathConverter\Tests;
+
+use MatthiasMullie\PathConverter\NoConverter;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Converter test case.
+ */
+class NoConverterTest extends TestCase
+{
+    /**
+     * Test Converter, provided by dataProvider.
+     *
+     * @test
+     *
+     * @dataProvider dataProvider
+     */
+    public function convert($relative, $expected)
+    {
+        $converter = new NoConverter();
+        $result = $converter->convert($relative);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @return array [relative, from, to, expected result]
+     */
+    public static function dataProvider()
+    {
+        $tests = array();
+
+        $tests[] = array(
+            '../images/img.jpg',
+            '../images/img.jpg',
+        );
+
+        $tests[] = array(
+            '../../images/icon.gif',
+            '../../images/icon.gif',
+        );
+
+        // absolute path - doesn't make sense :)
+        $tests[] = array(
+            '/home/username/file.txt',
+            '/home/username/file.txt',
+        );
+
+        $tests[] = array(
+            'image.jpg',
+            'image.jpg',
+        );
+
+        $tests[] = array(
+            '../images/img.jpg',
+            '../images/img.jpg',
+        );
+
+        // https://github.com/forkcms/forkcms/issues/1186
+        $tests[] = array(
+            '../images/img.jpg',
+            '../images/img.jpg',
+        );
+
+        // https://github.com/matthiasmullie/path-converter/issues/1
+        $tests[] = array(
+            'image.jpg',
+            'image.jpg',
+        );
+
+        return $tests;
+    }
+}
