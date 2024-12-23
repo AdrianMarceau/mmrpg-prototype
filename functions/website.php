b3d4134dc07173ec45def7f6bb6d1995f46ad10d
Adrian Marceau <adrian.marceau@gmail.com>
2024-12-23 16:38:49 -0500

    Added some new array functions to the website functions file

--- functions/website.php
+++ functions/website.php
@@ -1916,4 +1916,52 @@ function get_timestamp_from_milliseconds($milliseconds = 0){
     return sprintf('%02d:%02d:%02d', $parts['minutes'], $parts['seconds'], $parts['frames']);
 }
 
+
+// Define a function that inserts a new element into an associative array after a given key position
+function array_insert_after_key(&$parent_array, $parent_key, $child_array, $child_key) {
+    $new_array = [];
+    foreach ($parent_array as $key => $value) {
+        $new_array[$key] = $value;
+        if ($key === $parent_key){ $new_array[$child_key] = $child_array; }
+    }
+    $parent_array = $new_array;
+}
+
+// Define a function that inserts a new element into an associative array before a given key position
+function array_insert_before_key(&$parent_array, $parent_key, $child_array, $child_key) {
+    $new_array = [];
+    foreach ($parent_array as $key => $value) {
+        if ($key === $parent_key) { $new_array[$child_key] = $child_array; }
+        $new_array[$key] = $value;
+    }
+    $parent_array = $new_array;
+}
+
+// Define a function that takes a parent array and they rearranges the provided keys in the order provided
+function array_rearrange_keys(&$parent_array, $ordered_keys) {
+    $new_array = [];
+    $ordered_items = [];
+    // Collect items by the specified order
+    foreach ($ordered_keys as $key) {
+        if (array_key_exists($key, $parent_array)) {
+            $ordered_items[$key] = $parent_array[$key];
+            unset($parent_array[$key]);
+        }
+    }
+    // Build the new array with ordered items in specified sequence
+    $keys_added = false;
+    foreach ($parent_array as $key => $value) {
+        if (!$keys_added && empty($new_array)) {
+            $new_array = array_merge($ordered_items, $new_array);
+            $keys_added = true;
+        }
+        $new_array[$key] = $value;
+    }
+    // If $ordered_keys appear later in $parent_array, append them to the end
+    if (!$keys_added) {
+        $new_array = array_merge($new_array, $ordered_items);
+    }
+    $parent_array = $new_array;
+}
+
 ?>
