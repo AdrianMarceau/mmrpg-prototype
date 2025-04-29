c3bbb252081dae12e934740a659a10948b4cb756
Adrian Marceau <adrian.marceau@gmail.com>
2025-04-29 13:41:27 -0400

    Fixed typo in campaign-complete unlock message

--- prototype/unlocks.php
+++ prototype/unlocks.php
@@ -221,7 +221,7 @@ function generate_prototype_postgame_message($player_token){
 
     // Generate the markup for bonus chapters unlocks
     $temp_bonus_chapter_markup = array();
-    $temp_bonus_chapter_markup[] = '<p>The <strong class="ability_type ability_type_speed">Mission Randomizer</strong> chapter lets you to refight past foes in unlikey pairings. These missions are perfect for trying out new strategies or grinding for cores!</p>';
+    $temp_bonus_chapter_markup[] = '<p>The <strong class="ability_type ability_type_speed">Mission Randomizer</strong> chapter lets you to refight past foes in unlikely pairings. These missions are perfect for trying out new strategies or grinding for cores!</p>';
     if (mmrpg_prototype_item_unlocked('light-program')){ $temp_bonus_chapter_markup[] = '<p>The <strong class="ability_type ability_type_attack">Player Battles</strong> chapter contains missions against the ghost-data of other users from the leaderboard. These missions are great for grinding lots of zenny!</p>'; }
     if (mmrpg_prototype_item_unlocked('cossack-program')){ $temp_bonus_chapter_markup[] = '<p>The <strong class="ability_type ability_type_defense">Star Fields</strong> chapter locates and displays any Field Star or Fusion Star missions that you\'ve yet to complete.  Collecting stars helps your robots grow stronger!</p>'; }
     if (mmrpg_prototype_item_unlocked('wily-program')){ $temp_bonus_chapter_markup[] = '<p>The <strong class="ability_type ability_type_energy">Challenge Mode</strong> chapter offers a collection of unique missions designed by the MMRPG staff. These are hard but have great rewards! <span style="font-size: 70%;position: relative;bottom: 4px;left: 2px;">(Try the Endless Attack Mode!)</span></p>'; }
