05b4a5c0a79cf86442fc80361f40a9d2a0253fae
Adrian Marceau <adrian.marceau@gmail.com>
2025-08-08 12:41:46 -0400

    Fixed bug w/ world map home button not working

--- world.php
+++ world.php
@@ -300,7 +300,9 @@ if (empty($map_exit_pos)){
 $WORLD_SESSION['world_maps'][$map_token]['spawn_pos'] = $map_spawn_pos;
 $WORLD_SESSION['world_maps'][$map_token]['exit_pos'] = $map_exit_pos;
 
-// If the world position has not been set yet, we can use the spawn position for it
+// If the default position has not been set, we can use the spawn position for that
+if (empty($default_world_position)){ $default_world_position = $map_spawn_pos; }
+// If the world position has not been set yet, we can use the spawn position for it as well
 if (empty($this_prototype_data['this_current_position'])){ $this_prototype_data['this_current_position'] = $map_spawn_pos; }
 
 // If there are any buttons defined, check to see if any of them have been pushed already
