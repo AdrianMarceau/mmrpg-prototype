e695fd8e6955718564b1a33e6fd4f0836835a31e
Adrian Marceau <adrian.marceau@gmail.com>
2025-08-14 14:53:16 -0400

    Updated world max-player-robots to match their earned limit hearts elsewhere

--- world.php
+++ world.php
@@ -232,7 +232,7 @@ if (!isset($WORLD_PLAYER_SESSION['last_robots'])){ $WORLD_PLAYER_SESSION['last_r
 // Collect the current player's robots and battle history
 $this_prototype_data['this_player_id'] = $this_player_info['player_id'];
 $this_prototype_data['this_player_token'] = $this_player_info['player_token'];
-$max_player_robots = MMRPG_WORLD_DEFAULT_TEAMSIZE; // TODO: make this dynamic based on limit hearts
+$max_player_robots = mmrpg_prototype_limit_hearts_earned($this_player_token) || 0;
 $allowed_player_robots = mmrpg_prototype_robots_unlocked($this_player_token, true);
 $current_player_robots = !empty($allowed_player_robots) ? array_slice($allowed_player_robots, 0, $max_player_robots) : array(); // TODO: make this customizable
 $summoned_player_robots = rpg_world::get_battle_history($this_player_token, 'robots_summoned');
