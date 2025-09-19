fbb6f60f2a40ddf1f0085afb129640b1da1920c7
Adrian Marceau <adrian.marceau@gmail.com>
2025-09-19 14:52:00 -0400

    Updated world to pre-generate sprite styles given metadata (should move this to external file)

--- world.php
+++ world.php
@@ -596,6 +596,70 @@ $flag_skip_fadein = true;
 <script type="text/javascript" src="scripts/script.js?<?=MMRPG_CONFIG_CACHE_DATE?>"></script>
 <script type="text/javascript" src="scripts/world.js?<?=MMRPG_CONFIG_CACHE_DATE?>"></script>
 <script type="text/javascript" src="content/all.js?<?=MMRPG_CONFIG_CACHE_DATE?>"></script>
+<style type="text/css">
+    <?
+    // Loop through all the index objects that have sprites and generate the styles for them
+    $object_sprite_kinds = array('player', 'robot', 'ability', 'item');
+    $get_index_styles = function($object_kind) use ($mmrpg_indexes){
+        $object_xkind = rpg_world::get_xkind($object_kind);
+        if (!empty($mmrpg_indexes[$object_xkind])){
+            $object_index = $mmrpg_indexes[$object_xkind];
+            //echo ('#mmrpg .sprite.'.$object_kind.' { /* ... */ }'.PHP_EOL);
+            //echo ('#mmrpg .sprite.'.$object_kind.' .wrap { /* ... */ }'.PHP_EOL);
+            // Check if this is one of the sprite types that uses one large composite sprite sheet and set a flag
+            $use_composite = $object_xkind === 'items' || $object_xkind === 'abilities' ? true : false;
+            // If we're using a composite sprite sheet, we need to set some common styles on the inner .sprite element
+            // to indicate they all share the same image and background-size, then we can just adjust background-position
+            if ($use_composite){
+                $inner_styles = array();
+                if ($object_xkind === 'items'){ $pseudo_meta = rpg_world::get_sprite_meta($object_kind, 'small-screw'); }
+                elseif ($object_xkind === 'abilities'){ $pseudo_meta = rpg_world::get_sprite_meta($object_kind, 'buster-shot'); }
+                //error_log('-> $pseudo_meta = '.print_r($pseudo_meta, true));
+                if (!empty($pseudo_meta['sheetPath'])){ $inner_styles['background-image'] = 'url('.$pseudo_meta['sheetPath'].'?'.MMRPG_CONFIG_CACHE_DATE.')'; }
+                if (!empty($pseudo_meta['sheetSize'])){ $inner_styles['background-size'] = $pseudo_meta['sheetSize'][0].'px '.$pseudo_meta['sheetSize'][1].'px'; }
+                $inner_styles_string = !empty($inner_styles) ? implode('; ', array_map(function($v, $k){ return $k.': '.$v.' !important'; }, $inner_styles, array_keys($inner_styles))) : '';
+                //error_log('-> $inner_styles_string = '.print_r($inner_styles_string, true));
+                if (!empty($inner_styles_string)){ echo ('#mmrpg .sprite.'.$object_kind.' .wrap .sprite { '.$inner_styles_string.' }'.PHP_EOL); }
+                }
+            // Now we can loop through the actual objects of this type and generate their individual styles where applicable
+            foreach ($object_index AS $object_token => $object_info){
+                //error_log('-> scanning object w/ kind: '.$object_kind.' && token: '.$object_token);
+                if ($object_token === $object_kind){ continue; }
+                if ($object_info[$object_kind.'_class'] === 'system'){ continue; }
+                if (empty($object_info[$object_kind.'_flag_published'])){ continue; }
+                if (empty($object_info[$object_kind.'_flag_complete'])){ continue; }
+                $object_alt = ''; // TODO: maybe expand this later?
+                $sprite_meta = rpg_world::get_sprite_meta($object_kind, $object_token, $object_alt);
+                //error_log('-> $sprite_meta = '.print_r($sprite_meta, true));
+                if (empty($sprite_meta)){ continue; }
+                $outer_styles = array();
+                if (!empty($sprite_meta['spriteSpeed']) && $sprite_meta['spriteSpeed'] !== 1){ $outer_styles['--sprite-speed'] = $sprite_meta['spriteSpeed']; }
+                $outer_styles_string = !empty($outer_styles) ? implode('; ', array_map(function($v, $k){ return $k.': '.$v.' !important'; }, $outer_styles, array_keys($outer_styles))) : '';
+                if (!empty($outer_styles_string)){ echo('#mmrpg .sprite.'.$object_kind.'[data-token="'.$object_token.'"] { '.$outer_styles_string.' }'.PHP_EOL); }
+                $inner_styles = array();
+                if ($use_composite && !empty($sprite_meta['sheetOffset'])){ $inner_styles['background-position'] = $sprite_meta['sheetOffset'][0].'px '.$sprite_meta['sheetOffset'][1].'px'; }
+                if (!$use_composite && !empty($sprite_meta['sheetPath'])){ $inner_styles['background-image'] = 'url('.$sprite_meta['sheetPath'].'?'.MMRPG_CONFIG_CACHE_DATE.')'; }
+                $inner_styles_string = !empty($inner_styles) ? implode('; ', array_map(function($v, $k){ return $k.': '.$v.' !important'; }, $inner_styles, array_keys($inner_styles))) : '';
+                if (!empty($inner_styles_string)){ echo('#mmrpg .sprite.'.$object_kind.'[data-token="'.$object_token.'"] .wrap .sprite { '.$inner_styles_string.' }'.PHP_EOL); }
+                // If this particular object has any image alts defined, we'll need to generate styles for them too
+                if (!$use_composite && !empty($object_info[$object_kind.'_image_alts'])){
+                    $object_image_alts = $object_info[$object_kind.'_image_alts'];
+                    //error_log('-> '.$object_token.' / $object_image_alts = '.print_r($object_image_alts, true));
+                    foreach ($object_image_alts AS $alt_info){
+                        $alt_token = $alt_info['token'];
+                        $alt_meta = rpg_world::get_sprite_meta($object_kind, $object_token, $alt_token);
+                        $alt_inner_styles = array();
+                        if (!empty($alt_meta['sheetPath'])){ $alt_inner_styles['background-image'] = 'url('.$alt_meta['sheetPath'].'?'.MMRPG_CONFIG_CACHE_DATE.')'; }
+                        $alt_inner_styles_string = !empty($alt_inner_styles) ? implode('; ', array_map(function($v, $k){ return $k.': '.$v.' !important'; }, $alt_inner_styles, array_keys($alt_inner_styles))) : '';
+                        if (!empty($alt_inner_styles_string)){ echo('#mmrpg .sprite.'.$object_kind.'[data-token="'.$object_token.'"][data-alt="'.$alt_token.'"] .wrap .sprite { '.$alt_inner_styles_string.' }'.PHP_EOL); }
+                        }
+                    }
+                }
+            }
+        };
+    foreach ($object_sprite_kinds AS $object_kind){ $get_index_styles($object_kind); }
+    ?>
+</style>
 <script type="text/javascript">
 
 // Update relevent game settings and flags
